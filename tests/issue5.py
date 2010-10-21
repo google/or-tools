aac101bc4dd26eb2f657c7a0ab1923e7b6b5be32
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2010-10-21 18:08:09 +0000

    final version of tests/issue5.py

--- tests/issue5.py
+++ tests/issue5.py
@@ -1,25 +1,25 @@
 # Copyright 2010 Hakan Kjellerstrand hakank@bonetmail.com
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
+'''
 
   A programming puzzle from Einav in Google CP Solver.
 
   From
   'A programming puzzle from Einav'
   http://gcanyon.wordpress.com/2009/10/28/a-programming-puzzle-from-einav/
-  '''
+
   My friend Einav gave me this programming puzzle to work on. Given
   this array of positive and negative numbers:
   33   30  -10 -6  18   7  -11 -23   6
@@ -48,7 +48,6 @@
   66. So this solution is better (I don't know if it's the best)
   A pure brute force solution would have to try over 30 billion solutions.
   I wrote code to solve this in J. I'll post that separately.
-  '''
 
   Compare with the following models:
   * MiniZinc http://www.hakank.org/minizinc/einav_puzzle.mzn
@@ -57,7 +56,7 @@
 
   This model was created by Hakan Kjellerstrand (hakank@bonetmail.com)
   Also see my other Google CP Solver models: http://www.hakank.org/google_or_tools/
-"""
+'''
 
 from constraint_solver import pywrapcp
 
@@ -73,13 +72,42 @@ def main():
 
 
     # small problem
-    rows = 3;
-    cols = 3;
-    data = [
-        [ 33,  30, -10],
-        [-16,  19,   9],
-        [-17, -12, -14]
-        ]
+#    data = [
+#        [ 33,  30, -10],
+#        [-16,  19,   9],
+#        [-17, -12, -14]
+#        ]
+
+    data = [[33, 30, 10, -6, 18, -7, -11, 23, -6],
+            [16, -19, 9, -26, -8, -19, -8, -21, -14],
+            [17, 12, -14, 31, -30, 13, -13, 19, 16],
+            [-6, -11, 1, 17, -12, -4, -7, 14, -21],
+            [18, -31, 34, -22, 17, -19, 20, 24, 6],
+            [33, -18, 17, -15, 31, -5, 3, 27, -3],
+            [-18, -20, -18, 31, 6, 4, -2, -12, 24],
+            [27, 14, 4, -29, -3, 5, -29, 8, -12],
+            [-15, -7, -23, 23, -9, -8, 6, 8, -12],
+            [33, -23, -19, -4, -8, -7, 11, -12, 31],
+            [-20, 19, -15, -30, 11, 32, 7, 14, -5],
+            [-23, 18, -32, -2, -31, -7, 8, 24, 16],
+            [32, -4, -10, -14, -6, -1, 0, 23, 23],
+            [25, 0, -23, 22, 12, 28, -27, 15, 4],
+            [-30, -13, -16, -3, -3, -32, -3, 27, -31],
+            [22, 1, 26, 4, -2, -13, 26, 17, 14],
+            [-9, -18, 3, -20, -27, -32, -11, 27, 13],
+            [-17, 33, -7, 19, -32, 13, -31, -2, -24],
+            [-31, 27, -31, -29, 15, 2, 29, -15, 33],
+            [-18, -23, 15, 28, 0, 30, -4, 12, -32],
+            [-3, 34, 27, -25, -18, 26, 1, 34, 26],
+            [-21, -31, -10, -13, -30, -17, -12, -26, 31],
+            [23, -31, -19, 21, -17, -10, 2, -23, 23],
+            [-3, 6, 0, -3, -32, 0, -10, -25, 14],
+            [-19, 9, 14, -27, 20, 15, -5, -27, 18],
+            [11, -6, 24, 7, -17, 26, 20, -31, -25],
+            [-25, 4, -16, 30, 33, 23, -4, -4, 23]]
+
+    rows = len(data)
+    cols = len(data[0])
 
     #
     # variables
@@ -87,13 +115,11 @@ def main():
     x = {}
     for i in range(rows):
         for j in range(cols):
-            x[i,j] = solver.IntVar(-100, 100, "x[%i,%i]" % (i,j))
+            x[i, j] = solver.IntVar(-100, 100, 'x[%i,%i]' % (i, j))
 
-    x_flat = [x[i,j] for i in range(rows) for j in range(cols)]
-
-    row_signs = [solver.IntVar([-1, 1], "row_signs(%i)" % i)
+    row_signs = [solver.IntVar([-1, 1], 'row_signs(%i)' % i)
                  for i in range(rows)]
-    col_signs = [solver.IntVar([-1, 1], "col_signs(%i)" % j)
+    col_signs = [solver.IntVar([-1, 1], 'col_signs(%i)' % j)
                  for j in range(cols)]
 
     #
@@ -124,18 +150,19 @@ def main():
     #
     # search and result
     #
-    db = solver.Phase(row_signs + col_signs,
-                      solver.INT_VAR_SIMPLE,
-                      solver.INT_VALUE_SIMPLE)
+    db = solver.Phase(col_signs + row_signs,
+                      solver.CHOOSE_FIRST_UNBOUND,
+                      solver.ASSIGN_MIN_VALUE)
 
-    solver.NewSearch(db, [objective])
+    search_log = solver.SearchLog(100000, total_sum)
+    solver.NewSearch(db, [objective, search_log])
 
     num_solutions = 0
     while solver.NextSolution():
         num_solutions += 1
-        print "Sum =", objective.best()
-        print "row_sums:", [row_sums[i].Value() for i in range(rows)]
-        print "col_sums:", [col_sums[j].Value() for j in range(cols)]
+        print 'Sum =', objective.best()
+        print 'row_sums:', [row_sums[i].Value() for i in range(rows)]
+        print 'col_sums:', [col_sums[j].Value() for j in range(cols)]
         for i in range(rows):
             for j in range(cols):
                 print x[i,j].Value(),
@@ -145,10 +172,10 @@ def main():
     solver.EndSearch()
 
     print
-    print "num_solutions:", num_solutions
-    print "failures:", solver.failures()
-    print "branches:", solver.branches()
-    print "wall_time:", solver.wall_time()
+    print 'num_solutions:', num_solutions
+    print 'failures:', solver.failures()
+    print 'branches:', solver.branches()
+    print 'wall_time:', solver.wall_time(), 'ms'
 
 
 if __name__ == '__main__':
