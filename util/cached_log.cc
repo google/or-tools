3f9ed3ad60b61c83f47918273b39afdec0249729
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2010-10-25 15:28:23 +0000

    missing files

---
+++ util/cached_log.cc
@@ -0,0 +1,61 @@
+// Copyright 2010 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "util/cached_log.h"
+
+namespace operations_research {
+
+CachedLog::CachedLog() {}
+
+CachedLog::~CachedLog() {}
+
+namespace {
+const double kLn2 = 0.0;
+
+void InitLn2() {
+  if (kLn2 == 0.0) {
+    kLn2 = log(2);
+  }
+}
+
+double FastLog2(int64 input) {
+#if defined(_MSC_VER)
+  return log(input) / kLn2;
+#else
+  return log2(input);
+#endif
+}
+}
+
+void CachedLog::Init(int size) {
+#if defined(_MSC_VER)
+  InitLn2();
+#endif
+  CHECK(cache_.empty());
+  CHECK_GT(size, 0);
+  cache_.resize(size, 0.0);
+  for (int i = 0; i < size; ++i) {
+    cache_[i] = FastLog2(i + 1);
+  }
+}
+
+double CachedLog::Log2(int64 input) const {
+  CHECK_GE(input, 1);
+  if (input <= cache_.size()) {
+    return cache_[input - 1];
+  } else {
+    return FastLog2(input);
+  }
+}
+
+}  // namespace operations_research
---
+++ util/cached_log.h
@@ -0,0 +1,47 @@
+// Copyright 2010 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef UTIL_CACHED_LOG_H_
+#define UTIL_CACHED_LOG_H_
+
+#include <math.h>
+
+#include <vector>
+
+#include "base/logging.h"
+#include "base/util.h"
+
+namespace operations_research {
+// This class is used when manipulating search space estimations.  It
+// provides fast access to log of a domain size.
+// Future Extensions:
+//   - Sum of log on an array.
+//   - Sum of log on an array with callback.
+
+class CachedLog {
+ public:
+  CachedLog();
+  ~CachedLog();
+
+  // This method can only be called once, and with a cache_size > 0.
+  void Init(int cache_size);
+
+  // Returns the log2 of 'input'.
+  double Log2(int64 input) const;
+ private:
+  vector<double> cache_;
+  DISALLOW_COPY_AND_ASSIGN(CachedLog);
+};
+}  // namespace operations_research
+
+#endif  // UTIL_CACHED_LOG_H_
