9d10acdf89b625d3a8d04f99172bcc9608a2acf1
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2010-11-08 13:01:51 +0000

    smaller code in nonogram_table2

--- python/nonogram_table2.py
+++ python/nonogram_table2.py
@@ -71,143 +71,63 @@ from constraint_solver import pywrapcp
 
 
 #
-# Global constraint regular
-#
-# This is a translation of MiniZinc's regular constraint (defined in
-# lib/zinc/globals.mzn), via the Comet code refered above.
-# All comments are from the MiniZinc code.
-# '''
-# The sequence of values in array 'x' (which must all be in the range 1..S)
-# is accepted by the DFA of 'Q' states with input 1..S and transition
-# function 'd' (which maps (1..Q, 1..S) -> 0..Q)) and initial state 'q0'
-# (which must be in 1..Q) and accepting states 'F' (which all must be in
-# 1..Q).  We reserve state 0 to be an always failing state.
-# '''
-#
-# x : IntVar array
-# Q : number of states
-# S : input_max
-# d : transition matrix
-# q0: initial state
-# F : accepting states
-def regular(x, Q, S, d, q0, F):
-
-  solver = x[0].solver()
-
-  assert Q > 0, 'regular: "Q" must be greater than zero'
-  assert S > 0, 'regular: "S" must be greater than zero'
-
-  # d2 is the same as d, except we add one extra transition for
-  # each possible input;  each extra transition is from state zero
-  # to state zero.  This allows us to continue even if we hit a
-  # non-accepted input.
-
-  d2 = []
-  for i in range(Q + 1):
-    for j in range(1, S + 1):
-      if i == 0:
-        d2.append((0, j, 0))
-      else:
-        d2.append((i, j, d[i - 1][j - 1]))
-
-  solver.Add(solver.TransitionConstraint(x, d2, q0, F))
-
-
-#
-# Make a transition (automaton) matrix from a
+# Make a transition (automaton) list of tuples from a
 # single pattern, e.g. [3,2,1]
 #
-def make_transition_matrix(pattern):
-
+def make_transition_tuples(pattern):
   p_len = len(pattern)
   num_states = p_len + sum(pattern)
 
   # this is for handling 0-clues. It generates
-  # just the state 1,2
+  # just the minimal state
   if num_states == 0:
-    num_states = 1
+    return ((0, 0, 0))
 
-  t_matrix = []
-  for i in range(num_states):
-    row = []
-    for j in range(2):
-      row.append(0)
-    t_matrix.append(row)
+  tuples = []
 
   # convert pattern to a 0/1 pattern for easy handling of
   # the states
-  tmp = [0 for i in range(num_states)]
+  tmp = [0];
   c = 0
-  tmp[c] = 0
-  for i in range(p_len):
-    for j in range(pattern[i]):
-      c += 1
-      tmp[c] = 1
-    if c < num_states-1:
-      c += 1
-      tmp[c] = 0
-
-  t_matrix[num_states-1][0] = num_states
-  t_matrix[num_states-1][1] = 0
+  for pattern_index in range(p_len):
+    tmp.extend([1] * pattern[pattern_index])
+    tmp.append(0)
 
   for i in range(num_states):
+    state = i + 1
     if tmp[i] == 0:
-      t_matrix[i][0] = i+1
-      t_matrix[i][1] = i+2
+      tuples.append((state, 0, state))
+      tuples.append((state, 1, state + 1))
     else:
-      if i < num_states-1:
-        if tmp[i+1] == 1:
-          t_matrix[i][0] = 0
-          t_matrix[i][1] = i+2
+      if i < num_states - 1:
+        if tmp[i + 1] == 1:
+          tuples.append((state, 1, state + 1))
         else:
-          t_matrix[i][0] = i+2
-          t_matrix[i][1] = 0
+          tuples.append((state, 0, state + 1))
+  tuples.append((num_states, 0, num_states))
+  return (tuples, num_states)
 
-
-  # print 'The states:'
-  # for i in range(num_states):
-  #     for j in range(2):
-  #         print t_matrix[i][j],
-  #     print
-  # print
-
-  return t_matrix
 
 #
-# check each rule by creating an automaton
-# and regular
+# check each rule by creating an automaton and transition constraint.
 #
 def check_rule(rules, y):
-  solver = y[0].solver()
-
-  r_len = sum([1 for i in range(len(rules)) if rules[i] > 0])
-  rules_tmp = []
-  for i in range(len(rules)):
-    if rules[i] > 0:
-      rules_tmp.append(rules[i])
-
-  transition_fn = make_transition_matrix(rules_tmp)
-  n_states = len(transition_fn)
-  input_max = 2
-
-  # Note: we cannot use 0 since it's the failing state
+  cleaned_rule = [rules[i] for i in range(len(rules)) if rules[i] > 0]
+  (transition_tuples, last_state) = make_transition_tuples(cleaned_rule)
   initial_state = 1
-  accepting_states = [n_states] # This is the last state
+  accepting_states = [last_state]
 
-  regular(y, n_states, input_max, transition_fn,
-          initial_state, accepting_states)
+  solver = y[0].solver()
+  solver.Add(solver.TransitionConstraint(y,
+                                         transition_tuples,
+                                         initial_state,
+                                         accepting_states))
 
 
-
-def main(rows, row_rule_len, row_rules,
-         cols, col_rule_len, col_rules):
+def main(rows, row_rule_len, row_rules, cols, col_rule_len, col_rules):
 
   # Create the solver.
   solver = pywrapcp.Solver('Regular test')
-
-  #
-  # data
-  #
 
   #
   # variables
@@ -215,7 +135,7 @@ def main(rows, row_rule_len, row_rules,
   board = {}
   for i in range(rows):
     for j in range(cols):
-      board[i, j] = solver.IntVar(1, 2, 'board[%i, %i]' % (i, j))
+      board[i, j] = solver.IntVar(0, 1, 'board[%i, %i]' % (i, j))
   board_flat = [board[i, j] for i in range(rows) for j in range(cols)]
 
   # Flattened board for labeling.
@@ -236,12 +156,10 @@ def main(rows, row_rule_len, row_rules,
   # constraints
   #
   for i in range(rows):
-    check_rule([row_rules[i][j] for j in range(row_rule_len)],
-               [board[i,j] for j in range(cols)])
+    check_rule(row_rules[i], [board[i, j] for j in range(cols)])
 
   for j in range(cols):
-    check_rule([col_rules[j][k] for k in range(col_rule_len)],
-               [board[i, j] for i in range(rows)])
+    check_rule(col_rules[j], [board[i, j] for i in range(rows)])
 
 
   #
@@ -251,6 +169,7 @@ def main(rows, row_rule_len, row_rules,
                     solver.CHOOSE_FIRST_UNBOUND,
                     solver.ASSIGN_MIN_VALUE)
 
+  print 'before solver, wall time = ', solver.wall_time(), 'ms'
   solver.NewSearch(db)
 
   num_solutions = 0
@@ -258,7 +177,7 @@ def main(rows, row_rule_len, row_rules,
     print
     num_solutions += 1
     for i in range(rows):
-      row = [board[i, j].Value() - 1 for j in range(cols)]
+      row = [board[i, j].Value() for j in range(cols)]
       row_pres = []
       for j in row:
         if j == 1:
