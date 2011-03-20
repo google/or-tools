c190b8b9a5f594f64f9e02e7ad32cd17c8c7d26a
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2011-03-20 17:04:37 +0000

    remove arraylists, some cleanups

--- com/google/ortools/constraintsolver/samples/MagicSquare.java
+++ com/google/ortools/constraintsolver/samples/MagicSquare.java
@@ -23,123 +23,117 @@ import com.google.ortools.constraintsolver.Solver;
 
 public class MagicSquare {
 
-    static {
-        System.loadLibrary("jniconstraintsolver");
+  static {
+    System.loadLibrary("jniconstraintsolver");
+  }
+
+
+  /**
+   *
+   * Solves the Magic Square problem.
+   * See http://www.hakank.org/google_or_tools/magic_square.py
+   *
+   */
+  private static void solve(int n, int num) {
+
+    Solver solver = new Solver("MagicSquare");
+
+    System.out.println("n: " + n);
+
+    //
+    // variables
+    //
+    IntVar[][] x = new IntVar[n][n];
+
+    // for the branching
+    IntVar[] x_flat = new IntVar[n*n];
+
+    //
+    // constraints
+    //
+    final long s = (n * (n * n + 1)) / 2;
+    System.out.println("s: " + s);
+    // IntVar s = solver.makeIntVar(0, n*n*n, "s");
+
+    IntVar[] diag1 = new IntVar[n];
+    IntVar[] diag2 = new IntVar[n];
+    for(int i = 0; i < n; i++) {
+      IntVar[] row = new IntVar[n];
+      for(int j = 0; j < n; j++) {
+        x[i][j] = solver.makeIntVar(1, n * n, "x[" + i + "," + j + "]");
+        x_flat[i * n + j] = x[i][j];
+        row[j] = x[i][j];
+      }
+      // sum row to s
+      solver.addConstraint(solver.makeSumEquality(row, s));
+
+      diag1[i] = x[i][i];
+      diag2[i] = x[i][n - i - 1];
+    }
+    // sum diagonals to s
+    solver.addConstraint(solver.makeSumEquality(diag1, s));
+    solver.addConstraint(solver.makeSumEquality(diag2, s));
+
+    // sum columns to s
+    for(int j = 0; j < n; j++) {
+      IntVar[] col = new IntVar[n];
+      for(int i = 0; i < n; i++) {
+        col[i] = x[i][j];
+      }
+      solver.addConstraint(solver.makeSumEquality(col, s));
     }
 
+    // all are different
+    solver.addConstraint(solver.makeAllDifferent(x_flat, true));
 
-    /**
-     *
-     * Solves the Magic Square problem.
-     * See http://www.hakank.org/google_or_tools/magic_square.py
-     *
-     */
-    private static void solve(int n, int num) {
+    // symmetry breaking: upper left is 1
+    // solver.addConstraint(solver.makeEquality(x[0][0], 1));
 
-        Solver solver = new Solver("MagicSquare");
 
-        System.out.println("n: " + n);
-
-        //
-        // variables
-        //
-        IntVar x[][] = new IntVar[n][n];
-
-        // for the branching
-        IntVar x_flat[] = new IntVar[n*n];
-
-        // 
-        // constraints
-        // 
-        long s = (n*(n*n+1))/2;
-        System.out.println("s: " + s);
-        // IntVar s = solver.makeIntVar(0, n*n*n, "s");
-
-        ArrayList<IntVar> diag1 = new ArrayList<IntVar>();    
-        ArrayList<IntVar> diag2 = new ArrayList<IntVar>();    
-        for(int i = 0; i < n; i++) {
-            ArrayList<IntVar> row = new ArrayList<IntVar>();
-            for(int j = 0; j < n; j++) {
-                x[i][j] = solver.makeIntVar(1, n*n, "x["+i+","+j+"]");
-                x_flat[i*n+j] = x[i][j];
-
-                row.add(x[i][j]);
-            }
-            // sum row to s
-            solver.addConstraint(
-                solver.makeSumEquality(row.toArray(new IntVar[1]), s));
-
-            diag1.add(x[i][i]);
-            diag2.add(x[i][n-i-1]);
-
-        }
-        // sum diagonals to s
-        solver.addConstraint(
-            solver.makeSumEquality(diag1.toArray(new IntVar[1]), s));
-        solver.addConstraint(
-            solver.makeSumEquality(diag2.toArray(new IntVar[1]), s));
-
-        // sum columns to s
+    //
+    // Solve
+    //
+    DecisionBuilder db = solver.makePhase(x_flat,
+                                          solver.CHOOSE_FIRST_UNBOUND,
+                                          solver.ASSIGN_CENTER_VALUE);
+    solver.newSearch(db);
+    int c = 0;
+    while (solver.nextSolution()) {
+      for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
-            ArrayList<IntVar> col = new ArrayList<IntVar>();
-            for(int i = 0; i < n; i++) {
-                col.add(x[i][j]);
-            }
-            solver.addConstraint(
-                solver.makeSumEquality(col.toArray(new IntVar[1]), s));
+          System.out.print(x[i][j].value() + " ");
         }
-
-        // all are different
-        solver.addConstraint(solver.makeAllDifferent(x_flat, true));
-
-        // symmetry breaking: upper left is 1
-        // solver.addConstraint(solver.makeEquality(x[0][0], 1));
-
-
-        //
-        // Solve
-        //
-        DecisionBuilder db = solver.makePhase(x_flat,
-                                              solver.CHOOSE_FIRST_UNBOUND,
-                                              solver.ASSIGN_CENTER_VALUE);
-        solver.newSearch(db);
-        int c = 0;
-        while (solver.nextSolution()) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    System.out.print(x[i][j].value() + " ");
-                }
-                System.out.println();
-            }
-            System.out.println();
-            c++;
-            if (num > 0 && c >= num) {
-                break;
-            }
-        }
-        solver.endSearch();
-
-        // Statistics
         System.out.println();
-        System.out.println("Solutions: " + solver.solutions());
-        System.out.println("Failures: " + solver.failures());
-        System.out.println("Branches: " + solver.branches());
-        System.out.println("Wall time: " + solver.wall_time() + "ms");
+      }
+      System.out.println();
+      c++;
+      if (num > 0 && c >= num) {
+        break;
+      }
+    }
+    solver.endSearch();
 
+    // Statistics
+    System.out.println();
+    System.out.println("Solutions: " + solver.solutions());
+    System.out.println("Failures: " + solver.failures());
+    System.out.println("Branches: " + solver.branches());
+    System.out.println("Wall time: " + solver.wall_time() + "ms");
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    int n = 4;
+    int num = 0;
+
+    if (args.length > 0) {
+      n = Integer.parseInt(args[0]);
     }
 
-    public static void main(String[] args) throws Exception {
-        int n = 4;
-        int num = 0;
-
-        if (args.length > 0) {
-            n = Integer.parseInt(args[0]);
-        }
-
-        if (args.length > 1) {
-            num = Integer.parseInt(args[1]);
-        }
-
-        MagicSquare.solve(n, num);
+    if (args.length > 1) {
+      num = Integer.parseInt(args[1]);
     }
+
+    MagicSquare.solve(n, num);
+  }
 }
