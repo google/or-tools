76f48784cf0750d3addcd350e9121f988b870a49
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2011-05-26 09:33:52 +0000

    fix java example

--- com/google/ortools/flow/samples/FlowExample.java
+++ com/google/ortools/flow/samples/FlowExample.java
@@ -41,18 +41,18 @@ public class FlowExample {
     StarGraph graph = new StarGraph(numSources + numTargets,
                                     numSources * numTargets);
     MinCostFlow  minCostFlow = new MinCostFlow(graph);
-    for (int source = 1; source <= numSources; ++source) {
-      for (int target = 1; target <= numTargets; ++target) {
+    for (int source = 0; source < numSources; ++source) {
+      for (int target = 0; target < numTargets; ++target) {
         final long arc = graph.addArc(source, numSources + target);
-        minCostFlow.setArcUnitCost(arc, costs[source -  1][target - 1]);
+        minCostFlow.setArcUnitCost(arc, costs[source][target]);
         minCostFlow.setArcCapacity(arc, 1);
       }
     }
 
-    for (int source = 1; source <= numSources; ++source) {
+    for (int source = 0; source < numSources; ++source) {
       minCostFlow.setNodeSupply(source, 1);
     }
-    for (int target = 1; target <= numTargets; ++target) {
+    for (int target = 0; target < numTargets; ++target) {
       minCostFlow.setNodeSupply(numSources + target, -1);
     }
 
@@ -64,13 +64,13 @@ public class FlowExample {
     System.out.println("Max Flow Problem");
     final int numNodes = 6;
     final int numArcs = 9;
-    final int[] tails = {1, 1, 1, 1, 2, 3, 4, 4, 5};
-    final int[] heads = {2, 3, 4, 5, 4, 5, 5, 6, 6};
+    final int[] tails = {0, 0, 0, 0, 1, 2, 3, 3, 4};
+    final int[] heads = {1, 2, 3, 4, 3, 4, 4, 5, 5};
     final int[] capacities = {5, 8, 5, 3, 4, 5, 6, 6, 4};
     final int[] expectedFlows = {4, 4, 2, 0, 4, 4, 0, 6, 4};
     final int expectedTotalFlow = 10;
     StarGraph graph = new StarGraph(numNodes, numArcs);
-    MaxFlow maxFlow = new MaxFlow(graph, 1, numNodes);
+    MaxFlow maxFlow = new MaxFlow(graph, 0, numNodes - 1);
     for (int i = 0; i < numArcs; ++i) {
       final long arc = graph.addArc(tails[i], heads[i]);
       maxFlow.setArcCapacity(arc, capacities[i]);
@@ -78,7 +78,7 @@ public class FlowExample {
     final long totalFlow = maxFlow.computeMaxFlow();
     System.out.println("total flow " + totalFlow + "/" + expectedTotalFlow);
     for (int i = 0; i < numArcs; ++i) {
-      System.out.println("Arc " + i + ": " + maxFlow.flow(i + 1) + "/" +
+      System.out.println("Arc " + i + ": " + maxFlow.flow(i) + "/" +
                          expectedFlows[i]);
     }
   }
