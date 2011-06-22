6f0850aae156719865cce808fee2720d521bdce9
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2011-06-22 08:37:05 +0000

    small fix on base dependencies

--- base/stringprintf.cc
+++ base/stringprintf.cc
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "base/stringpiece.h"
+#include "base/stringprintf.h"
 
 #include <cstdarg>
 #include <cstdio>
