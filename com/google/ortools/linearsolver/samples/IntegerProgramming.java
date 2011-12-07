01c911090ea78c6f65698ba85900c9c9495964cb
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2011-12-07 10:49:29 +0000

    detect LP solver in java before calling them

--- com/google/ortools/linearsolver/samples/IntegerProgramming.java
+++ com/google/ortools/linearsolver/samples/IntegerProgramming.java
@@ -29,52 +29,57 @@ public class IntegerProgramming {
   }
 
 
-  private static void runIntegerProgrammingExample(int optimizationProblemType) {
-    MPSolver solver = new MPSolver("IntegerProgrammingExample",
-                                   optimizationProblemType);
-    double infinity = solver.infinity();
-    // x1 and x2 are integer non-negative variables.
-    MPVariable x1 = solver.makeIntVar(0.0, infinity, "x1");
-    MPVariable x2 = solver.makeIntVar(0.0, infinity, "x2");
+  private static void runIntegerProgrammingExample(String solverType) {
+    try {
+      MPSolver solver = new MPSolver("IntegerProgrammingExample",
+                                     MPSolver.getSolverEnum(solverType));
+      double infinity = solver.infinity();
+      // x1 and x2 are integer non-negative variables.
+      MPVariable x1 = solver.makeIntVar(0.0, infinity, "x1");
+      MPVariable x2 = solver.makeIntVar(0.0, infinity, "x2");
 
-    // Minimize x1 + 2 * x2.
-    solver.setObjectiveCoefficient(x1, 1);
-    solver.setObjectiveCoefficient(x2, 2);
+      // Minimize x1 + 2 * x2.
+      solver.setObjectiveCoefficient(x1, 1);
+      solver.setObjectiveCoefficient(x2, 2);
 
-    // 2 * x2 + 3 * x1 >= 17.
-    MPConstraint ct = solver.makeConstraint(17, infinity);
-    ct.setCoefficient(x1, 3);
-    ct.setCoefficient(x2, 2);
+      // 2 * x2 + 3 * x1 >= 17.
+      MPConstraint ct = solver.makeConstraint(17, infinity);
+      ct.setCoefficient(x1, 3);
+      ct.setCoefficient(x2, 2);
 
-    int resultStatus = solver.solve();
+      int resultStatus = solver.solve();
 
-    // Check that the problem has an optimal solution.
-    if (resultStatus != MPSolver.OPTIMAL) {
-      System.err.println("The problem does not have an optimal solution!");
-      return;
+      // Check that the problem has an optimal solution.
+      if (resultStatus != MPSolver.OPTIMAL) {
+        System.err.println("The problem does not have an optimal solution!");
+        return;
+      }
+
+      System.out.println("Problem solved in " + solver.wallTime() +
+                         " milliseconds");
+
+      // The objective value of the solution.
+      System.out.println("Optimal objective value = " + solver.objectiveValue());
+
+      // The value of each variable in the solution.
+      System.out.println("x1 = " + x1.solutionValue());
+      System.out.println("x2 = " + x2.solutionValue());
+
+      System.out.println("Advanced usage:");
+      System.out.println("Problem solved in " + solver.nodes() +
+                         " branch-and-bound nodes");
+    } catch (java.lang.Exception exc) {
+      System.out.println("- Solver not supported.");
     }
-
-    System.out.println("Problem solved in " + solver.wallTime() +
-                       " milliseconds");
-
-    // The objective value of the solution.
-    System.out.println("Optimal objective value = " + solver.objectiveValue());
-
-    // The value of each variable in the solution.
-    System.out.println("x1 = " + x1.solutionValue());
-    System.out.println("x2 = " + x2.solutionValue());
-
-    System.out.println("Advanced usage:");
-    System.out.println("Problem solved in " + solver.nodes() +
-                       " branch-and-bound nodes");
   }
+
 
   public static void main(String[] args) throws Exception {
     System.out.println("---- Integer programming example with GLPK ----");
-    runIntegerProgrammingExample(MPSolver.GLPK_MIXED_INTEGER_PROGRAMMING);
+    runIntegerProgrammingExample("GLPK_MIXED_INTEGER_PROGRAMMING");
     System.out.println("---- Integer programming example with CBC ----");
-    runIntegerProgrammingExample(MPSolver.CBC_MIXED_INTEGER_PROGRAMMING);
+    runIntegerProgrammingExample("CBC_MIXED_INTEGER_PROGRAMMING");
     System.out.println("---- Integer programming example with SCIP ----");
-    runIntegerProgrammingExample(MPSolver.SCIP_MIXED_INTEGER_PROGRAMMING);
+    runIntegerProgrammingExample("SCIP_MIXED_INTEGER_PROGRAMMING");
   }
 }
--- com/google/ortools/linearsolver/samples/LinearProgramming.java
+++ com/google/ortools/linearsolver/samples/LinearProgramming.java
@@ -29,79 +29,84 @@ public class LinearProgramming {
   }
 
 
-  private static void runLinearProgrammingExample(int optimizationProblemType) {
-    MPSolver solver = new MPSolver("LinearProgrammingExample",
-                                   optimizationProblemType);
-    double infinity = solver.infinity();
-    // x1, x2 and x3 are continuous non-negative variables.
-    MPVariable x1 = solver.makeNumVar(0.0, infinity, "x1");
-    MPVariable x2 = solver.makeNumVar(0.0, infinity, "x2");
-    MPVariable x3 = solver.makeNumVar(0.0, infinity, "x3");
+  private static void runLinearProgrammingExample(String solverType) {
+    try {
+      MPSolver solver = new MPSolver("LinearProgrammingExample",
+                                     MPSolver.getSolverEnum(solverType));
+      double infinity = solver.infinity();
+      // x1, x2 and x3 are continuous non-negative variables.
+      MPVariable x1 = solver.makeNumVar(0.0, infinity, "x1");
+      MPVariable x2 = solver.makeNumVar(0.0, infinity, "x2");
+      MPVariable x3 = solver.makeNumVar(0.0, infinity, "x3");
 
-    // Maximize 10 * x1 + 6 * x2 + 4 * x3.
-    solver.setObjectiveCoefficient(x1, 10);
-    solver.setObjectiveCoefficient(x2, 6);
-    solver.setObjectiveCoefficient(x3, 4);
-    solver.setMaximization();
+      // Maximize 10 * x1 + 6 * x2 + 4 * x3.
+      solver.setObjectiveCoefficient(x1, 10);
+      solver.setObjectiveCoefficient(x2, 6);
+      solver.setObjectiveCoefficient(x3, 4);
+      solver.setMaximization();
 
-    // x1 + x2 + x3 <= 100.
-    MPConstraint c0 = solver.makeConstraint(-infinity, 100.0);
-    c0.setCoefficient(x1, 1);
-    c0.setCoefficient(x2, 1);
-    c0.setCoefficient(x3, 1);
+      // x1 + x2 + x3 <= 100.
+      MPConstraint c0 = solver.makeConstraint(-infinity, 100.0);
+      c0.setCoefficient(x1, 1);
+      c0.setCoefficient(x2, 1);
+      c0.setCoefficient(x3, 1);
 
-    // 10 * x1 + 4 * x2 + 5 * x3 <= 600.
-    MPConstraint c1 = solver.makeConstraint(-infinity, 600.0);
-    c1.setCoefficient(x1, 10);
-    c1.setCoefficient(x2, 4);
-    c1.setCoefficient(x3, 5);
+      // 10 * x1 + 4 * x2 + 5 * x3 <= 600.
+      MPConstraint c1 = solver.makeConstraint(-infinity, 600.0);
+      c1.setCoefficient(x1, 10);
+      c1.setCoefficient(x2, 4);
+      c1.setCoefficient(x3, 5);
 
-    // 2 * x1 + 2 * x2 + 6 * x3 <= 300.
-    MPConstraint c2 = solver.makeConstraint(-infinity, 300.0);
-    c2.setCoefficient(x1, 2);
-    c2.setCoefficient(x2, 2);
-    c2.setCoefficient(x3, 6);
+      // 2 * x1 + 2 * x2 + 6 * x3 <= 300.
+      MPConstraint c2 = solver.makeConstraint(-infinity, 300.0);
+      c2.setCoefficient(x1, 2);
+      c2.setCoefficient(x2, 2);
+      c2.setCoefficient(x3, 6);
 
-    System.out.println("Number of variables = " + solver.numVariables());
-    System.out.println("Number of constraints = " + solver.numConstraints());
+      System.out.println("Number of variables = " + solver.numVariables());
+      System.out.println("Number of constraints = " + solver.numConstraints());
 
-    int resultStatus = solver.solve();
+      int resultStatus = solver.solve();
 
-    // Check that the problem has an optimal solution.
-    if (resultStatus != MPSolver.OPTIMAL) {
-      System.err.println("The problem does not have an optimal solution!");
-      return;
+      // Check that the problem has an optimal solution.
+      if (resultStatus != MPSolver.OPTIMAL) {
+        System.err.println("The problem does not have an optimal solution!");
+        return;
+      }
+
+      System.out.println("Problem solved in " + solver.wallTime() +
+                         " milliseconds");
+
+      // The objective value of the solution.
+      System.out.println("Optimal objective value = " +
+                         solver.objectiveValue());
+
+      // The value of each variable in the solution.
+      System.out.println("x1 = " + x1.solutionValue());
+      System.out.println("x2 = " + x2.solutionValue());
+      System.out.println("x3 = " + x3.solutionValue());
+
+      System.out.println("Advanced usage:");
+      System.out.println("Problem solved in " + solver.iterations() +
+                         " iterations");
+      System.out.println("x1: reduced cost = " + x1.reducedCost());
+      System.out.println("x2: reduced cost = " + x2.reducedCost());
+      System.out.println("x3: reduced cost = " + x3.reducedCost());
+      System.out.println("c0: dual value = " + c0.dualValue());
+      System.out.println("    activity = " + c0.activity());
+      System.out.println("c1: dual value = " + c1.dualValue());
+      System.out.println("    activity = " + c1.activity());
+      System.out.println("c2: dual value = " + c2.dualValue());
+      System.out.println("    activity = " + c2.activity());
+    } catch (java.lang.Exception exc) {
+      System.out.println("- Solver not supported.");
     }
-
-    System.out.println("Problem solved in " + solver.wallTime() +
-                       " milliseconds");
-
-    // The objective value of the solution.
-    System.out.println("Optimal objective value = " + solver.objectiveValue());
-
-    // The value of each variable in the solution.
-    System.out.println("x1 = " + x1.solutionValue());
-    System.out.println("x2 = " + x2.solutionValue());
-    System.out.println("x3 = " + x3.solutionValue());
-
-    System.out.println("Advanced usage:");
-    System.out.println("Problem solved in " + solver.iterations() +
-                       " iterations");
-    System.out.println("x1: reduced cost = " + x1.reducedCost());
-    System.out.println("x2: reduced cost = " + x2.reducedCost());
-    System.out.println("x3: reduced cost = " + x3.reducedCost());
-    System.out.println("c0: dual value = " + c0.dualValue());
-    System.out.println("    activity = " + c0.activity());
-    System.out.println("c1: dual value = " + c1.dualValue());
-    System.out.println("    activity = " + c1.activity());
-    System.out.println("c2: dual value = " + c2.dualValue());
-    System.out.println("    activity = " + c2.activity());
   }
 
   public static void main(String[] args) throws Exception {
     System.out.println("---- Linear programming example with GLPK ----");
-    runLinearProgrammingExample(MPSolver.GLPK_LINEAR_PROGRAMMING);
+    runLinearProgrammingExample("GLPK_LINEAR_PROGRAMMING");
     System.out.println("---- Linear programming example with CLP ----");
-    runLinearProgrammingExample(MPSolver.CLP_LINEAR_PROGRAMMING);
+    runLinearProgrammingExample("CLP_LINEAR_PROGRAMMING");
   }
 }
