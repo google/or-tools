7381679951edde05d6cf5fcb4be0558f77d2ea18
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-01-29 15:51:03 +0000

    more tests on testlp.cs

--- tests/testlp.cs
+++ tests/testlp.cs
@@ -45,15 +45,77 @@ public class CsTestLp
     MPConstraint ct5 = solver.Add(1 <= x);
     MPConstraint ct6 = solver.Add(1 == x);
     CheckEquality(ct1.GetCoefficient(x), 1.0, "test1");
-    CheckEquality(ct2.GetCoefficient(x), 1.0, "test1");
-    CheckEquality(ct3.GetCoefficient(x), 1.0, "test1");
-    CheckEquality(ct4.GetCoefficient(x), 1.0, "test1");
-    CheckEquality(ct5.GetCoefficient(x), 1.0, "test1");
-    CheckEquality(ct6.GetCoefficient(x), 1.0, "test1");
+    CheckEquality(ct2.GetCoefficient(x), 1.0, "test2");
+    CheckEquality(ct3.GetCoefficient(x), 1.0, "test3");
+    CheckEquality(ct4.GetCoefficient(x), 1.0, "test4");
+    CheckEquality(ct5.GetCoefficient(x), 1.0, "test5");
+    CheckEquality(ct6.GetCoefficient(x), 1.0, "test6");
+    CheckEquality(ct1.Lb(), 1.0, "test7");
+    CheckEquality(ct1.Ub(), double.PositiveInfinity, "test8");
+    CheckEquality(ct2.Lb(), double.NegativeInfinity, "test9");
+    CheckEquality(ct2.Ub(), 1.0, "test10");
+    CheckEquality(ct3.Lb(), 1.0, "test11");
+    CheckEquality(ct3.Ub(), 1.0, "test12");
+    CheckEquality(ct4.Lb(), double.NegativeInfinity, "test13");
+    CheckEquality(ct4.Ub(), 1.0, "test14");
+    CheckEquality(ct5.Lb(), 1.0, "test15");
+    CheckEquality(ct5.Ub(), double.PositiveInfinity, "test16");
+    CheckEquality(ct6.Lb(), 1.0, "test17");
+    CheckEquality(ct6.Ub(), 1.0, "test18");
+  }
+
+  static void TestVarAddition()
+  {
+    Console.WriteLine("Running TestVarAddition");
+    MPSolver solver = new MPSolver("TestVarAddition",
+                                   MPSolver.CLP_LINEAR_PROGRAMMING);
+    MPVariable x = solver.MakeNumVar(0.0, 100.0, "x");
+    MPVariable y = solver.MakeNumVar(0.0, 100.0, "y");
+    MPConstraint ct1 = solver.Add(x + y == 1);
+    CheckEquality(ct1.GetCoefficient(x), 1.0, "test1");
+    CheckEquality(ct1.GetCoefficient(y), 1.0, "test2");
+    MPConstraint ct2 = solver.Add(x + x == 1);
+    CheckEquality(ct2.GetCoefficient(x), 2.0, "test3");
+    MPConstraint ct3 = solver.Add(x + (y + x) == 1);
+    CheckEquality(ct3.GetCoefficient(x), 2.0, "test4");
+    CheckEquality(ct3.GetCoefficient(y), 1.0, "test5");
+    MPConstraint ct4 = solver.Add(x + (y + x + 3) == 1);
+    CheckEquality(ct4.GetCoefficient(x), 2.0, "test4");
+    CheckEquality(ct4.GetCoefficient(y), 1.0, "test5");
+    CheckEquality(ct4.Lb(), -2.0, "test6");
+    CheckEquality(ct4.Ub(), -2.0, "test7");
+  }
+
+  static void TestVarMultiplication()
+  {
+    Console.WriteLine("Running TestVarMultiplication");
+    MPSolver solver = new MPSolver("TestVarMultiplication",
+                                   MPSolver.CLP_LINEAR_PROGRAMMING);
+    MPVariable x = solver.MakeNumVar(0.0, 100.0, "x");
+    MPVariable y = solver.MakeNumVar(0.0, 100.0, "y");
+    MPConstraint ct1 = solver.Add(3 * x == 1);
+    CheckEquality(ct1.GetCoefficient(x), 3.0, "test1");
+    MPConstraint ct2 = solver.Add(x * 3 == 1);
+    CheckEquality(ct2.GetCoefficient(x), 3.0, "test2");
+    MPConstraint ct3 = solver.Add(x + (2 * y + 3 * x) == 1);
+    CheckEquality(ct3.GetCoefficient(x), 4.0, "test3");
+    CheckEquality(ct3.GetCoefficient(y), 2.0, "test4");
+    MPConstraint ct4 = solver.Add(x + 5 * (y + x + 3) == 1);
+    CheckEquality(ct4.GetCoefficient(x), 6.0, "test5");
+    CheckEquality(ct4.GetCoefficient(y), 5.0, "test6");
+    CheckEquality(ct4.Lb(), -14.0, "test7");
+    CheckEquality(ct4.Ub(), -14.0, "test8");
+    MPConstraint ct5 = solver.Add(x + (2 * y + x + 3) * 3 == 1);
+    CheckEquality(ct5.GetCoefficient(x), 4.0, "test9");
+    CheckEquality(ct5.GetCoefficient(y), 6.0, "test10");
+    CheckEquality(ct5.Lb(), -8.0, "test11");
+    CheckEquality(ct5.Ub(), -8.0, "test12");
   }
 
   static void Main()
   {
     TestVarOperator();
+    TestVarAddition();
+    TestVarMultiplication();
   }
 }
