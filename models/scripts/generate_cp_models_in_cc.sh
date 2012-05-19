ea872867683ab6698e53466d4114d4f9f223360b
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-05-19 15:57:52 +0000

    fix generate model script

--- models/scripts/generate_cp_models_in_cc.sh
+++ models/scripts/generate_cp_models_in_cc.sh
@@ -6,31 +6,31 @@ for i in 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 do
   name=costas_array_hard_$i
   echo $name
-  ./costas_array --minsize=$i --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/costas_array --minsize=$i --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for i in 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 do
   name=costas_array_soft_$i
   echo $name
-  ./costas_array --minsize=$i --cp_export_file=/tmp/$name --cp_no_solve --soft_constraints
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/costas_array --minsize=$i --cp_export_file=/tmp/$name --cp_no_solve --soft_constraints
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for name in cryptarithm tsp cvrptw
 do
   echo $name
-  ./$name --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/$name --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for i in 4 5 6 7 8 9 10 11 12 13
 do
   name=golomb_$i
   echo $name
-  ./golomb --size=$i --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/golomb --size=$i --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for data_file in data/jobshop/*
@@ -38,16 +38,16 @@ do
   data=${data_file##*/}
   name=jobshop_$data
   echo $name
-  ./jobshop --data_file=$data_file --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/jobshop --data_file=$data_file --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for i in 4 5 6 7 8 9 10 11 12 13 15 18 20 25 30 35 40 50
 do
   name=magic_square_$i
   echo $name
-  ./magic_square --size=$i --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/magic_square --size=$i --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for data_file in data/multidim_knapsack/*
@@ -55,16 +55,16 @@ do
   data=${data_file##*/}
   name=multidim_knapsack_$data
   echo $name
-  ./multidim_knapsack --data_file=$data_file --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/multidim_knapsack --data_file=$data_file --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for i in 4 5 6 7 8 9 10 15 20 25 30 40 50 100 150 200 250 300 500 750 1000
 do
   name=nqueen_$i
   echo $name
-  ./nqueens --size=$i --cp_export_file=/tmp/$name --cp_no_solve
-  ./model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
+  ./bin/nqueens --size=$i --cp_export_file=/tmp/$name --cp_no_solve
+  ./bin/model_util --input=/tmp/$name --rename_model=$name --insert_license=models/headers/google_license.txt --output=models/$name.cp
 done
 
 for clients in 0 10 20
@@ -84,7 +84,7 @@ do
       backbone_degree=4
       name=network_design_clients${clients}_backbones${backbones}_demands${demands}_traffic${traffic_min}-${traffic_max}_capacity${capacity}_client_degree${client_degree}_backbone_degree${backbone_degree}_seed${seed}
       echo $name
-      ./network_routing \
+      ./bin/network_routing \
         --clients=${clients}\
         --backbones=${backbones}\
         --demands=${demands}\
@@ -98,7 +98,7 @@ do
         --seed=${seed}\
         --cp_export_file=/tmp/$name\
         --time_limit=1
-      ./model_util \
+      ./bin/model_util \
         --input=/tmp/$name\
          --rename_model=$name\
         --insert_license=models/headers/google_license.txt\
