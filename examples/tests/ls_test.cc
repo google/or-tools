fcff53987b8c18139cc4828645ee1c6e8bf80386
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-07-26 05:26:46 +0000

    more example ls with filter

--- examples/tests/ls_test.cc
+++ examples/tests/ls_test.cc
@@ -81,11 +81,77 @@ class MoveOneVar: public IntVarLocalSearchOperator {
   bool move_up_;
 };
 
+class SumFilter : public IntVarLocalSearchFilter {
+ public:
+  SumFilter(const IntVar* const* vars, int size) :
+      IntVarLocalSearchFilter(vars, size), sum_(0) {}
+
+  ~SumFilter() {}
+
+  virtual void OnSynchronize() {
+    sum_ = 0;
+    for (int index = 0; index < Size(); ++index) {
+      sum_ += Value(index);
+    }
+  }
+
+  virtual bool Accept(const Assignment* delta,
+                      const Assignment* unused_deltadelta) {
+    const Assignment::IntContainer& solution_delta = delta->IntVarContainer();
+    const int solution_delta_size = solution_delta.Size();
+
+    // The input const Assignment* delta given to Accept() may
+    // actually contain "Deactivated" elements, which represent
+    // variables that have been freed -- they are not bound to a
+    // single value anymore. This happens with LNS-type (Large
+    // Neighborhood Search) LocalSearchOperator, which are not used in
+    // this example as of 2012-01; and we refer the reader to
+    // ./routing.cc for an example of such LNS-type operators.
+    //
+    // For didactical purposes, we will assume for a moment that a
+    // LNS-type operator might be applied. The Filter will still be
+    // called, but our filter here won't be able to work, since
+    // it needs every variable to be bound (i.e. have a fixed value),
+    // in the assignment that it considers. Therefore, we include here
+    // a snippet of code that will detect if the input assignment is
+    // not fully bound. For further details, read ./routing.cc -- but
+    // we strongly advise the reader to first try and understand all
+    // of this file.
+    for (int i = 0; i < solution_delta_size; ++i) {
+      if (!solution_delta.Element(i).Activated()) {
+        VLOG(1)
+            << "Element #" << i << " of the delta assignment given to"
+            << " SumFilter::Accept() is not activated (i.e. its variable"
+            << " is not bound to a single value anymore). This means that"
+            << " we are in a LNS phase, and the DobbleFilter won't be able"
+            << " to filter anything. Returning true.";
+        return true;
+      }
+    }
+    int64 new_sum = sum_;
+    VLOG(1) << "No LNS, size = " << solution_delta_size;
+    for (int index = 0; index < solution_delta_size; ++index) {
+      int64 touched_var = -1;
+      FindIndex(solution_delta.Element(index).Var(), &touched_var);
+      const int64 old_value = Value(touched_var);
+      const int64 new_value = solution_delta.Element(index).Value();
+      new_sum += new_value - old_value;
+    }
+    VLOG(1) << "new sum = " << new_sum << ", old sum = " << sum_;
+    return new_sum < sum_;
+  }
+
+ private:
+  int64 sum_;
+};
+
+
 
 void BasicLns() {
+  LOG(INFO) << "Basic LNS";
   Solver s("Sample");
   vector<IntVar*> vars;
-  s.MakeIntVarArray(10, 0, 10, &vars);
+  s.MakeIntVarArray(4, 0, 4, &vars);
   IntVar* const sum_var = s.MakeSum(vars)->Var();
   OptimizeVar* const obj = s.MakeMinimize(sum_var, 1);
   DecisionBuilder* const db =
@@ -99,15 +165,16 @@ void BasicLns() {
   SolutionCollector* const collector = s.MakeLastSolutionCollector();
   collector->Add(vars);
   collector->AddObjective(sum_var);
-  SearchMonitor* const log = s.MakeSearchLog(100, obj);
+  SearchMonitor* const log = s.MakeSearchLog(1000, obj);
   s.Solve(ls, collector, obj, log);
   LOG(INFO) << "Objective value = " << collector->objective_value(0);
 }
 
 void BasicLs() {
+  LOG(INFO) << "Basic LS";
   Solver s("Sample");
   vector<IntVar*> vars;
-  s.MakeIntVarArray(10, 0, 10, &vars);
+  s.MakeIntVarArray(4, 0, 4, &vars);
   IntVar* const sum_var = s.MakeSum(vars)->Var();
   OptimizeVar* const obj = s.MakeMinimize(sum_var, 1);
   DecisionBuilder* const db =
@@ -121,7 +188,33 @@ void BasicLs() {
   SolutionCollector* const collector = s.MakeLastSolutionCollector();
   collector->Add(vars);
   collector->AddObjective(sum_var);
-  SearchMonitor* const log = s.MakeSearchLog(100, obj);
+  SearchMonitor* const log = s.MakeSearchLog(1000, obj);
+  s.Solve(ls, collector, obj, log);
+  LOG(INFO) << "Objective value = " << collector->objective_value(0);
+}
+
+void BasicLsWithFilter() {
+  LOG(INFO) << "Basic LS with Filter";
+  Solver s("Sample");
+  vector<IntVar*> vars;
+  s.MakeIntVarArray(4, 0, 4, &vars);
+  IntVar* const sum_var = s.MakeSum(vars)->Var();
+  OptimizeVar* const obj = s.MakeMinimize(sum_var, 1);
+  DecisionBuilder* const db =
+      s.MakePhase(vars,
+                  Solver::CHOOSE_FIRST_UNBOUND,
+                  Solver::ASSIGN_MAX_VALUE);
+  MoveOneVar one_var_ls(vars);
+  std::vector<LocalSearchFilter*> filters;
+  filters.push_back(s.RevAlloc(new SumFilter(vars.data(), vars.size())));
+
+  LocalSearchPhaseParameters* const ls_params =
+      s.MakeLocalSearchPhaseParameters(&one_var_ls, db, NULL, filters);
+  DecisionBuilder* const ls = s.MakeLocalSearchPhase(vars, db, ls_params);
+  SolutionCollector* const collector = s.MakeLastSolutionCollector();
+  collector->Add(vars);
+  collector->AddObjective(sum_var);
+  SearchMonitor* const log = s.MakeSearchLog(1000, obj);
   s.Solve(ls, collector, obj, log);
   LOG(INFO) << "Objective value = " << collector->objective_value(0);
 }
@@ -131,6 +224,7 @@ int main(int argc, char** argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   operations_research::BasicLns();
   operations_research::BasicLs();
+  operations_research::BasicLsWithFilter();
   return 0;
 }
 
