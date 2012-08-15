13b8a6468ac07948c43c299821bbf20a7c1bcdd8
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-08-15 18:30:08 +0000

    offer extensibility for the linear solver

---
+++ src/linear_solver/linear_solver_ext.h
@@ -0,0 +1,41 @@
+// Copyright 2010-2012 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef OR_TOOLS_LINEAR_SOLVER_LINEAR_SOLVER_EXT_H_
+#define OR_TOOLS_LINEAR_SOLVER_LINEAR_SOLVER_EXT_H_
+
+#include "base/hash.h"
+#include "base/hash.h"
+#include <limits>
+#include <string>
+#include <vector>
+
+#include "base/commandlineflags.h"
+#include "base/integral_types.h"
+#include "base/logging.h"
+#include "base/macros.h"
+#include "base/scoped_ptr.h"
+#include "base/timer.h"
+#include "base/strutil.h"
+#include "base/sparsetable.h"
+#include "base/hash.h"
+#include "linear_solver/linear_solver.h"
+
+using std::string;
+
+namespace operations_research {
+// Special API
+
+}  // namespace operations_research
+
+#endif  // OR_TOOLS_LINEAR_SOLVER_LINEAR_SOLVER_EXT_H_
