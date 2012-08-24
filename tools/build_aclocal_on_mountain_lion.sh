5fbf46902a7dbfd88725c50a4ce21530789bd0fb
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-08-24 01:37:51 +0000

    improve build autotool script

--- tools/build_aclocal_on_mountain_lion.sh
+++ tools/build_aclocal_on_mountain_lion.sh
@@ -1,27 +1,31 @@
 export build=`pwd`/dependencies/install
 export source=`pwd`/dependencies/sources
+export archive=`pwd`/dependencies/archives
 mkdir -p $build
 
-cd $source
+cd $archive
 curl -OL http://ftpmirror.gnu.org/autoconf/autoconf-2.68.tar.gz
-tar xzf autoconf-2.68.tar.gz
+cd $source
+tar xzf $archive/autoconf-2.68.tar.gz
 cd autoconf-2.68
 ./configure --prefix=$build
 make
 make install
 export PATH=$PATH:$build/bin
 
-cd $source
+cd $archive
 curl -OL http://ftpmirror.gnu.org/automake/automake-1.11.tar.gz
-tar xzf automake-1.11.tar.gz
+cd $source
+tar xzf $archive/automake-1.11.tar.gz
 cd automake-1.11
 ./configure --prefix=$build
 make
 make install
 
-cd $source
+cd $archive
 curl -OL http://ftpmirror.gnu.org/libtool/libtool-2.4.tar.gz
-tar xzf libtool-2.4.tar.gz
+cd $source
+tar xzf $archive/libtool-2.4.tar.gz
 cd libtool-2.4
 ./configure --prefix=$build
 make
