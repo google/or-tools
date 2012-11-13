3241df4a89471b19f35a8c0cdd00e16960f35709
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2012-11-13 01:25:09 +0000

    fix

--- examples/tests/cpp11_test.cc
+++ examples/tests/cpp11_test.cc
@@ -2,31 +2,33 @@
 #include <memory>
 #include <vector>
 
-// struct Foo {
-//   Foo() { std::cout << "Foo::Foo\n"; }
-//   ~Foo() { std::cout << "Foo::~Foo\n"; }
-//   void bar() { std::cout << "Foo::bar\n"; }
-// };
+#if defined(__linux__)
+struct Foo {
+  Foo() { std::cout << "Foo::Foo\n"; }
+  ~Foo() { std::cout << "Foo::~Foo\n"; }
+  void bar() { std::cout << "Foo::bar\n"; }
+};
 
-// void f(const Foo &foo) {
-//   std::cout << "f(const Foo&)\n";
-// }
+void f(const Foo &foo) {
+  std::cout << "f(const Foo&)\n";
+}
 
-// void test_unique() {
-//   std::unique_ptr<Foo> p1(new Foo);  // p1 owns Foo
-//   if (p1) p1->bar();
+void test_unique() {
+  std::unique_ptr<Foo> p1(new Foo);  // p1 owns Foo
+  if (p1) p1->bar();
 
-//   {
-//     std::unique_ptr<Foo> p2(std::move(p1));  // now p2 owns Foo
-//     f(*p2);
+  {
+    std::unique_ptr<Foo> p2(std::move(p1));  // now p2 owns Foo
+    f(*p2);
 
-//     p1 = std::move(p2);  // ownership returns to p1
-//     std::cout << "destroying p2...\n";
-//   }
+    p1 = std::move(p2);  // ownership returns to p1
+    std::cout << "destroying p2...\n";
+  }
 
-//   if (p1) p1->bar();
-//   // Foo instance is destroyed when p1 goes out of scope
-// }
+  if (p1) p1->bar();
+  // Foo instance is destroyed when p1 goes out of scope
+}
+#endif
 
 void test_auto() {
   std::vector<int> numbers;
@@ -43,7 +45,9 @@ void test_auto() {
 }
 
 int main() {
-  //  test_unique();
+#if defined(__linux__)
+  test_unique();
+#endif
   test_auto();
   return 0;
 }
