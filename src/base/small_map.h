64f44abd849aa9ee1cc7a8927470b0ff8307d967
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-01-10 19:01:19 +0000

    missing files

---
+++ src/base/small_map.h
@@ -0,0 +1,8 @@
+#ifndef OR_TOOLS_BASE_SMALL_MAP_H_
+#define OR_TOOLS_BASE_SMALL_MAP_H_
+
+template<class T>
+class small_map : public T {
+};
+
+#endif  // OR_TOOLS_BASE_SMALL_MAP_H_
---
+++ src/base/sparse_hash.h
@@ -0,0 +1,17 @@
+// An all-in one inclusion (for simplicity) of the {dense,sparse}_hash_{map,set}
+// files in the Google sparsehash open-source library.
+
+#ifndef OR_TOOLS_BASE_SPARSE_HASH_H_
+#define OR_TOOLS_BASE_SPARSE_HASH_H_
+
+#include <google/dense_hash_map>
+#include <google/dense_hash_set>
+#include <google/sparse_hash_map>
+#include <google/sparse_hash_set>
+
+using google::dense_hash_map;
+using google::dense_hash_set;
+using google::sparse_hash_map;
+using google::sparse_hash_set;
+
+#endif  // OR_TOOLS_BASE_SPARSE_HASH_H_
