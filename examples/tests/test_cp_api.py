798d3fc2c375b273e6ed18dbad7a95a798025b5d
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-04-14 20:21:30 +0000

    modulo API on python

--- examples/tests/test_cp_api.py
+++ examples/tests/test_cp_api.py
@@ -13,10 +13,17 @@ def test_sparse_var():
   x = solver.IntVar([1, 3, 5], 'x')
   print x
 
+def test_modulo():
+  solver = pywrapcp.Solver('test modulo')
+  x = solver.IntVar(0, 10, 'x')
+  y = solver.IntVar(2, 4, 'y')
+  print x % 3
+  print x % y
 
 def main():
   test_member()
   test_sparse_var()
+  test_modulo()
 
 
 if __name__ == '__main__':
