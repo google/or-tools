36d87d01cf1dac4a3c1834ec1b510029835a231f
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-04-15 07:10:25 +0000

    modulo API on python

--- examples/tests/test_cp_api.py
+++ examples/tests/test_cp_api.py
@@ -1,12 +1,12 @@
-
+# Various calls to CP api from python to verify they work.
 from constraint_solver import pywrapcp
+
 
 def test_member():
   solver = pywrapcp.Solver('test member')
   x = solver.IntVar(1, 10, 'x')
   ct = x.Member([1, 2, 3, 5])
   print ct
-
 
 def test_sparse_var():
   solver = pywrapcp.Solver('test sparse')
@@ -19,6 +19,7 @@ def test_modulo():
   y = solver.IntVar(2, 4, 'y')
   print x % 3
   print x % y
+
 
 def main():
   test_member()
