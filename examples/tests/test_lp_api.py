6b5eb9e2fcd61edbd74af93b2139d86863e2316c
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-04-17 09:33:43 +0000

    allow linear_solver.Sum() to work without []

---
+++ examples/tests/test_lp_api.py
@@ -0,0 +1,22 @@
+from linear_solver import pywraplp
+import types
+
+def Sum(arg):
+  if type(arg) is types.GeneratorType:
+    arg = [x for x in arg]
+  sum = 0;
+  for i in arg:
+    sum += i
+  print arg, sum
+
+def test_sum_no_brackets():
+  Sum(x for x in range(10) if x % 2 == 0)
+  Sum([x for x in range(10) if x % 2 == 0])
+
+
+def main():
+  test_sum_no_brackets()
+
+
+if __name__ == '__main__':
+  main()
