81a2880a7db2bf8d295bb8c520cdece7137268f2
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-12-10 16:05:43 +0000

    missing file

---
+++ src/base/small_ordered_set.h
@@ -0,0 +1,8 @@
+#ifndef OR_TOOLS_BASE_SMALL_ORDERED_SET_H_
+#define OR_TOOLS_BASE_SMALL_ORDERED_SET_H_
+
+template<class T>
+class small_ordered_set : public T {
+};
+
+#endif  // OR_TOOLS_BASE_SMALL_ORDERED_SET_H_
