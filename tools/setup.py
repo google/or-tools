1d706684af3ce20a87727d4a0c44aee3164a326d
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2013-12-27 12:24:15 +0000

    improve pypi distribution

--- tools/setup.py
+++ tools/setup.py
@@ -7,7 +7,7 @@ from os.path import dirname
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(dirname(__file__), fname)).read()
+    return open(pjoin(dirname(__file__), fname)).read()
 
 setup(
     name='or-tools',
