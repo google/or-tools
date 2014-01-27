667d6b0087600420c333c83017e0ed60e1211016
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2014-01-27 04:57:00 +0000

    dual loading example in python

---
+++ examples/tests/dual_loading.py
@@ -0,0 +1,11 @@
+from ortools.constraint_solver import pywrapcp
+from ortools.linear_solver import pywraplp
+
+
+def main():
+  cp = pywrapcp.Solver("test")
+  lp = pywraplp.Solver("test", pywraplp.Solver.CLP_LINEAR_PROGRAMMING)
+
+
+if __name__ == '__main__':
+  main()
