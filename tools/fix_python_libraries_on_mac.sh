43fac6bb46817cc90586bee577dd5c5cf7dc2bf6
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2014-02-14 13:51:13 +0000

    fix

--- tools/fix_python_libraries_on_mac.sh
+++ tools/fix_python_libraries_on_mac.sh
@@ -1,4 +1,4 @@
-export P=`otool -L temp/ortools/lib/libortools.dylib | grep -v ':' | grep libortools | cut -d '(' -f 1`
+export P=`otool -L temp/ortools/ortools/libortools.dylib | grep -v ':' | grep libortools | cut -d '(' -f 1`
 echo path to change = $P
 install_name_tool -change $P @loader_path/../libortools.dylib temp/ortools/ortools/algorithms/_pywrapknapsack_solver.so
 install_name_tool -change $P @loader_path/../libortools.dylib temp/ortools/ortools/constraint_solver/_pywrapcp.so
