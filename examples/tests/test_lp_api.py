1adfb3db7e9290bfd226eb49464ebbd06040e01b
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2014-02-19 18:53:31 +0000

    fix proto as input in python

--- examples/tests/test_lp_api.py
+++ examples/tests/test_lp_api.py
@@ -1,4 +1,7 @@
-from linear_solver import pywraplp
+from google.protobuf import text_format
+from ortools.linear_solver import pywraplp
+from ortools.linear_solver import linear_solver2_pb2
+import sys
 import types
 
 def Sum(arg):
@@ -13,9 +16,40 @@ def test_sum_no_brackets():
   Sum(x for x in range(10) if x % 2 == 0)
   Sum([x for x in range(10) if x % 2 == 0])
 
+text_model = """
+solver_type:CBC_MIXED_INTEGER_PROGRAMMING
+model <
+  maximize:true
+  variable < lower_bound:1 upper_bound:10 objective_coefficient:2 >
+  variable < lower_bound:1 upper_bound:10 objective_coefficient:1 >
+  constraint < lower_bound:-10000 upper_bound:4
+    var_index:0 coefficient:1
+    var_index:1 coefficient:2
+    linear_term < var_index:0 coefficient:1 >
+    linear_term < var_index:1 coefficient:2 >
+  >
+>
+"""
+
+def test_proto():
+  input_proto = linear_solver2_pb2.MPModelRequest()
+  text_format.Merge(text_model, input_proto)
+  solver = pywraplp.Solver('solveFromProto',
+                           pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+  print input_proto
+  # For now, create the model from the proto by parsing the proto
+  solver.LoadModelFromProto(input_proto.model)
+  solver.EnableOutput();
+  solver.Solve()
+  # Fill solution
+  solution = linear_solver2_pb2.MPSolutionResponse()
+  solver.FillSolutionResponseProto(solution)
+  print solution
+
 
 def main():
   test_sum_no_brackets()
+  test_proto()
 
 
 if __name__ == '__main__':
