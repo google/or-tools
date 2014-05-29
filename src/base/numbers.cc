40912e2d299f46c39eaa554b515772e678ae9648
laurent.perron@gmail.com <laurent.perron@gmail.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2014-05-29 19:32:39 +0000

    fix for visual studio 2012

--- src/base/numbers.cc
+++ src/base/numbers.cc
@@ -57,7 +57,11 @@ bool safe_strtod(const std::string& str, double* value) {
 bool safe_strto64(const std::string& str, int64* value) {
   if (str.empty()) return false;
   char* endptr;
+#if defined(_MSV_VER) 
+  *value = _strtoll(str.c_str(), &endptr, /*base=*/10);  // NOLINT
+#else
   *value = strtoll(str.c_str(), &endptr, /*base=*/10);  // NOLINT
+#endif
   return *endptr == '\0' && str[0] != '\0';
 }
 }  // namespace operations_research
