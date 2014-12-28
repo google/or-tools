dc6e9bfb56c89274c83b711b82cbb706191b1d9c
lperron@google.com <lperron@google.com@59fed7e4-672f-1a80-1451-5ac2ff6d83f1>
2014-12-28 10:03:40 +0000

    cleanup old flatzinc2 references

--- makefiles/Makefile.cpp.mk
+++ makefiles/Makefile.cpp.mk
@@ -351,7 +351,9 @@ clean_cc:
 	-$(DEL) $(OBJ_DIR)$Sconstraint_solver$S*.$O
 	-$(DEL) $(OBJ_DIR)$Slinear_solver$S*.$O
 	-$(DEL) $(OBJ_DIR)$Sutil$S*.$O
+	-$(DEL) $(BIN_DIR)$Sold_fz$E
 	-$(DEL) $(BIN_DIR)$Sfz$E
+	-$(DEL) $(BIN_DIR)$Sfz2$E
 	-$(DEL) $(BIN_DIR)$Ssat_runner$E
 	-$(DEL) $(CPBINARIES)
 	-$(DEL) $(LPBINARIES)
