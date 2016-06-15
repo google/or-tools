a05a94bf4e866690bd998f6ab9922e6a66443ca7
Laurent Perron <lperron@google.com>
2016-06-15 16:27:24 +0200

    fix for ubuntu16.04

--- src/util/fp_utils.h
+++ src/util/fp_utils.h
@@ -45,7 +45,7 @@
 #if defined(_MSC_VER)
 static inline double isnan(double value) { return _isnan(value); }
 static inline double round(double value) { return floor(value + 0.5); }
-#elif defined(__APPLE__)
+#elif defined(__APPLE__) || __GNUC_MAJOR == 3
 using std::isnan;
 #endif
 
