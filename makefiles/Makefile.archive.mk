3e923d73f02a823737b64a7a8f02b7a5c78f3b7d
Laurent Perron <lperron@google.com>
2016-07-03 15:29:03 +0200

    INSTALL_PORT -> PORT in makefiles/Makefile.archive.mk

--- makefiles/Makefile.archive.mk
+++ makefiles/Makefile.archive.mk
@@ -1,9 +1,9 @@
 
 archive: $(LIB_DIR)$S$(LIB_PREFIX)ortools.$(LIB_SUFFIX) csharp java create_dirs cc_archive dotnet_archive java_archive
 ifeq "$(SYSTEM)" "win"
-	cd temp && ..$Stools$Szip.exe -r ..$SGoogle.OrTools.$(INSTALL_PORT)-$(OR_TOOLS_VERSION).zip $(INSTALL_DIR)
+	cd temp && ..$Stools$Szip.exe -r ..$SGoogle.OrTools.$(PORT)-$(OR_TOOLS_VERSION).zip $(INSTALL_DIR)
 else
-	cd temp && tar -c -v -z --no-same-owner -f ..$SGoogle.OrTools.$(INSTALL_PORT)-$(OR_TOOLS_VERSION).tar.gz $(INSTALL_DIR)
+	cd temp && tar -c -v -z --no-same-owner -f ..$SGoogle.OrTools.$(PORT)-$(OR_TOOLS_VERSION).tar.gz $(INSTALL_DIR)
 endif
 	-$(DELREC) temp
 
@@ -151,7 +151,7 @@ fz_archive: fz
 	$(COPY) LICENSE-2.0.txt temp$S$(INSTALL_DIR)
 	$(COPY) bin$Sfz.exe temp$S$(INSTALL_DIR)$Sbin$Sfzn-or-tools.exe
 	$(COPY) src$Sflatzinc$Smznlib$S*.mzn temp$S$(INSTALL_DIR)$Sshare$Sminizinc
-	cd temp && ..$Stools$Szip.exe -r ..$SGoogle.OrTools.flatzinc.$(INSTALL_PORT)-$(OR_TOOLS_VERSION).zip $(INSTALL_DIR)
+	cd temp && ..$Stools$Szip.exe -r ..$SGoogle.OrTools.flatzinc.$(PORT)-$(OR_TOOLS_VERSION).zip $(INSTALL_DIR)
 	-$(DELREC) temp
 else
 fz_archive: $(LIB_DIR)$S$(LIB_PREFIX)ortools.$(LIB_SUFFIX)
@@ -164,6 +164,6 @@ fz_archive: $(LIB_DIR)$S$(LIB_PREFIX)ortools.$(LIB_SUFFIX)
 	$(COPY) LICENSE-2.0.txt temp$S$(INSTALL_DIR)
 	$(COPY) bin$Sfz temp$S$(INSTALL_DIR)$Sbin$Sfzn-or-tools
 	$(COPY) src$Sflatzinc$Smznlib$S* temp$S$(INSTALL_DIR)$Sshare$Sminizinc
-	cd temp && tar cvzf ..$SGoogle.OrTools.flatzinc.$(INSTALL_PORT)-$(OR_TOOLS_VERSION).tar.gz $(INSTALL_DIR)
+	cd temp && tar cvzf ..$SGoogle.OrTools.flatzinc.$(PORT)-$(OR_TOOLS_VERSION).tar.gz $(INSTALL_DIR)
 	-$(DELREC) temp
 endif
