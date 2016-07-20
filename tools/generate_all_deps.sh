bc11bc2f03dbabd08a1172d802a8afe7986cb02f
Laurent Perron <lperron@google.com>
2016-07-20 08:32:50 -0700

    reorder generate_all_deps.sh

--- tools/generate_all_deps.sh
+++ tools/generate_all_deps.sh
@@ -1,10 +1,10 @@
 tools/generate_deps.sh BASE base
 tools/generate_deps.sh UTIL util base
 tools/generate_deps.sh LP_DATA lp_data util base algorithms
-tools/generate_deps.sh GLOP glop util base linear_solver lp_data
+tools/generate_deps.sh GLOP glop util base lp_data linear_solver
 tools/generate_deps.sh GRAPH graph base util
 tools/generate_deps.sh ALGORITHMS algorithms base util graph linear_solver
-tools/generate_deps.sh SAT sat base util algorithms graph linear_solver lp_data glop
-tools/generate_deps.sh BOP bop base util glop lp_data sat
-tools/generate_deps.sh LP linear_solver base util glop lp_data bop
+tools/generate_deps.sh SAT sat base util algorithms graph lp_data glop linear_solver
+tools/generate_deps.sh BOP bop base util lp_data glop sat
+tools/generate_deps.sh LP linear_solver base util lp_data glop bop
 tools/generate_deps.sh CP constraint_solver base util graph linear_solver sat
