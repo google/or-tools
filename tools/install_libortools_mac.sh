63cbf29cad4032ccfe684f27af4aaf564c136f91
Laurent Perron <lperron@google.com>
2016-07-22 15:03:49 -0700

    fix install_libortools_mac.sh

--- tools/install_libortools_mac.sh
+++ tools/install_libortools_mac.sh
@@ -5,12 +5,12 @@ export N=`pwd`/lib
 export B=`pwd`/bin/csharp
 export O=`otool -L lib/libortools.dylib | grep -v ':' | grep libortools | cut -d '(' -f 1`
 echo install library in path $N
-install_name_tool -id $B/libGoogle.OrTools.so bin/libGoogle.OrTools.so
-install_name_tool -change $O $N/libortools.dylib bin/libGoogle.OrTools.so
-install_name_tool -id $N/libcvrptw_lib.dylib lib/libcvrptw_lib.dylib
-install_name_tool -id $N/libdimacs.dylib lib/libdimacs.dylib
-install_name_tool -id $N/libjniortools.jnilib lib/libjniortools.jnilib
-install_name_tool -change $O $N/libortools.dylib lib/libjniortools.jnilib
-install_name_tool -id $N/libortools.dylib lib/libortools.dylib
+install_name_tool -id $B/libGoogle.OrTools.so $B/libGoogle.OrTools.so
+install_name_tool -change $O $N/libortools.dylib $B/libGoogle.OrTools.so
+install_name_tool -id $N/libcvrptw_lib.dylib $N/libcvrptw_lib.dylib
+install_name_tool -id $N/libdimacs.dylib $N/libdimacs.dylib
+install_name_tool -id $N/libjniortools.jnilib $N/libjniortools.jnilib
+install_name_tool -change $O $N/libortools.dylib $N/libjniortools.jnilib
+install_name_tool -id $N/libortools.dylib $N/libortools.dylib
 export P=`otool -L lib/libortools.dylib | grep -v ':' | grep libortools | cut -d '(' -f 1`
 echo the new path of the or-tools library is $P
