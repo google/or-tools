9fbe3e2e33ee5cca2f9330e95ab42a0f81fc805d
Amit Prakash Ambasta <amit.ambasta@delhivery.com>
2017-05-31 18:50:57 +0530

    Added cbc with fallback to locally compiled

--- cmake/external/cbc.cmake
+++ cmake/external/cbc.cmake
@@ -1,27 +1,40 @@
-SET(CBC_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/protobuf/src/protobuf/src)
-SET(CBC_URL https://github.com/google/protobuf)
+SET(CoinUtils_VERSION 2.10.13)
+SET(Osi_VERSION 0.107.8)
+SET(Clp_VERSION 1.16.10)
+SET(Cgl_VERSION 0.59.9)
 
-IF(WIN32)
-    SET(CBC_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/protobuf/src/protobuf/${CMAKE_BUILD_TYPE}/libcbc.lib)
-ELSE()
-    SET(CBC_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/protobuf/src/protobuf/libprotobuf.a)
-ENDIF()
+SET(CoinUtils_URL https://github.com/coin-or/CoinUtils.git)
+SET(Osi_URL https://github.com/coin-or/Osi.git)
+SET(Clp_URL https://github.com/coin-or/Clp.git)
+SET(Cgl_URL https://github.com/coin-or/Cgl.git)
+SET(Cbc_URL https://github.com/coin-or/Cbc.git)
 
-ExternalProject_Add(Cbc
-        PREFIX Cbc
-        GIT_REPOSITORY ${CBC_URL}
-        GIT_TAG "v${CBC_VERSION}"
-        DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
-        BUILD_IN_SOURCE 1
-        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/protobuf/src/protobuf
-        CONFIGURE_COMMAND ${CMAKE_COMMAND} cmake/
-        -Dprotobuf_BUILD_TESTS=OFF
-        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        ${Protobuf_ADDITIONAL_CMAKE_OPTIONS}
-        INSTALL_COMMAND ""
-        CMAKE_CACHE_ARGS
-        -DCMAKE_BUILD_TYPE:STRING=Release
-        -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
-        -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
+FOREACH(COIN_PROJECT CoinUtils Osi Clp Cgl Cbc)
+    IF(WIN32)
+        SET(${COIN_PROJECT}_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/install/lib/lib${COIN_PROJECT}.lib)
+        SET(${COIN_PROJECT}_ADDITIONAL_CMAKE_OPTIONS --enable-msvc)
+    ELSE()
+        SET(${COIN_PROJECT}_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/install/lib/lib${COIN_PROJECT}.a)
+    ENDIF()
 
-LIST(APPEND ${PROJECT_NAME}externalTargets Protobuf)
+    SET(${COIN_PROJECT}_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/install/include)
+
+    ExternalProject_Add(${COIN_PROJECT}
+            PREFIX ${COIN_PROJECT}
+            GIT_REPOSITORY ${${COIN_PROJECT}_URL}
+            GIT_TAG "releases/${${COIN_PROJECT}_VERSION}"
+            DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
+            BUILD_IN_SOURCE 1
+            CONFIGURE_COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${COIN_PROJECT}/src/${COIN_PROJECT}/configure --enable-static --prefix=${CMAKE_CURRENT_BINARY_DIR}/install CXXFLAGS=${CMAKE_CXX_FLAGS})
+ENDFOREACH()
+
+FOREACH(COIN_PROJECT CoinUtils Osi Clp Cgl)
+    LIST(APPEND Cbc_LIBRARIES ${${COIN_PROJECT}_LIBRARIES})
+ENDFOREACH()
+
+ADD_DEPENDENCIES(Osi CoinUtils)
+ADD_DEPENDENCIES(Clp CoinUtils Osi)
+ADD_DEPENDENCIES(Cgl CoinUtils Osi Clp)
+ADD_DEPENDENCIES(Cbc CoinUtils Osi Clp Cgl)
+
+LIST(APPEND ${PROJECT_NAME}externalTargets Cbc)
