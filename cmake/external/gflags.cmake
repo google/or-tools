e833d3b90fda35a3e2be307f0667a2cd652be4ea
Amit Prakash Ambasta <amit.ambasta@delhivery.com>
2017-06-06 19:00:30 +0530

    Gflags is reflected as gflags in the project CMakeConfig

--- cmake/external/gflags.cmake
+++ cmake/external/gflags.cmake
@@ -1,10 +1,10 @@
-SET(Gflags_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/gflags_project/src/gflags/include/)
-SET(Gflags_URL https://github.com/gflags/gflags)
+SET(gflags_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/gflags_project/src/gflags/include/)
+SET(gflags_URL https://github.com/gflags/gflags)
 
-ExternalProject_Add(Gflags_project
-        PREFIX Gflags
-        GIT_REPOSITORY ${Gflags_URL}
-        GIT_TAG "v${Gflags_VERSION}"
+ExternalProject_Add(gflags_project
+        PREFIX gflags
+        GIT_REPOSITORY ${gflags_URL}
+        GIT_TAG "v${gflags_VERSION}"
         DOWNLOAD_DIR "${DOWNLOAD_LOCATION}"
         UPDATE_COMMAND ""
         BUILD_IN_SOURCE 1
@@ -13,14 +13,14 @@ ExternalProject_Add(Gflags_project
         -DBUILD_STATIC_LIBS=ON
         -DBUILD_TESTING=OFF
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-        ${Gflags_ADDITIONAL_CMAKE_OPTIONS}
+        ${gflags_ADDITIONAL_CMAKE_OPTIONS}
         INSTALL_COMMAND ""
         CMAKE_CACHE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
         -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
 
-ADD_LIBRARY(Gflags STATIC IMPORTED) 
-SET_PROPERTY(TARGET Gflags PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/gflags_project/src/gflags/lib/libgflags.a)
-SET(Gflags_LIBRARIES "")
-LIST(APPEND Gflags_LIBRARIES Gflags)
+ADD_LIBRARY(gflags STATIC IMPORTED) 
+SET_PROPERTY(TARGET gflags PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/gflags_project/src/gflags/lib/libgflags.a)
+SET(gflags_LIBRARIES "")
+LIST(APPEND gflags_LIBRARIES gflags)
