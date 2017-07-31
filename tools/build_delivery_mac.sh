12b5ac178e84043750a1d4b8f769da0bd9e86356
Laurent Perron <lperron@google.com>
2017-07-31 12:06:19 -0700

    fix docker targets

--- tools/build_delivery_mac.sh
+++ tools/build_delivery_mac.sh
@@ -3,18 +3,18 @@
 echo Cleaning or-tools
 make clean
 echo Builing all libraries
-make all fz -j 5
+make all fz -j 5 UNIX_PYTHON_VER=2.7
 echo Running tests
-make test
+make test UNIX_PYTHON_VER=2.7
 echo Creating standard artifacts.
-make python_examples_archive archive fz_archive pypi_upload
+make python_examples_archive archive fz_archive pypi_upload UNIX_PYTHON_VER=2.7
 echo Rebuilding for python 3.5
-make clean_python
+make clean_python UNIX_PYTHON_VER=2.7
 make python UNIX_PYTHON_VER=3.5 -j 5
 make test_python UNIX_PYTHON_VER=3.5
 make pypi_upload UNIX_PYTHON_VER=3.5
 echo Rebuilding for python 3.6
-make clean_python
+make clean_python UNIX_PYTHON_VER=3.5
 make python UNIX_PYTHON_VER=3.6 -j 5
 make test_python UNIX_PYTHON_VER=3.6
 make pypi_upload UNIX_PYTHON_VER=3.6
