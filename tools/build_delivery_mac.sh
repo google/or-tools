a2eb8279dce85aa6874a900bbc235bbbcf9830ac
Laurent Perron <lperron@google.com>
2017-10-02 16:57:02 +0200

    make the build script on mac more robust

--- tools/build_delivery_mac.sh
+++ tools/build_delivery_mac.sh
@@ -7,14 +7,17 @@ make all fz -j 5 UNIX_PYTHON_VER=2.7
 echo Running tests
 make test UNIX_PYTHON_VER=2.7
 echo Creating standard artifacts.
+rm -rf temp-python*
 make python_examples_archive archive fz_archive pypi_upload UNIX_PYTHON_VER=2.7
 make clean_python UNIX_PYTHON_VER=2.7
 echo Rebuilding for python 3.5
+rm -rf temp-python*
 make python UNIX_PYTHON_VER=3.5 -j 5
 make test_python UNIX_PYTHON_VER=3.5
 make pypi_upload UNIX_PYTHON_VER=3.5
 make clean_python UNIX_PYTHON_VER=3.5
 echo Rebuilding for python 3.6
+rm -rf temp-python*
 make python UNIX_PYTHON_VER=3.6 -j 5
 make test_python UNIX_PYTHON_VER=3.6
 make pypi_upload UNIX_PYTHON_VER=3.6
