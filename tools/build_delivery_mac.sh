09516b7900f47c41c315aa4661b6464ddabd7fb7
Laurent Perron <lperron@google.com>
2017-10-02 14:41:12 +0200

    small cleanup of the build script on mac

--- tools/build_delivery_mac.sh
+++ tools/build_delivery_mac.sh
@@ -8,13 +8,14 @@ echo Running tests
 make test UNIX_PYTHON_VER=2.7
 echo Creating standard artifacts.
 make python_examples_archive archive fz_archive pypi_upload UNIX_PYTHON_VER=2.7
-echo Rebuilding for python 3.5
 make clean_python UNIX_PYTHON_VER=2.7
+echo Rebuilding for python 3.5
 make python UNIX_PYTHON_VER=3.5 -j 5
 make test_python UNIX_PYTHON_VER=3.5
 make pypi_upload UNIX_PYTHON_VER=3.5
-echo Rebuilding for python 3.6
 make clean_python UNIX_PYTHON_VER=3.5
+echo Rebuilding for python 3.6
 make python UNIX_PYTHON_VER=3.6 -j 5
 make test_python UNIX_PYTHON_VER=3.6
 make pypi_upload UNIX_PYTHON_VER=3.6
+make clean_python UNIX_PYTHON_VER=3.6
