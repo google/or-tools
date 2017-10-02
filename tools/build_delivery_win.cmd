21e9bc0768e9686d27ebd9d736033dc69701fc48
Laurent Perron <lperron@google.com>
2017-10-02 14:55:51 +0000

    fix build script on windows

--- tools/build_delivery_win.cmd
+++ tools/build_delivery_win.cmd
@@ -6,10 +6,10 @@ echo Running tests
 tools\make.exe test
 echo Creating standard artifacts.
 tools\rm -rf temp-python27
-tools\make.exe python_examples_archive archive fz_archive
+tools\make.exe python_examples_archive archive fz_archive WINDOWS_PATH_TO_PYTHON=c:\python27-64
 set VS90COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools
-tools\make.exe pypi_upload nuget_upload
-tools\make.exe clean_python
+tools\make.exe pypi_upload nuget_upload WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make.exe clean_python WINDOWS_PATH_TO_PYTHON=c:\python27-64
 echo Rebuilding for python 3.5
 tools\rm -rf temp-python35
 tools\make.exe python WINDOWS_PATH_TO_PYTHON=c:\python35-64
