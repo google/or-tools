da82ddd1a039b55153d9cb4b71a4b3b76587bb1d
Laurent Perron <lperron@google.com>
2017-10-23 21:57:53 +0200

    fix

--- makefiles/Makefile.unix.mk
+++ makefiles/Makefile.unix.mk
@@ -142,10 +142,11 @@ ifeq ($(PLATFORM),LINUX)
   # This is needed to find libprotobuf.a
   ifeq ($(DISTRIBUTION_ID),Fedora)
     PROTOBUF_LNK = $(UNIX_PROTOBUF_DIR)/lib64/libprotobuf.a
-  elif ($(DISTRIBUTION_ID),CentOS)
-    PROTOBUF_LNK = $(UNIX_PROTOBUF_DIR)/lib64/libprotobuf.a
-  else
-    PROTOBUF_LNK = $(UNIX_PROTOBUF_DIR)/lib/libprotobuf.a
+    else ifeq ($(DISTRIBUTION_ID),CentOS)
+      PROTOBUF_LNK = $(UNIX_PROTOBUF_DIR)/lib64/libprotobuf.a
+    else
+      PROTOBUF_LNK = $(UNIX_PROTOBUF_DIR)/lib/libprotobuf.a
+    endif
   endif
   # This is needed to find libglog.a
   GLOG_LNK = $(UNIX_GLOG_DIR)/lib/libglog.a
