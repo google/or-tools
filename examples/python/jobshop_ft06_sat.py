d4a81c143c47959d13b690af69444e7e16001d84
Laurent Perron <lperron@google.com>
2017-11-20 17:04:25 +0100

    py to ipynb

--- examples/python/jobshop_ft06_sat.py
+++ examples/python/jobshop_ft06_sat.py
@@ -1,70 +1,87 @@
-from ortools.sat.python import cp_model
-
-
-def main():
-  # Creates the solver.
-  model = cp_model.CpModel()
-
-  machines_count = 6
-  jobs_count = 6
-  all_machines = range(0, machines_count)
-  all_jobs = range(0, jobs_count)
-
-  durations = [[1, 3, 6, 7, 3, 6],
-               [8, 5, 10, 10, 10, 4],
-               [5, 4, 8, 9, 1, 7],
-               [5, 5, 5, 3, 8, 9],
-               [9, 3, 5, 4, 3, 1],
-               [3, 3, 9, 10, 4, 1]]
-
-  machines = [[2, 0, 1, 3, 5, 4],
-              [1, 2, 4, 5, 0, 3],
-              [2, 3, 5, 0, 1, 4],
-              [1, 0, 2, 3, 4, 5],
-              [2, 1, 4, 5, 0, 3],
-              [1, 3, 5, 0, 4, 2]]
-
-  # Computes horizon dynamically.
-  horizon = sum([sum(durations[i]) for i in all_jobs])
-
-  # Creates jobs.
-  all_tasks = {}
-  for i in all_jobs:
-    for j in all_machines:
-      start = model.NewIntVar(0, horizon, 'start_%i_%i' % (i, j))
-      duration = durations[i][j]
-      end = model.NewIntVar(0, horizon, 'end_%i_%i' % (i, j))
-      interval = model.NewIntervalVar(start, duration, end,
-                                      'interval_%i_%i' % (i, j))
-      all_tasks[(i, j)] = (start, end, interval)
-
-  # Create disjuctive constraints.
-  machine_to_jobs = {}
-  for i in all_machines:
-    machines_jobs = []
-    for j in all_jobs:
-      for k in all_machines:
-        if machines[j][k] == i:
-          machines_jobs.append(all_tasks[(j, k)][2])
-    machine_to_jobs[i] = machines_jobs
-    model.AddNoOverlap(machines_jobs)
-
-  # Precedences inside a job.
-  for i in all_jobs:
-    for j in range(0, machines_count - 1):
-      model.Add(all_tasks[(i, j + 1)][0] >= all_tasks[(i, j)][1])
-
-  # Makespan objective.
-  obj_var = model.NewIntVar(0, horizon, 'makespan')
-  model.AddMaxEquality(
-      obj_var, [all_tasks[(i, machines_count - 1)][1] for i in all_jobs])
-  model.Minimize(obj_var)
-
-  # Solve model.
-  solver = cp_model.CpSolver()
-  response = solver.Solve(model)
-  print(solver.ObjectiveValue())
-
-
-if __name__ == '__main__':
-  main()
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {
+    "collapsed": false
+   },
+   "outputs": [],
+   "source": [
+    "from ortools.sat.python import cp_model\n",
+    "\n",
+    "\n",
+    "def main():\n",
+    "  # Creates the solver.\n",
+    "  model = cp_model.CpModel()\n",
+    "\n",
+    "  machines_count = 6\n",
+    "  jobs_count = 6\n",
+    "  all_machines = range(0, machines_count)\n",
+    "  all_jobs = range(0, jobs_count)\n",
+    "\n",
+    "  durations = [[1, 3, 6, 7, 3, 6],\n",
+    "               [8, 5, 10, 10, 10, 4],\n",
+    "               [5, 4, 8, 9, 1, 7],\n",
+    "               [5, 5, 5, 3, 8, 9],\n",
+    "               [9, 3, 5, 4, 3, 1],\n",
+    "               [3, 3, 9, 10, 4, 1]]\n",
+    "\n",
+    "  machines = [[2, 0, 1, 3, 5, 4],\n",
+    "              [1, 2, 4, 5, 0, 3],\n",
+    "              [2, 3, 5, 0, 1, 4],\n",
+    "              [1, 0, 2, 3, 4, 5],\n",
+    "              [2, 1, 4, 5, 0, 3],\n",
+    "              [1, 3, 5, 0, 4, 2]]\n",
+    "\n",
+    "  # Computes horizon dynamically.\n",
+    "  horizon = sum([sum(durations[i]) for i in all_jobs])\n",
+    "\n",
+    "  # Creates jobs.\n",
+    "  all_tasks = {}\n",
+    "  for i in all_jobs:\n",
+    "    for j in all_machines:\n",
+    "      start = model.NewIntVar(0, horizon, 'start_%i_%i' % (i, j))\n",
+    "      duration = durations[i][j]\n",
+    "      end = model.NewIntVar(0, horizon, 'end_%i_%i' % (i, j))\n",
+    "      interval = model.NewIntervalVar(start, duration, end,\n",
+    "                                      'interval_%i_%i' % (i, j))\n",
+    "      all_tasks[(i, j)] = (start, end, interval)\n",
+    "\n",
+    "  # Create disjuctive constraints.\n",
+    "  machine_to_jobs = {}\n",
+    "  for i in all_machines:\n",
+    "    machines_jobs = []\n",
+    "    for j in all_jobs:\n",
+    "      for k in all_machines:\n",
+    "        if machines[j][k] == i:\n",
+    "          machines_jobs.append(all_tasks[(j, k)][2])\n",
+    "    machine_to_jobs[i] = machines_jobs\n",
+    "    model.AddNoOverlap(machines_jobs)\n",
+    "\n",
+    "  # Precedences inside a job.\n",
+    "  for i in all_jobs:\n",
+    "    for j in range(0, machines_count - 1):\n",
+    "      model.Add(all_tasks[(i, j + 1)][0] >= all_tasks[(i, j)][1])\n",
+    "\n",
+    "  # Makespan objective.\n",
+    "  obj_var = model.NewIntVar(0, horizon, 'makespan')\n",
+    "  model.AddMaxEquality(\n",
+    "      obj_var, [all_tasks[(i, machines_count - 1)][1] for i in all_jobs])\n",
+    "  model.Minimize(obj_var)\n",
+    "\n",
+    "  # Solve model.\n",
+    "  solver = cp_model.CpSolver()\n",
+    "  response = solver.Solve(model)\n",
+    "  print(solver.ObjectiveValue())\n",
+    "\n",
+    "\n",
+    "if __name__ == '__main__':\n",
+    "  main()"
+   ]
+  }
+ ],
+ "metadata": {},
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
---
+++ tools/export_to_ipynb.py
@@ -0,0 +1,18 @@
+from nbformat import v3, v4
+import sys
+
+input = sys.argv[1]
+print('reading %s' % input)
+with open(input) as fpin:
+    text = fpin.read()
+
+nbook = v3.reads_py(text)
+nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
+
+jsonform = v4.writes(nbook) + "\n"
+output = input
+output = output.replace('.py', '.ipynb')
+output = output.replace('examples/python', 'examples/colab')
+print('writing %s' % output)
+with open(output, "w") as fpout:
+    fpout.write(jsonform)
