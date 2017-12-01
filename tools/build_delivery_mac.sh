7d86f78bf2395ee0a8eebb19c821851112fd5088
Corentin Le Molgat <corentinl@google.com>
2017-12-01 15:32:12 +0100

    rework build_delivery_* scripts

--- tools/build_delivery_mac.sh
+++ tools/build_delivery_mac.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-
 echo Cleaning or-tools
 make clean
 echo Builing all libraries
 make all fz -j 5 UNIX_PYTHON_VER=2.7
 echo Running tests
 make test UNIX_PYTHON_VER=2.7
+
 echo Creating standard artifacts.
-rm -rf temp-python*
-make python_examples_archive archive fz_archive pypi_upload UNIX_PYTHON_VER=2.7
+rm -rf temp *.tar.gz
+make archive fz_archive python_examples_archive UNIX_PYTHON_VER=2.7
 make clean_python UNIX_PYTHON_VER=2.7
+
+echo Rebuilding for python 2.7
+rm -rf temp-python2.7
+make python UNIX_PYTHON_VER=2.7 -j 5
+make test_python UNIX_PYTHON_VER=2.7
+make pypi_upload UNIX_PYTHON_VER=2.7
+make clean_python UNIX_PYTHON_VER=2.7
+
 echo Rebuilding for python 3.5
-rm -rf temp-python*
+rm -rf temp-python3.5
 make python UNIX_PYTHON_VER=3.5 -j 5
 make test_python UNIX_PYTHON_VER=3.5
 make pypi_upload UNIX_PYTHON_VER=3.5
 make clean_python UNIX_PYTHON_VER=3.5
+
 echo Rebuilding for python 3.6
-rm -rf temp-python*
+rm -rf temp-python3.6
 make python UNIX_PYTHON_VER=3.6 -j 5
 make test_python UNIX_PYTHON_VER=3.6
 make pypi_upload UNIX_PYTHON_VER=3.6
 make clean_python UNIX_PYTHON_VER=3.6
+
--- tools/build_delivery_win.cmd
+++ tools/build_delivery_win.cmd
@@ -1,24 +1,39 @@
+REM Each blocks could be triggered independently (i.e. commenting others)
+REM (e.g. want to upload only python35 artifacts)
 echo Cleaning or-tools
-tools\make.exe clean
+tools\make clean
 echo Builing all libraries
-tools\make.exe all fz
+tools\make all fz WINDOWS_PATH_TO_PYTHON=c:\python27-64
 echo Running tests
-tools\make.exe test
+tools\make test WINDOWS_PATH_TO_PYTHON=c:\python27-64
+
 echo Creating standard artifacts.
-tools\rm -rf temp-python27
-tools\make.exe python_examples_archive archive fz_archive WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\rm -rf temp *.zip
+tools\make archive fz_archive python_examples_archive WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make clean_python WINDOWS_PATH_TO_PYTHON=c:\python27-64
+
+echo Creating .NET artifacts.
 set VS90COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools
-tools\make.exe pypi_upload nuget_upload WINDOWS_PATH_TO_PYTHON=c:\python27-64
-tools\make.exe clean_python WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make nuget_upload WINDOWS_PATH_TO_PYTHON=c:\python27-64
+
+echo Rebuilding for python 2.7
+tools\rm -rf temp-python27
+tools\make python WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make test_python WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python27-64
+tools\make clean_python WINDOWS_PATH_TO_PYTHON=c:\python27-64
+
 echo Rebuilding for python 3.5
 tools\rm -rf temp-python35
-tools\make.exe python WINDOWS_PATH_TO_PYTHON=c:\python35-64
-tools\make.exe test_python WINDOWS_PATH_TO_PYTHON=c:\python35-64
-tools\make.exe pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python35-64
-tools\make.exe clean_python WINDOWS_PATH_TO_PYTHON=c:\python35-64
+tools\make python WINDOWS_PATH_TO_PYTHON=c:\python35-64
+tools\make test_python WINDOWS_PATH_TO_PYTHON=c:\python35-64
+tools\make pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python35-64
+tools\make clean_python WINDOWS_PATH_TO_PYTHON=c:\python35-64
+
 echo Rebuilding for python 3.6
 tools\rm -rf temp-python36
-tools\make.exe python WINDOWS_PATH_TO_PYTHON=c:\python36-64
-tools\make.exe test_python WINDOWS_PATH_TO_PYTHON=c:\python36-64
-tools\make.exe pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python36-64
+tools\make python WINDOWS_PATH_TO_PYTHON=c:\python36-64
+tools\make test_python WINDOWS_PATH_TO_PYTHON=c:\python36-64
+tools\make pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python36-64
 tools\make clean_python WINDOWS_PATH_TO_PYTHON=c:\python36-64
+
