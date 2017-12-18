e5e110316d841955a3ccab841cf4505ced35ab76
Laurent Perron <lperron@google.com>
2017-12-18 14:26:50 +0100

    minor optim

--- ortools/sat/cp_model_solver.cc
+++ ortools/sat/cp_model_solver.cc
@@ -123,7 +123,7 @@ VariableUsage ComputeVariableUsage(const CpModelProto& model_proto,
   std::vector<int> used_booleans;
   for (int i = 0; i < model_proto.variables_size(); ++i) {
     bool is_bool = false;
-    const auto domain = model_proto.variables(i).domain();
+    const auto& domain = model_proto.variables(i).domain();
     if (domain.Get(0) >= 0 && domain.Get(domain.size() - 1) <= 1) {
       is_bool = true;
       used_booleans.push_back(i);
