7157b3d2d6afe6eae4ab49a3f54b660499968eaa
Corentin Le Molgat <corentinl@google.com>
2018-02-21 10:12:34 +0100

    Update linux mono version
    - By default trusty only provide mono 3.2.8
    cf https://packages.ubuntu.com/trusty/mono-complete

--- .travis/install.sh
+++ .travis/install.sh
@@ -22,7 +22,11 @@ if [ "${BUILDER}" == make ];then
 				pyenv global system 3.6;
 				python3.6 -m pip install -q virtualenv wheel six;
 			elif [ "${LANGUAGE}" == csharp ];then
-				sudo apt-get -yqq install mono-complete;
+				# http://www.mono-project.com/download/stable/
+				sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF &&
+					echo "deb http://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list &&
+					sudo apt-get update -qq &&
+					sudo apt-get install -yqq mono-complete
 			fi
 		else
 			# Linux Docker Makefile build:
