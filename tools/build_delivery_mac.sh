b62e9786569609415c30653b6467eac5474dd690
Corentin Le Molgat <corentinl@google.com>
2018-02-22 09:11:14 +0100

    Update build_delivery_mac

--- tools/build_delivery_mac.sh
+++ tools/build_delivery_mac.sh
@@ -2,20 +2,25 @@
 set -x
 set -e
 
+echo Rebuilding third party...
+make clean_third_party
+make third_party UNIX_PYTHON_VER=2.7
+echo Rebuilding third party...DONE
+
 echo Rebuilding or-tools...
 make clean
-make all fz -j 5 UNIX_PYTHON_VER=2.7
+make all fz -l 1 UNIX_PYTHON_VER=2.7
 make test UNIX_PYTHON_VER=2.7
 echo Rebuilding or-tools...DONE
 
 echo Creating standard artifacts...
-rm -rf temp *.tar.gz
+rm -rf temp ./*.tar.gz
 make archive fz_archive python_examples_archive UNIX_PYTHON_VER=2.7
 echo Creating standard artifacts...DONE
 
 echo Rebuilding for Python 2.7...
 make clean_python UNIX_PYTHON_VER=2.7
-make python UNIX_PYTHON_VER=2.7 -j 5
+make python -l 1 UNIX_PYTHON_VER=2.7
 make test_python UNIX_PYTHON_VER=2.7
 make pypi_archive UNIX_PYTHON_VER=2.7
 echo Rebuilding for Python 2.7...DONE
@@ -24,7 +29,6 @@ TEMP_DIR=temp-python2.7
 VENV_DIR=${TEMP_DIR}/venv
 python2.7 -m pip install --user virtualenv
 python2.7 -m virtualenv -p python2.7 ${VENV_DIR}
-VENV_BIN=${VENV_DIR}/bin/python
 # Bug: setup.py must be run in this directory !
 (cd ${TEMP_DIR}/ortools && ../venv/bin/python setup.py install)
 cp test.py.in ${TEMP_DIR}/venv/test.py
@@ -32,7 +36,7 @@ echo Creating Python 2.7 venv...DONE
 
 echo Rebuilding for Python 3.5
 make clean_python UNIX_PYTHON_VER=3.5
-make python UNIX_PYTHON_VER=3.5 -j 5
+make python -l 1 UNIX_PYTHON_VER=3.5
 make test_python UNIX_PYTHON_VER=3.5
 make pypi_archive UNIX_PYTHON_VER=3.5
 echo Rebuilding for Python 3.5...DONE
@@ -41,7 +45,6 @@ TEMP_DIR=temp-python3.5
 VENV_DIR=${TEMP_DIR}/venv
 python3.5 -m pip install --user virtualenv
 python3.5 -m virtualenv -p python3.5 ${VENV_DIR}
-VENV_BIN=${VENV_DIR}/bin/python
 # Bug: setup.py must be run in this directory !
 (cd ${TEMP_DIR}/ortools && ../venv/bin/python setup.py install)
 cp test.py.in ${TEMP_DIR}/venv/test.py
@@ -49,7 +52,7 @@ echo Creating Python 3.5 venv...DONE
 
 echo Rebuilding for Python 3.6
 make clean_python UNIX_PYTHON_VER=3.6
-make python UNIX_PYTHON_VER=3.6 -j 5
+make python -l 1 UNIX_PYTHON_VER=3.6
 make test_python UNIX_PYTHON_VER=3.6
 make pypi_archive UNIX_PYTHON_VER=3.6
 echo Rebuilding for Python 3.6...DONE
@@ -58,7 +61,6 @@ TEMP_DIR=temp-python3.6
 VENV_DIR=${TEMP_DIR}/venv
 python3.6 -m pip install --user virtualenv
 python3.6 -m virtualenv -p python3.6 ${VENV_DIR}
-VENV_BIN=${VENV_DIR}/bin/python
 # Bug: setup.py must be run in this directory !
 (cd ${TEMP_DIR}/ortools && ../venv/bin/python setup.py install)
 cp test.py.in ${TEMP_DIR}/venv/test.py
