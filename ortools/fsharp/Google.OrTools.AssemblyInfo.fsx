e1354344979a7a411d82c90b7f78e79323ecb5a8
acco32 <mtthw.j.mr@gmail.com>
2018-03-06 21:52:08 -0800

    Add nuget package

---
+++ ortools/fsharp/Google.OrTools.AssemblyInfo.fsx
@@ -0,0 +1,14 @@
+open System.Reflection
+
+[<assembly: AssemblyTitle("Google.OrTools.FSharp")>]
+[<assembly: AssemblyDescription(".NET F# Assembly for the Operations Research Tools project")>]
+
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Google Inc")>]
+[<assembly: AssemblyProduct("Operations Research Tools")>]
+[<assembly: AssemblyCopyright("Copyright © 2018")>]
+[<assembly: AssemblyTrademark("")>]
+
+[<assembly: AssemblyVersion("VVVV")>]
+
+()
