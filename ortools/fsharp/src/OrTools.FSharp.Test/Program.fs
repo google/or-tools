aa6b091bd7dd2e59acbab1e23965cd469ae6e6ed
acco32 <mtthw.j.mr@gmail.com>
2018-04-15 09:20:08 -0700

    Add xunit test library

---
+++ ortools/fsharp/src/OrTools.FSharp.Test/Program.fs
@@ -0,0 +1,9 @@
+﻿namespace Google.OrTools.FSharp.Tests
+
+module Program =
+  // open Google.OrTools.FSharp.Tests
+
+  [<EntryPoint>]
+  let main _ =
+      printfn "Google.OrTools.FSharp Tests"
+      0
