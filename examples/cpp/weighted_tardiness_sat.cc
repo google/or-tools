1ec337d9c041ed03960d3a9bfc3e50c076b573bd
Laurent Perron <lperron@google.com>
2018-06-08 13:36:19 +0200

    fix weighted_tardiness_sat crash

--- examples/cpp/weighted_tardiness_sat.cc
+++ examples/cpp/weighted_tardiness_sat.cc
@@ -212,14 +212,30 @@ void Solve(const std::vector<int>& durations, const std::vector<int>& due_dates,
   Model model;
   model.Add(NewSatParameters(FLAGS_params));
   model.Add(NewFeasibleSolutionObserver([&](const CpSolverResponse& r) {
-    // Note that we conpute the "real" cost here and do not use the tardiness
+    // Note that we compute the "real" cost here and do not use the tardiness
     // variables. This is because in the core based approach, the tardiness
     // variable might be fixed before the end date, and we just have a >=
     // relation.
+    auto min_value = [&r](int v) {
+      if (r.solution_size() > 0) {
+        return r.solution(v);
+      } else {
+        return r.solution_lower_bounds(v);
+      }
+    };
+    auto max_value = [&r](int v) {
+      if (r.solution_size() > 0) {
+        return r.solution(v);
+      } else {
+        return r.solution_upper_bounds(v);
+      }
+    };
+
     int64 objective = 0;
     for (int i = 0; i < num_tasks; ++i) {
-      objective += weights[i] * std::max<int64>(0ll, r.solution(tasks_end[i]) -
-                                                         due_dates[i]);
+      CHECK_EQ(min_value(tasks_end[i]), max_value(tasks_end[i]));
+      const int64 end = min_value(tasks_end[i]);
+      objective += weights[i] * std::max<int64>(0ll, end - due_dates[i]);
     }
     LOG(INFO) << "Cost " << objective;
 
@@ -227,21 +243,23 @@ void Solve(const std::vector<int>& durations, const std::vector<int>& due_dates,
     std::vector<int> sorted_tasks(num_tasks);
     std::iota(sorted_tasks.begin(), sorted_tasks.end(), 0);
     std::sort(sorted_tasks.begin(), sorted_tasks.end(), [&](int v1, int v2) {
-      return r.solution(tasks_start[v1]) < r.solution(tasks_start[v2]);
+      CHECK_EQ(min_value(tasks_start[v1]), max_value(tasks_start[v1]));
+      CHECK_EQ(min_value(tasks_start[v2]), max_value(tasks_start[v2]));
+      return min_value(tasks_start[v1]) < min_value(tasks_start[v2]);
     });
     std::string solution = "0";
     int end = 0;
     for (const int i : sorted_tasks) {
-      const int64 cost = weights[i] * r.solution(tardiness_vars[i]);
+      const int64 cost = weights[i] * min_value(tardiness_vars[i]);
       absl::StrAppend(&solution, "| #", i, " ");
       if (cost > 0) {
         // Display the cost in red.
         absl::StrAppend(&solution, "\033[1;31m(+", cost, ") \033[0m");
       }
-      absl::StrAppend(&solution, "|", r.solution(tasks_end[i]));
-      CHECK_EQ(end, r.solution(tasks_start[i]));
+      absl::StrAppend(&solution, "|", min_value(tasks_end[i]));
+      CHECK_EQ(end, min_value(tasks_start[i]));
       end += durations[i];
-      CHECK_EQ(end, r.solution(tasks_end[i]));
+      CHECK_EQ(end, min_value(tasks_end[i]));
     }
     LOG(INFO) << "solution: " << solution;
   }));
