f73841ef38d3266ca4b7266756f87c899eca9b3b
Laurent Perron <lperron@google.com>
2018-07-23 15:29:39 -0700

    balanced group example

--- examples/python/balance_group_sat.py
+++ examples/python/balance_group_sat.py
@@ -15,29 +15,37 @@ from __future__ import print_function
 
 from ortools.sat.python import cp_model
 
+num_groups = 10
+num_values = 100
+
 
 model = cp_model.CpModel()
 
 boo_x_i_j = {}
-for i in range(100):
-    for j in range(10):
+for i in range(num_values):
+    for j in range(num_groups):
         boo_x_i_j[(i, j)] = model.NewBoolVar('x%d belongs to group %d' % (i, j))
 
 e = model.NewIntVar(0, 5, 'epsilon')
-values = [i + 1 for i in range(100)]
+
+values = [i + 1 for i in range(num_values)]
 sum_of_values = sum(values)
-average_value = sum_of_values / 10
+average_value = sum_of_values / num_groups
 
-for j in range(10):
-    model.Add(sum(boo_x_i_j[(i, j)] for i in range(100)) == 10)
+for j in range(num_groups):
+    model.Add(sum(boo_x_i_j[(i, j)] for i in range(num_values)) == num_values / num_groups)
 
-for i in range(100):
-    model.Add(sum(boo_x_i_j[(i, j)] for j in range(10)) == 1)
+for i in range(num_values):
+    model.Add(sum(boo_x_i_j[(i, j)] for j in range(num_groups)) == 1)
 
-for j in range(10):
-    model.Add(-e <= sum(boo_x_i_j[(i, j)]*values[i] for i in range(100)) -
+for j in range(num_groups):
+    model.Add(sum(boo_x_i_j[(i, j)] * values[i] for i in range(num_values)) -
               average_value <= e)
+    model.Add(sum(boo_x_i_j[(i, j)] * values[i] for i in range(num_values)) -
+              average_value >= -e)
+
 model.Minimize(e)
+
 
 solver = cp_model.CpSolver()
 status = solver.Solve(model)
@@ -48,15 +56,16 @@ print('  - branches  : %i' % solver.NumBranches())
 print('  - wall time : %f s' % solver.WallTime())
 
 groups = {}
-for j in range(10):
+for j in range(num_groups):
     groups[j] = []
-for i in range(100):
-    for j in range(10):
+for i in range(num_values):
+    for j in range(num_groups):
         if solver.Value(boo_x_i_j[(i, j)]):
             groups[j].append(values[i])
 
-for j in range(10):
-    print ('group %i: average = %0.2f [' % (j, sum(groups[j]) / 10.0), end='')
+for j in range(num_groups):
+    print ('group %i: average = %0.2f [' % (
+        j, 1.0 * sum(groups[j]) / len(groups[j])), end='')
     for v in groups[j]:
         print(' %i' % v, end='')
     print(' ]')
