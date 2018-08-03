460bbf9bf159e7626fd5fe3bb9f0a53e85d425d8
Laurent Perron <lperron@google.com>
2018-08-03 13:02:33 -0700

    add python3.7 to setup.py

--- tools/setup.py
+++ tools/setup.py
@@ -80,6 +80,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Office/Business :: Scheduling',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
