c0eac159c2fca3278089428f755ab73a6a6a2eb0
Laurent Perron <lperron@google.com>
2018-09-05 14:27:48 +0200

    update notebooks to remove main() method

--- tools/export_to_ipynb.py
+++ tools/export_to_ipynb.py
@@ -15,6 +15,8 @@ line_start = [c.lineno-1 for c in all_blocks]
 line_start[0] = 0
 lines = text.split('\n')
 
+full_text = ''
+
 for c_block, s, e in zip(all_blocks, line_start, line_start[1:]+[len(lines)]):
     c_text = '\n'.join(lines[s:e])
     if isinstance(c_block, ast.If) and c_block.test.comparators[0].s=='__main__':
@@ -23,16 +25,19 @@ for c_block, s, e in zip(all_blocks, line_start, line_start[1:]+[len(lines)]):
         # remove start and de-indent lines
         c_lines = lines[s+1:e]
         spaces_to_delete = c_block.body[0].col_offset
-        fixed_lines = [n_line[spaces_to_delete:] if n_line.startswith(' '*spaces_to_delete) 
+        fixed_lines = [n_line[spaces_to_delete:] if n_line.startswith(' '*spaces_to_delete)
                        else n_line for n_line in c_lines]
         fixed_text = '\n'.join(fixed_lines)
         print('Unwrapping main function')
-        nbook['cells'].append(v4.new_code_cell(fixed_text))
+        full_text += fixed_text
     else:
         print('appending', c_block)
-        nbook['cells'].append(v4.new_code_cell(c_text))
+        full_text += c_text + '\n'
 
-jsonform = v4.writes(nbook) + "\n"
+nbook['cells'].append(v4.new_code_cell(full_text))
+
+
+jsonform = v4.writes(nbook) + '\n'
 output = input
 output = output.replace('.py', '.ipynb')
 output = output.replace('examples/python', 'examples/notebook')
