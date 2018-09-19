1438b6769614e3f84d02fbfcb36e25b07cf4b27a
Corentin Le Molgat <corentinl@google.com>
2018-09-19 14:35:06 +0200

    Fix publish_delivery_win
    - Nuget package are published by hand,
     since I need the three `runtime.<rid>` on one machine first...

--- tools/release/publish_delivery_win.cmd
+++ tools/release/publish_delivery_win.cmd
@@ -9,7 +9,3 @@ make.exe pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python35-64
 make.exe pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python36-64
 make.exe pypi_upload WINDOWS_PATH_TO_PYTHON=c:\python37-64
 echo Uploading all Python artifacts...DONE
-
-echo Uploading .NET artifacts...
-make.exe nuget_upload WINDOWS_PATH_TO_PYTHON=c:\python37-64
-echo Uploading .NET artifacts...DONE
