eb2f85232d8cb96a885829dfba93e2e01e519c0f
Corentin Le Molgat <corentinl@google.com>
2018-10-03 17:34:39 +0200

    Makefile: Add abseil-cpp on windows

--- makefiles/Makefile.third_party.win.mk
+++ makefiles/Makefile.third_party.win.mk
@@ -14,6 +14,8 @@ WINDOWS_GLOG_DIR ?= $(OR_ROOT)dependencies/install
 WINDOWS_GLOG_PATH = $(subst /,$S,$(WINDOWS_GLOG_DIR))
 WINDOWS_PROTOBUF_DIR ?= $(OR_ROOT)dependencies/install
 WINDOWS_PROTOBUF_PATH = $(subst /,$S,$(WINDOWS_PROTOBUF_DIR))
+WINDOWS_ABSL_DIR ?= $(OR_ROOT)dependencies/install
+WINDOWS_ABSL_PATH = $(subst /,$S,$(WINDOWS_ABSL_DIR))
 WINDOWS_CBC_DIR ?= $(OR_ROOT)dependencies/install
 WINDOWS_CBC_PATH = $(subst /,$S,$(WINDOWS_CBC_DIR))
 WINDOWS_CGL_DIR ?= $(WINDOWS_CBC_DIR)
@@ -36,6 +38,7 @@ ZLIB_ARCHIVE_TAG = 1211
 GFLAGS_TAG = 2.2.1
 GLOG_TAG = 0.3.5
 PROTOBUF_TAG = 3.6.1
+ABSL_TAG = master
 CBC_TAG = 2.9.9
 CGL_TAG = 0.59.10
 CLP_TAG = 1.16.11
@@ -128,6 +131,7 @@ build_third_party: \
  install_gflags \
  install_glog \
  install_protobuf \
+ install_absl \
  install_swig \
  install_coin_cbc
 
@@ -334,6 +338,61 @@ dependencies/install/lib/protobuf.jar: | dependencies/install/bin/protoc.exe
 	cd dependencies\\sources\\protobuf-$(PROTOBUF_TAG)\\java\\core\\src\\main\\java && "$(JAVAC_BIN)" com\\google\\protobuf\\*java
 	cd dependencies\\sources\\protobuf-$(PROTOBUF_TAG)\\java\\core\\src\\main\\java && "$(JAR_BIN)" cvf ..\\..\\..\\..\\..\\..\\..\\install\\lib\\protobuf.jar com\\google\\protobuf\\*class
 
+##################
+##  ABSEIL-CPP  ##
+##################
+# This uses abseil-cpp cmake-based build.
+.PHONY: install_absl
+install_absl: dependencies/install/lib/absl.lib
+
+dependencies/install/lib/absl.lib: dependencies/sources/abseil-cpp-$(ABSL_TAG) | dependencies/install
+	cd dependencies\sources\abseil-cpp-$(ABSL_TAG) && \
+  $(SET_COMPILER) "$(CMAKE)" -H. -Bbuild_cmake \
+    -DCMAKE_PREFIX_PATH=..\..\install \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DCMAKE_INSTALL_PREFIX=..\..\install \
+    -G "NMake Makefiles" && \
+  "$(CMAKE)" --build build_cmake && \
+  "$(CMAKE)" --build build_cmake --target install
+
+dependencies/sources/abseil-cpp-$(ABSL_TAG): | dependencies/sources
+	-$(DELREC) dependencies/sources/abseil-cpp-$(ABSL_TAG)
+	git clone --quiet -b $(ABSL_TAG) https://github.com/abseil/abseil-cpp.git dependencies\sources\abseil-cpp-$(ABSL_TAG)
+	cd dependencies\sources\abseil-cpp-$(ABSL_TAG) \
+ && git apply "$(OR_TOOLS_TOP)\patches\abseil-cpp-$(ABSL_TAG).patch"
+	$(COPY) patches\absl-config.cmake dependencies\sources\abseil-cpp-$(ABSL_TAG)\CMake
+
+ABSL_INC = /I"$(WINDOWS_ABSL_PATH)\\include"
+ABSL_SWIG = -I"$(WINDOWS_ABSL_PATH)/include"
+STATIC_ABSL_LNK = \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_bad_any_cast.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_bad_optional_access.lib"\
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_base.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_container.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_dynamic_annotations.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_examine_stack.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_failure_signal_handler.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_hash.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_int128.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_leak_check.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_malloc_internal.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_optional.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_spinlock_wait.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_stack_consumption.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_stacktrace.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_strings.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_symbolize.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_synchronization.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_throw_delegate.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_time.lib" \
+ "$(WINDOWS_ABSL_PATH)\\lib\\absl_variant.lib"
+DYNAMIC_ABSL_LNK = $(STATIC_ABSL_LNK)
+
+ABSL_LNK = $(STATIC_ABSL_LNK)
+DEPENDENCIES_LNK += $(ABSL_LNK)
+
 ############
 ##  COIN  ##
 ############
@@ -474,6 +533,7 @@ clean_third_party: remove_readonly_svn_attribs
 	-$(DELREC) dependencies\sources\gflags*
 	-$(DELREC) dependencies\sources\glog*
 	-$(DELREC) dependencies\sources\protobuf*
+	-$(DELREC) dependencies\sources\abseil-cpp*
 	-$(DELREC) dependencies\sources\Cbc-*
 	-$(DELREC) dependencies\sources\google*
 	-$(DELREC) dependencies\sources\glpk*
