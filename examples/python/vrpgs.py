73bde2d60f10732456f8cedc199e904c1b213760
Laurent Perron <lperron@google.com>
2018-10-16 14:25:40 +0200

    fix last? python examples; fix license text

--- examples/python/vrpgs.py
+++ examples/python/vrpgs.py
@@ -24,9 +24,13 @@
 """
 
 from __future__ import print_function
+
+from functools import partial
 from six.moves import xrange
+
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
+
 
 ###########################
 # Problem Data Definition #
@@ -48,11 +52,11 @@ def create_data_model():
   # Compute locations in meters using the block dimension defined as follow
   # Manhattan average block: 750ft x 264ft -> 228m x 80m
   # here we use: 114m x 80m city block
-  # src: https://nyti.ms/2GDoRIe "NY Times: Know Your distance"
-  data["locations"] = [(l[0] * 114, l[1] * 80) for l in _locations]
-  data["num_locations"] = len(data["locations"])
-  data["num_vehicles"] = 4
-  data["depot"] = 0
+  # src: https://nyti.ms/2GDoRIe 'NY Times: Know Your distance'
+  data['locations'] = [(l[0] * 114, l[1] * 80) for l in _locations]
+  data['num_locations'] = len(data['locations'])
+  data['num_vehicles'] = 4
+  data['depot'] = 0
   return data
 
 #######################
@@ -63,23 +67,25 @@ def manhattan_distance(position_1, position_2):
   return (
       abs(position_1[0] - position_2[0]) + abs(position_1[1] - position_2[1]))
 
+
 def create_distance_evaluator(data):
   """Creates callback to return distance between points."""
   _distances = {}
   # precompute distance between location to have distance callback in O(1)
-  for from_node in xrange(data["num_locations"]):
+  for from_node in xrange(data['num_locations']):
     _distances[from_node] = {}
-    for to_node in xrange(data["num_locations"]):
+    for to_node in xrange(data['num_locations']):
       if from_node == to_node:
         _distances[from_node][to_node] = 0
       else:
         _distances[from_node][to_node] = (
-            manhattan_distance(data["locations"][from_node],
-                               data["locations"][to_node]))
+            manhattan_distance(data['locations'][from_node],
+                               data['locations'][to_node]))
 
-  def distance_evaluator(from_node, to_node):
+  def distance_evaluator(manager, from_node, to_node):
     """Returns the manhattan distance between the two nodes"""
-    return _distances[from_node][to_node]
+    return _distances[manager.IndexToNode(from_node)][manager.IndexToNode(
+        to_node)]
 
   return distance_evaluator
 
@@ -100,20 +106,20 @@ def add_distance_dimension(routing, distance_evaluator):
 ###########
 # Printer #
 ###########
-def print_solution(data, routing, assignment):
+def print_solution(data, routing, manager, assignment):  # pylint:disable=too-many-locals
   """Prints assignment on console"""
   print('Objective: {}'.format(assignment.ObjectiveValue()))
   total_distance = 0
-  for vehicle_id in xrange(data["num_vehicles"]):
+  for vehicle_id in xrange(data['num_vehicles']):
     index = routing.Start(vehicle_id)
     plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
     distance = 0
     while not routing.IsEnd(index):
-      plan_output += ' {} ->'.format(routing.IndexToNode(index))
+      plan_output += ' {} ->'.format(manager.IndexToNode(index))
       previous_index = index
       index = assignment.Value(routing.NextVar(index))
       distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
-    plan_output += ' {}\n'.format(routing.IndexToNode(index))
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
     plan_output += 'Distance of the route: {}m\n'.format(distance)
     print(plan_output)
     total_distance += distance
@@ -127,23 +133,26 @@ def main():
   # Instantiate the data problem.
   data = create_data_model()
 
+  # Create the routing index manager
+  manager =  pywrapcp.RoutingIndexManager(
+      data['num_locations'], data['num_vehicles'], data['depot'])
+
   # Create Routing Model
-  routing = pywrapcp.RoutingModel(
-      data["num_locations"],
-      data["num_vehicles"],
-      data["depot"])
+  routing = pywrapcp.RoutingModel(manager)
+
   # Define weight of each edge
-  distance_evaluator = create_distance_evaluator(data)
-  routing.SetArcCostEvaluatorOfAllVehicles(distance_evaluator)
+  distance_evaluator = routing.RegisterTransitCallback(
+      partial(create_distance_evaluator(data), manager))
   add_distance_dimension(routing, distance_evaluator)
 
   # Setting first solution heuristic (cheapest addition).
-  search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+  search_parameters = pywrapcp.DefaultRoutingSearchParameters()
   search_parameters.first_solution_strategy = (
-      routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # pylint: disable=no-member
+      routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)  # pylint: disable=no-member
   # Solve the problem.
   assignment = routing.SolveWithParameters(search_parameters)
-  print_solution(data, routing, assignment)
+  print_solution(data, routing, manager, assignment)
+
 
 if __name__ == '__main__':
   main()
