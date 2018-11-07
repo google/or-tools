475ccd99e38e6604fc848a5a08f47088addefa78
Laurent Perron <lperron@corp.google.com>
2018-11-07 20:49:03 +0100

    move contrib examples to a dedicated contrib directory

--- examples/contrib/volsay3.cs
+++ examples/contrib/volsay3.cs
@@ -1,5 +1,4 @@
 // Copyright 2012 Hakan Kjellerstrand
-// Copyright 2018 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
