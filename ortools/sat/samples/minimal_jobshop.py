a74016ca96c986b6493c80306994230e47619888
Laurent Perron <lperron@google.com>
2018-11-08 12:51:12 +0100

    improve jobshop python sat samples; rename it to minimal_jobshop

--- ortools/sat/samples/minimal_jobshop.py
+++ ortools/sat/samples/minimal_jobshop.py
@@ -1,15 +1,34 @@
+# Copyright 2010-2017 Google
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Minimal jobshop example."""
+
+# [START program]
 from __future__ import print_function
 
 import collections
 
+# [START model]
 # Import Python wrapper for or-tools CP-SAT solver.
 from ortools.sat.python import cp_model
+
 
 def main():
   """Minimal jobshop problem."""
   # Create the model.
   model = cp_model.CpModel()
+  # [END model]
 
+  # [START data]
   machines_count = 3
   jobs_count = 3
   all_machines = range(0, machines_count)
@@ -18,16 +37,19 @@ def main():
   machines = [[0, 1, 2], [0, 2, 1], [1, 2]]
 
   processing_times = [[3, 2, 2], [2, 1, 4], [4, 3]]
-  # Computes horizon.
+  # [END data]
+
+  # Compute horizon.
   horizon = 0
   for job in all_jobs:
     horizon += sum(processing_times[job])
 
+  # [START variables]
   task_type = collections.namedtuple('task_type', 'start end interval')
   assigned_task_type = collections.namedtuple('assigned_task_type',
                                               'start job index')
 
-  # Creates jobs.
+  # Create jobs.
   all_tasks = {}
   for job in all_jobs:
     for index in range(0, len(machines[job])):
@@ -38,8 +60,10 @@ def main():
                                           'interval_%i_%i' % (job, index))
       all_tasks[(job, index)] = task_type(
           start=start_var, end=end_var, interval=interval_var)
+  # [END variables]
 
-  # Creates sequence variables and add disjunctive constraints.
+  # [START constraints]
+  # Create and add disjunctive constraints.
   for machine in all_machines:
     intervals = []
     for job in all_jobs:
@@ -53,19 +77,25 @@ def main():
     for index in range(0, len(machines[job]) - 1):
       model.Add(
           all_tasks[(job, index + 1)].start >= all_tasks[(job, index)].end)
+  # [END constraints]
 
+  # [START objective]
   # Makespan objective.
   obj_var = model.NewIntVar(0, horizon, 'makespan')
   model.AddMaxEquality(
       obj_var,
       [all_tasks[(job, len(machines[job]) - 1)].end for job in all_jobs])
   model.Minimize(obj_var)
+  # [END objective]
 
+  # [START solver]
   # Solve model.
   solver = cp_model.CpSolver()
   status = solver.Solve(model)
+  # [END solver]
 
   if status == cp_model.OPTIMAL:
+    # [START solution_printing]
     # Print out makespan.
     print('Optimal Schedule Length: %i' % solver.ObjectiveValue())
     print()
@@ -110,6 +140,7 @@ def main():
     print(sol_line_tasks)
     print('Time Intervals for task_types\n')
     print(sol_line)
+    # [END solution_printing]
 
 
 if __name__ == '__main__':
