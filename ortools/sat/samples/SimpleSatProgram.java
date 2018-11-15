e86822f80419c0fae5067e889e62cf3a44bd00c9
Laurent Perron <lperron@google.com>
2018-11-15 20:44:59 +0100

    fix

--- ortools/sat/samples/SimpleSatProgram.java
+++ ortools/sat/samples/SimpleSatProgram.java
@@ -14,7 +14,7 @@
 import com.google.ortools.sat.CpModel;
 import com.google.ortools.sat.IntVar;
 
-public class CodeSample {
+public class SimpleSatProgram {
   static {
     System.loadLibrary("jniortools");
   }
