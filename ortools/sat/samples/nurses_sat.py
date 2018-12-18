b9a1fabd4c3a83442cb6a5e792c624c823740207
Corentin Le Molgat <corentinl@google.com>
2018-12-18 13:58:34 +0100

    Update nurses_sat.py

--- ortools/sat/samples/nurses_sat.py
+++ ortools/sat/samples/nurses_sat.py
@@ -10,124 +10,125 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Example of a simple nurse scheduling problem."""
 
+# [START program]
 from __future__ import print_function
-import sys
+import math
+# [START import]
 from ortools.sat.python import cp_model
+# [END import]
 
 
+# [START solution_printer]
 class NursesPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
-    """Print intermediate solutions."""
+  """Print intermediate solutions."""
 
-    def __init__(self, shifts, num_nurses, num_days, num_shifts, sols):
-        cp_model.CpSolverSolutionCallback.__init__(self)
-        self.__shifts = shifts
-        self.__num_nurses = num_nurses
-        self.__num_days = num_days
-        self.__num_shifts = num_shifts
-        self.__solutions = set(sols)
-        self.__solution_count = 0
+  def __init__(self, shifts, num_nurses, num_days, num_shifts, sols):
+    cp_model.CpSolverSolutionCallback.__init__(self)
+    self.__shifts = shifts
+    self.__num_nurses = num_nurses
+    self.__num_days = num_days
+    self.__num_shifts = num_shifts
+    self.__solutions = set(sols)
+    self.__solution_count = 0
 
-    def on_solution_callback(self):
-        self.__solution_count += 1
-        if self.__solution_count in self.__solutions:
-            print('Solution #%i' % self.__solution_count)
-            for d in range(self.__num_days):
-                print('Day #%i' % d)
-                for n in range(self.__num_nurses):
-                    for s in range(self.__num_shifts):
-                        if self.Value(self.__shifts[(n, d, s)]):
-                            print('  Nurse #%i is working shift #%i' % (n, s))
-            print()
+  def on_solution_callback(self):
+    self.__solution_count += 1
+    if self.__solution_count in self.__solutions:
+      print('Solution %i' % self.__solution_count)
+      for d in range(self.__num_days):
+        print('Day %i' % d)
+        for n in range(self.__num_nurses):
+          is_working = False
+          for s in range(self.__num_shifts):
+            if self.Value(self.__shifts[(n, d, s)]):
+              is_working = True
+              print('  Nurse %i is working shift %i' % (n, s))
+          if not is_working:
+            print('  Nurse {} is not working'.format(n))
+      print()
 
-    def solution_count(self):
-        return self.__solution_count
+  def solution_count(self):
+    return self.__solution_count
+# [END solution_printer]
 
 
 def main():
-    # Data.
-    num_nurses = 4
-    num_shifts = 4  # Nurse assigned to shift 0 means not working that day.
-    num_days = 7
-    all_nurses = range(num_nurses)
-    all_shifts = range(num_shifts)
-    all_working_shifts = range(1, num_shifts)
-    all_days = range(num_days)
+  # Data.
+  # [START data]
+  num_nurses = 4
+  num_shifts = 3
+  num_days = 3
+  all_nurses = range(num_nurses)
+  all_shifts = range(num_shifts)
+  all_days = range(num_days)
+  # [END data]
+  # Creates the model.
+  # [START model]
+  model = cp_model.CpModel()
+  # [END model]
 
-    # Creates the model.
-    model = cp_model.CpModel()
-
-    # Creates shift variables.
-    # shifts[(n, d, s)]: nurse 'n' works shift 's' on day 'd'.
-    shifts = {}
-    for n in all_nurses:
-        for d in all_days:
-            for s in all_shifts:
-                shifts[(n, d, s)] = model.NewBoolVar('shift_n%id%is%i' % (n, d,
-                                                                          s))
-
-    # Makes assignments different on each day, that is each shift is assigned at
-    # most one nurse. As we have the same number of nurses and shifts, then each
-    # day, each shift is assigned to exactly one nurse.
+  # Creates shift variables.
+  # shifts[(n, d, s)]: nurse 'n' works shift 's' on day 'd'.
+  # [START variables]
+  shifts = {}
+  for n in all_nurses:
     for d in all_days:
-        for s in all_shifts:
-            model.Add(sum(shifts[(n, d, s)] for n in all_nurses) == 1)
+      for s in all_shifts:
+        shifts[(n, d, s)] = model.NewBoolVar('shift_n%id%is%i' % (n, d, s))
+  # [END variables]
 
-    # Nurses do 1 shift per day.
-    for n in all_nurses:
-        for d in all_days:
-            model.Add(sum(shifts[(n, d, s)] for s in all_shifts) == 1)
+  # Each shift is assigned to exactly one nurse.
+  # [START exactly_one_nurse]
+  for d in all_days:
+    for s in all_shifts:
+      model.Add(sum([shifts[(n, d, s)] for n in all_nurses]) == 1)
+  # [END exactly_one_nurse]
 
-    # Each nurse works 5 or 6 days in a week.
-    # That is each nurse works shift 0 at most 2 times.
-    for n in all_nurses:
-        model.AddSumConstraint([shifts[(n, d, 0)] for d in all_days], 1, 2)
+  # Each nurse works at most one shift per day.
+  # [START at_most_one_shift]
+  for n in all_nurses:
+    for d in all_days:
+      model.Add(sum([shifts[(n, d, s)] for s in all_shifts]) <= 1)
+  # [END at_most_one_shift]
 
-    # works_shift[(n, s)] is 1 if nurse n works shift s at least one day in
-    # the week.
-    works_shift = {}
-    for n in all_nurses:
-        for s in all_shifts:
-            works_shift[(n, s)] = model.NewBoolVar('works_shift_n%is%i' % (n,
-                                                                           s))
-            model.AddMaxEquality(works_shift[(n, s)],
-                                 [shifts[(n, d, s)] for d in all_days])
+  # works_shift[(n, s)] is 1 if nurse n works shift s at least one day in
+  # the schedule period.
+  works_shift = {}
+  for n in all_nurses:
+    for s in all_shifts:
+      works_shift[(n, s)] = model.NewBoolVar('works_shift_n%is%i' % (n, s))
+      model.AddMaxEquality(works_shift[(n, s)],
+                           [shifts[(n, d, s)] for d in all_days])
 
-    # For each shift, at most 2 nurses are assigned to that shift during the week.
-    for s in all_working_shifts:
-        model.Add(sum(works_shift[(n, s)] for n in all_nurses) <= 2)
+  # [START assign_nurses_evenly]
+  # min_shifts_assigned is the largest integer such that every nurse can be
+  # assigned at least that number of shifts.
+  min_shifts_assigned = int(math.floor(num_shifts * num_days / num_nurses))
+  for s in all_shifts:
+    model.Add(
+        sum([works_shift[(n, s)] for n in all_nurses]) >= min_shifts_assigned)
+  # [END assign_nurses_evenly]
 
-    # If a nurse works shifts 2 or 3 on, she must also work that shift the
-    # previous day or the following day.
-    # This means that on a given day and shift, either she does not work that
-    # shift on that day, or she works that shift on the day before, or the day
-    # after.
-    for n in all_nurses:
-        for s in [2, 3]:
-            for d in all_days:
-                yesterday = (d - 1) % num_days
-                tomorrow = (d + 1) % num_days
-                model.AddBoolOr([
-                    shifts[(n, yesterday, s)], shifts[(n, d, s)].Not(),
-                    shifts[(n, tomorrow, s)]
-                ])
+  # Creates the solver and solve.
+  # [START solve]
+  solver = cp_model.CpSolver()
+  # Display the first five solutions.
+  a_few_solutions = range(5)
+  solution_printer = NursesPartialSolutionPrinter(
+      shifts, num_nurses, num_days, num_shifts, a_few_solutions)
+  solver.SearchForAllSolutions(model, solution_printer)
+  # [END solve]
 
-    # Creates the solver and solve.
-    solver = cp_model.CpSolver()
-    # Display a few solutions picked at random.
-    a_few_solutions = [859, 2034, 5091, 7003]
-    solution_printer = NursesPartialSolutionPrinter(
-        shifts, num_nurses, num_days, num_shifts, a_few_solutions)
-    status = solver.SearchForAllSolutions(model, solution_printer)
-
-    # Statistics.
-    print()
-    print('Statistics')
-    print('  - conflicts       : %i' % solver.NumConflicts())
-    print('  - branches        : %i' % solver.NumBranches())
-    print('  - wall time       : %f ms' % solver.WallTime())
-    print('  - solutions found : %i' % solution_printer.solution_count())
-
+  # Statistics.
+  print()
+  print('Statistics')
+  print('  - conflicts       : %i' % solver.NumConflicts())
+  print('  - branches        : %i' % solver.NumBranches())
+  print('  - wall time       : %f ms' % solver.WallTime())
+  print('  - solutions found : %i' % solution_printer.solution_count())
 
 if __name__ == '__main__':
-    main()
+  main()
+# [END program]
