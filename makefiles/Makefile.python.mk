c3e0a7b3de64adc1142314040e5d8d0c97f50653
Corentin Le Molgat <corentinl@google.com>
2019-01-10 16:02:00 +0100

    Add vrp_pickup_delivery.py

--- makefiles/Makefile.python.mk
+++ makefiles/Makefile.python.mk
@@ -506,6 +506,7 @@ test_python_constraint_solver_samples: \
  rpy_vrp_capacity \
  rpy_vrp_drop_nodes \
  rpy_vrp_global_span \
+ rpy_vrp_pickup_delivery \
  rpy_vrp_starts_ends \
  rpy_vrp_time_windows \
  \
