0f24fe1e084ac4914e96c259f834042c784d5fd5
Laurent Perron <lperron@corp.google.com>
2019-04-10 10:50:49 -0700

    simple tsp_sat example

--- examples/python/tsp_sat.py
+++ examples/python/tsp_sat.py
@@ -1,3 +1,15 @@
+# Copyright 2010-2018 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Simple travelling salesman problem between cities."""
 
 from __future__ import print_function
@@ -51,20 +63,20 @@ distance_matrix = [
 
 def main():
     """Entry point of the program."""
-    # Instantiate the data problem.
     num_nodes = len(distance_matrix)
+    all_nodes = range(num_nodes)
+    print('Num nodes =', num_nodes)
 
     # Model.
     model = cp_model.CpModel()
-    print('Num nodes =', num_nodes)
 
     obj_vars = []
     obj_coeffs = []
 
     # Create the circuit constraint.
     arcs = []
-    for i in range(num_nodes):
-        for j in range(num_nodes):
+    for i in all_nodes:
+        for j in all_nodes:
             if i == j:
                 continue
 
@@ -83,7 +95,9 @@ def main():
     # Solve and print out the solution.
     solver = cp_model.CpSolver()
     solver.parameters.log_search_progress = True
+    # To benefit from the linearization of the circuit constraint.
     solver.parameters.linearization_level = 2
+
     status = solver.Solve(model)
     print(solver.ResponseStats())
 
