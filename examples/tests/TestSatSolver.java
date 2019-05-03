263d040142770c13388742770b1ef38f6f5359d8
Laurent Perron <lperron@corp.google.com>
2019-05-03 18:18:05 +0200

    fix

--- examples/tests/TestSatSolver.java
+++ examples/tests/TestSatSolver.java
@@ -34,7 +34,7 @@ public class TestSatSolver {
 
     // Create a linear constraint which enforces that only x or y can be greater
     // than 0.
-    model.addLinearSum(new IntVar[] { x, y }, 0, 1);
+    model.addSumConstraint(new IntVar[] { x, y }, 0, 1);
 
     // Create the objective variable
     IntVar obj = model.newIntVar(0, 3, "obj");
