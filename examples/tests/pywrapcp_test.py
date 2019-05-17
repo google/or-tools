21f91816dd7d356cd944ba77a8065b6b08cad7fb
Laurent Perron <lperron@google.com>
2019-05-17 14:02:20 +0200

    use unittest in cp and lp tests

--- examples/tests/pywrapcp_test.py
+++ examples/tests/pywrapcp_test.py
@@ -14,271 +14,231 @@
 
 import sys
 
-from ortools.constraint_solver import model_pb2
+import unittest
 from ortools.constraint_solver import pywrapcp
 
 
-def test_member():
-  print 'test_member'
-  solver = pywrapcp.Solver('test member')
-  x = solver.IntVar(1, 10, 'x')
-  ct = x.Member([1, 2, 3, 5])
-  print ct
-
-
-def test_sparse_var():
-  print 'test_sparse_var'
-  solver = pywrapcp.Solver('test_sparse_var')
-  x = solver.IntVar([1, 3, 5], 'x')
-  print x
-
-
-def test_modulo():
-  print 'test_modulo'
-  solver = pywrapcp.Solver('test_modulo')
-  x = solver.IntVar(0, 10, 'x')
-  y = solver.IntVar(2, 4, 'y')
-  print x % 3
-  print x % y
-
-
-def test_limit():
-  solver = pywrapcp.Solver('test_limit')
-  # TODO(user): expose the proto-based MakeLimit() API in or-tools and test it
-  # here.
-  time = 10000  # ms
-  branches = 10
-  failures = sys.maxint
-  solutions = sys.maxint
-  smart_time_check = True
-  cumulative = False
-  limit = solver.Limit(time, branches, failures, solutions, smart_time_check,
-                       cumulative)
-  print limit
-
-
-def test_export():
-  print 'test_export'
-  solver = pywrapcp.Solver('test_export')
-  x = solver.IntVar(1, 10, 'x')
-  ct = x.Member([1, 2, 3, 5])
-  solver.Add(ct)
-  proto = model_pb2.CPModelProto()
-  proto.model = 'wrong name'
-  solver.ExportModel(proto)
-  print repr(proto)
-  print str(proto)
-
-
 class SearchMonitorTest(pywrapcp.SearchMonitor):
+    def __init__(self, solver, nexts):
+        pywrapcp.SearchMonitor.__init__(self, solver)
+        self._nexts = nexts
 
-  def __init__(self, solver, nexts):
-    pywrapcp.SearchMonitor.__init__(self, solver)
-    self._nexts = nexts
+    def BeginInitialPropagation(self):
+        print(self._nexts)
 
-  def BeginInitialPropagation(self):
-    print self._nexts
-
-  def EndInitialPropagation(self):
-    print self._nexts
-
-
-def test_search_monitor():
-  print 'test_search_monitor'
-  solver = pywrapcp.Solver('test search_monitor')
-  x = solver.IntVar(1, 10, 'x')
-  ct = (x == 3)
-  solver.Add(ct)
-  db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
-  monitor = SearchMonitorTest(solver, x)
-  solver.Solve(db, monitor)
+    def EndInitialPropagation(self):
+        print(self._nexts)
 
 
 class DemonTest(pywrapcp.PyDemon):
+    def __init__(self, x):
+        pywrapcp.PyDemon.__init__(self)
+        self._x = x
+        print('Demon built')
 
-  def __init__(self, x):
-    pywrapcp.PyDemon.__init__(self)
-    self._x = x
-    print 'Demon built'
-
-  def Run(self, solver):
-    print 'in Run(), saw ' + str(self._x)
-
-
-def test_demon():
-  print 'test_demon'
-  solver = pywrapcp.Solver('test_demon')
-  x = solver.IntVar(1, 10, 'x')
-  demon = DemonTest(x)
-  demon.Run(solver)
+    def Run(self, solver):
+        print('in Run(), saw ' + str(self._x))
 
 
 class ConstraintTest(pywrapcp.PyConstraint):
+    def __init__(self, solver, x):
+        pywrapcp.Constraint.__init__(self, solver)
+        self._x = x
+        print('Constraint built')
 
-  def __init__(self, solver, x):
-    pywrapcp.Constraint.__init__(self, solver)
-    self._x = x
-    print 'Constraint built'
+    def Post(self):
+        print('in Post()')
+        self._demon = DemonTest(self._x)
+        self._x.WhenBound(self._demon)
+        print('out of Post()')
 
-  def Post(self):
-    print 'in Post()'
-    self._demon = DemonTest(self._x)
-    self._x.WhenBound(self._demon)
-    print 'out of Post()'
-
-  def InitialPropagate(self):
-    print 'in InitialPropagate()'
-    self._x.SetMin(5)
-    print self._x
-    print 'out of InitialPropagate()'
-
-
-def test_constraint():
-  print 'test_constraint'
-  solver = pywrapcp.Solver('test_constraint')
-  x = solver.IntVar(1, 10, 'x')
-  myct = ConstraintTest(solver, x)
-  solver.Add(myct)
-  db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
-  solver.Solve(db)
+    def InitialPropagate(self):
+        print('in InitialPropagate()')
+        self._x.SetMin(5)
+        print(self._x)
+        print('out of InitialPropagate()')
 
 
 class InitialPropagateDemon(pywrapcp.PyDemon):
+    def __init__(self, ct):
+        pywrapcp.Demon.__init__(self)
+        self._ct = ct
 
-  def __init__(self, ct):
-    pywrapcp.Demon.__init__(self)
-    self._ct = ct
-
-  def Run(self, solver):
-    self._ct.InitialPropagate()
+    def Run(self, solver):
+        self._ct.InitialPropagate()
 
 
 class DumbGreaterOrEqualToFive(pywrapcp.PyConstraint):
+    def __init__(self, solver, x):
+        pywrapcp.Constraint.__init__(self, solver)
+        self._x = x
 
-  def __init__(self, solver, x):
-    pywrapcp.Constraint.__init__(self, solver)
-    self._x = x
+    def Post(self):
+        self._demon = InitialPropagateDemon(self)
+        self._x.WhenBound(self._demon)
 
-  def Post(self):
-    self._demon = InitialPropagateDemon(self)
-    self._x.WhenBound(self._demon)
-
-  def InitialPropagate(self):
-    if self._x.Bound():
-      if self._x.Value() < 5:
-        print 'Reject %d' % self._x.Value()
-        self.solver().Fail()
-      else:
-        print 'Accept %d' % self._x.Value()
-
-
-def test_failing_constraint():
-  print 'test_failing_constraint'
-  solver = pywrapcp.Solver('test failing constraint')
-  x = solver.IntVar(1, 10, 'x')
-  myct = DumbGreaterOrEqualToFive(solver, x)
-  solver.Add(myct)
-  db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
-  solver.Solve(db)
-
-
-def test_domain_iterator():
-  print 'test_domain_iterator'
-  solver = pywrapcp.Solver('test_domain_iterator')
-  x = solver.IntVar([1, 2, 4, 6], 'x')
-  for i in x.DomainIterator():
-    print i
+    def InitialPropagate(self):
+        if self._x.Bound():
+            if self._x.Value() < 5:
+                print('Reject %d' % self._x.Value())
+                self.solver().Fail()
+            else:
+                print('Accept %d' % self._x.Value())
 
 
 class WatchDomain(pywrapcp.PyDemon):
+    def __init__(self, x):
+        pywrapcp.Demon.__init__(self)
+        self._x = x
 
-  def __init__(self, x):
-    pywrapcp.Demon.__init__(self)
-    self._x = x
-
-  def Run(self, solver):
-    for i in self._x.HoleIterator():
-      print 'Removed %d' % i
+    def Run(self, solver):
+        for i in self._x.HoleIterator():
+            print('Removed %d' % i)
 
 
 class HoleConstraintTest(pywrapcp.PyConstraint):
+    def __init__(self, solver, x):
+        pywrapcp.Constraint.__init__(self, solver)
+        self._x = x
 
-  def __init__(self, solver, x):
-    pywrapcp.Constraint.__init__(self, solver)
-    self._x = x
+    def Post(self):
+        self._demon = WatchDomain(self._x)
+        self._x.WhenDomain(self._demon)
 
-  def Post(self):
-    self._demon = WatchDomain(self._x)
-    self._x.WhenDomain(self._demon)
-
-  def InitialPropagate(self):
-    self._x.RemoveValue(5)
-
-
-def test_hole_iterator():
-  print 'test_hole_iterator'
-  solver = pywrapcp.Solver('test_hole_iterator')
-  x = solver.IntVar(1, 10, 'x')
-  myct = HoleConstraintTest(solver, x)
-  solver.Add(myct)
-  db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND, solver.ASSIGN_MIN_VALUE)
-  solver.Solve(db)
+    def InitialPropagate(self):
+        self._x.RemoveValue(5)
 
 
 class BinarySum(pywrapcp.PyConstraint):
+    def __init__(self, solver, x, y, z):
+        pywrapcp.Constraint.__init__(self, solver)
+        self._x = x
+        self._y = y
+        self._z = z
 
-  def __init__(self, solver, x, y, z):
-    pywrapcp.Constraint.__init__(self, solver)
-    self._x = x
-    self._y = y
-    self._z = z
+    def Post(self):
+        self._demon = InitialPropagateDemon(self)
+        self._x.WhenRange(self._demon)
+        self._y.WhenRange(self._demon)
+        self._z.WhenRange(self._demon)
 
-  def Post(self):
-    self._demon = InitialPropagateDemon(self)
-    self._x.WhenRange(self._demon)
-    self._y.WhenRange(self._demon)
-    self._z.WhenRange(self._demon)
-
-  def InitialPropagate(self):
-    self._z.SetRange(self._x.Min() + self._y.Min(),
-                     self._x.Max() + self._y.Max())
-    self._x.SetRange(self._z.Min() - self._y.Max(),
-                     self._z.Max() - self._y.Min())
-    self._y.SetRange(self._z.Min() - self._x.Max(),
-                     self._z.Max() - self._x.Min())
-
-
-def test_sum_constraint():
-  print 'test_sum_constraint'
-  solver = pywrapcp.Solver('test_sum_constraint')
-  x = solver.IntVar(1, 5, 'x')
-  y = solver.IntVar(1, 5, 'y')
-  z = solver.IntVar(1, 5, 'z')
-  binary_sum = BinarySum(solver, x, y, z)
-  solver.Add(binary_sum)
-  db = solver.Phase([x, y, z], solver.CHOOSE_FIRST_UNBOUND,
-                    solver.ASSIGN_MIN_VALUE)
-  solver.NewSearch(db)
-  while solver.NextSolution():
-    print '%d + %d == %d' % (x.Value(), y.Value(), z.Value())
-  solver.EndSearch()
+    def InitialPropagate(self):
+        self._z.SetRange(self._x.Min() + self._y.Min(),
+                         self._x.Max() + self._y.Max())
+        self._x.SetRange(self._z.Min() - self._y.Max(),
+                         self._z.Max() - self._y.Min())
+        self._y.SetRange(self._z.Min() - self._x.Max(),
+                         self._z.Max() - self._x.Min())
 
 
-def main():
-  test_member()
-  test_sparse_var()
-  test_modulo()
-  test_limit()
-  # TODO(user): expose ExportModel() in python and re-enable this test.
-  # test_export()
-  test_search_monitor()
-  test_demon()
-  test_failing_constraint()
-  test_constraint()
-  test_domain_iterator()
-  test_hole_iterator()
-  test_sum_constraint()
+class PyWrapCp(unittest.TestCase):
+    def test_member(self):
+        print('test_member')
+        solver = pywrapcp.Solver('test member')
+        x = solver.IntVar(1, 10, 'x')
+        ct = x.Member([1, 2, 3, 5])
+        print(ct)
+
+    def test_sparse_var(self):
+        print('test_sparse_var')
+        solver = pywrapcp.Solver('test_sparse_var')
+        x = solver.IntVar([1, 3, 5], 'x')
+        print(x)
+
+    def test_modulo(self):
+        print('test_modulo')
+        solver = pywrapcp.Solver('test_modulo')
+        x = solver.IntVar(0, 10, 'x')
+        y = solver.IntVar(2, 4, 'y')
+        print(x % 3)
+        print(x % y)
+
+    def test_limit(self):
+        solver = pywrapcp.Solver('test_limit')
+        # TODO(user): expose the proto-based MakeLimit() API in or-tools and test it
+        # here.
+        time = 10000  # ms
+        branches = 10
+        failures = sys.maxsize
+        solutions = sys.maxsize
+        smart_time_check = True
+        cumulative = False
+        limit = solver.Limit(time, branches, failures, solutions,
+                             smart_time_check, cumulative)
+        print(limit)
+
+    def test_search_monitor(self):
+        print('test_search_monitor')
+        solver = pywrapcp.Solver('test search_monitor')
+        x = solver.IntVar(1, 10, 'x')
+        ct = (x == 3)
+        solver.Add(ct)
+        db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND,
+                          solver.ASSIGN_MIN_VALUE)
+        monitor = SearchMonitorTest(solver, x)
+        solver.Solve(db, monitor)
+
+    def test_demon(self):
+        print('test_demon')
+        solver = pywrapcp.Solver('test_demon')
+        x = solver.IntVar(1, 10, 'x')
+        demon = DemonTest(x)
+        demon.Run(solver)
+
+    def test_constraint(self):
+        print('test_constraint')
+        solver = pywrapcp.Solver('test_constraint')
+        x = solver.IntVar(1, 10, 'x')
+        myct = ConstraintTest(solver, x)
+        solver.Add(myct)
+        db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND,
+                          solver.ASSIGN_MIN_VALUE)
+        solver.Solve(db)
+
+    def test_failing_constraint(self):
+        print('test_failing_constraint')
+        solver = pywrapcp.Solver('test failing constraint')
+        x = solver.IntVar(1, 10, 'x')
+        myct = DumbGreaterOrEqualToFive(solver, x)
+        solver.Add(myct)
+        db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND,
+                          solver.ASSIGN_MIN_VALUE)
+        solver.Solve(db)
+
+    def test_domain_iterator(self):
+        print('test_domain_iterator')
+        solver = pywrapcp.Solver('test_domain_iterator')
+        x = solver.IntVar([1, 2, 4, 6], 'x')
+        for i in x.DomainIterator():
+            print(i)
+
+    def test_hole_iterator(self):
+        print('test_hole_iterator')
+        solver = pywrapcp.Solver('test_hole_iterator')
+        x = solver.IntVar(1, 10, 'x')
+        myct = HoleConstraintTest(solver, x)
+        solver.Add(myct)
+        db = solver.Phase([x], solver.CHOOSE_FIRST_UNBOUND,
+                          solver.ASSIGN_MIN_VALUE)
+        solver.Solve(db)
+
+
+    def test_sum_constraint(self):
+        print('test_sum_constraint')
+        solver = pywrapcp.Solver('test_sum_constraint')
+        x = solver.IntVar(1, 5, 'x')
+        y = solver.IntVar(1, 5, 'y')
+        z = solver.IntVar(1, 5, 'z')
+        binary_sum = BinarySum(solver, x, y, z)
+        solver.Add(binary_sum)
+        db = solver.Phase([x, y, z], solver.CHOOSE_FIRST_UNBOUND,
+                          solver.ASSIGN_MIN_VALUE)
+        solver.NewSearch(db)
+        while solver.NextSolution():
+            print('%d + %d == %d' % (x.Value(), y.Value(), z.Value()))
+        solver.EndSearch()
+
 
 
 if __name__ == '__main__':
-  main()
+    unittest.main()
--- examples/tests/pywraplp_test.py
+++ examples/tests/pywraplp_test.py
@@ -17,99 +17,83 @@ import types
 from ortools.linear_solver import linear_solver_pb2
 from ortools.linear_solver import pywraplp
 
+import unittest
 from google.protobuf import text_format
 
-
-def compute_sum(arg):
-  if type(arg) is types.GeneratorType:
-    arg = [x for x in arg]
-  s = 0
-  for i in arg:
-    s += i
-  print 'sum(%s) = %d' % (str(arg), s)
-
-
-def test_sum_no_brackets():
-  compute_sum(x for x in range(10) if x % 2 == 0)
-  compute_sum([x for x in range(10) if x % 2 == 0])
-
-text_model = """
-solver_type:CBC_MIXED_INTEGER_PROGRAMMING
-model <
-  maximize:true
-  variable < lower_bound:1 upper_bound:10 objective_coefficient:2 >
-  variable < lower_bound:1 upper_bound:10 objective_coefficient:1 >
-  constraint < lower_bound:-10000 upper_bound:4
-    var_index:0
-    var_index:1
-    coefficient:1
-    coefficient:2
-  >
->
+TEXT_MODEL = """
+variable {
+  lower_bound: 1.0
+  upper_bound: 10.0
+  objective_coefficient: 2.0
+}
+variable {
+  lower_bound: 1.0
+  upper_bound: 10.0
+  objective_coefficient: 1.0
+}
+constraint {
+  lower_bound: -10000.0
+  upper_bound: 4.0
+  var_index: 0
+  var_index: 1
+  coefficient: 1.0
+  coefficient: 2.0
+}
 """
 
 
-def test_proto():
-  input_proto = linear_solver_pb2.MPModelRequest()
-  text_format.Merge(text_model, input_proto)
-  solver = pywraplp.Solver('solveFromProto',
-                           pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
-  print input_proto
-  # For now, create the model from the proto by parsing the proto
-  errors = solver.LoadModelFromProto(input_proto.model)
-  assert not errors
-  solver.EnableOutput()
-  solver.Solve()
-  # Fill solution
-  solution = linear_solver_pb2.MPSolutionResponse()
-  solver.FillSolutionResponseProto(solution)
-  print solution
+class PyWrapLp(unittest.TestCase):
+    def test_proto(self):
+        input_proto = linear_solver_pb2.MPModelProto()
+        text_format.Merge(TEXT_MODEL, input_proto)
+        solver = pywraplp.Solver('solveFromProto',
+                                 pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+        # For now, create the model from the proto by parsing the proto
+        errors = solver.LoadModelFromProto(input_proto)
+        self.assertFalse(errors)
+        solver.Solve()
+        # Fill solution
+        solution = linear_solver_pb2.MPSolutionResponse()
+        solver.FillSolutionResponseProto(solution)
+        self.assertEqual(solution.objective_value, 3.0)
+        self.assertEqual(solution.variable_value[0], 1.0)
+        self.assertEqual(solution.variable_value[1], 1.0)
+        self.assertEqual(solution.best_objective_bound, 3.0)
 
+    def test_external_api(self):
+        solver = pywraplp.Solver('TestExternalAPI',
+                                 pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        infinity = solver.Infinity()
+        infinity2 = solver.infinity()
+        self.assertEqual(infinity, infinity2)
+        # x1, x2 and x3 are continuous non-negative variables.
+        x1 = solver.NumVar(0.0, infinity, 'x1')
+        x2 = solver.NumVar(0.0, infinity, 'x2')
+        x3 = solver.NumVar(0.0, infinity, 'x3')
+        self.assertEqual(x1.Lb(), 0)
+        self.assertEqual(x1.Ub(), infinity)
+        self.assertFalse(x1.Integer())
+        solver.Maximize(10 * x1 + 6 * x2 + 4 * x3 + 5)
+        self.assertEqual(solver.Objective().Offset(), 5)
+        c0 = solver.Add(10 * x1 + 4 * x2 + 5 * x3 <= 600, 'ConstraintName0')
+        c1 = solver.Add(2 * x1 + 2 * x2 + 6 * x3 <= 300)
+        sum_of_vars = sum([x1, x2, x3])
+        solver.Add(sum_of_vars <= 100.0, 'OtherConstraintName')
+        self.assertEqual(c1.Lb(), -infinity)
+        self.assertEqual(c1.Ub(), 300)
+        c1.SetLb(-100000)
+        self.assertEqual(c1.Lb(), -100000)
+        c1.SetUb(301)
+        self.assertEqual(c1.Ub(), 301)
 
-def test_external_api():
-  solver = pywraplp.Solver('TestExternalAPI',
-                           pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-  infinity = solver.Infinity()
-  infinity2 = solver.infinity()
-  assert infinity == infinity2
-  # x1, x2 and x3 are continuous non-negative variables.
-  x1 = solver.NumVar(0.0, infinity, 'x1')
-  x2 = solver.NumVar(0.0, infinity, 'x2')
-  x3 = solver.NumVar(0.0, infinity, 'x3')
-  assert x1.Lb() == 0
-  assert x1.Ub() == infinity
-  assert not x1.Integer()
-  solver.Maximize(10 * x1 + 6 * x2 + 4 * x3 + 5)
-  assert solver.Objective().Offset() == 5
-  c0 = solver.Add(10 * x1 + 4 * x2 + 5 * x3 <= 600, 'ConstraintName0')
-  c1 = solver.Add(2 * x1 + 2 * x2 + 6 * x3 <= 300)
-  sum_of_vars = sum([x1, x2, x3])
-  solver.Add(sum_of_vars <= 100.0, 'OtherConstraintName')
-  assert c1.Lb() == -infinity
-  assert c1.Ub() == 300
-  c1.SetLb(-100000)
-  assert c1.Lb() == -100000
-  c1.SetUb(301)
-  assert c1.Ub() == 301
+        solver.SetTimeLimit(10000)
+        result_status = solver.Solve()
 
-  solver.SetTimeLimit(10000)
-  result_status = solver.Solve()
-
-  # The problem has an optimal solution.
-  assert result_status == pywraplp.Solver.OPTIMAL
-  print 'Problem solved in %f milliseconds' % solver.WallTime()
-  print 'Problem solved in %d iterations' % solver.Iterations()
-  print x1.ReducedCost()
-  print c0.DualValue()
-
-
-def main():
-  test_sum_no_brackets()
-  # TODO(user): Support the proto API in or-tools. When this happens, re-enable
-  # the test below:
-  # test_proto()
-  test_external_api()
+        # The problem has an optimal solution.
+        self.assertEqual(result_status, pywraplp.Solver.OPTIMAL)
+        self.assertAlmostEqual(x1.ReducedCost(), 0.0)
+        self.assertAlmostEqual(c0.DualValue(), 0.6666666666666667)
 
 
 if __name__ == '__main__':
-  main()
+    unittest.main()
