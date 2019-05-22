01710d141fe1b64428061cc565f20e742bcca9bd
dimitar.gueorguiev <dimitar.gueorguiev@celect.comw>
2019-05-22 10:29:31 -0400

    forecaster class: work in progress

--- ortools/forecaster/fourier_forecaster.cc
+++ ortools/forecaster/fourier_forecaster.cc
@@ -4,22 +4,47 @@
 namespace operations_research {
 namespace forecaster {
 
+//TODO (dpg):
 FourierForecaster::FourierForecaster() {                                                                                                        
 }		
 
+//TODO (dpg):
 Forecaster::ForecasterType FourierForecaster::GetType() {
    return Forecaster::ForecasterType::Fourier; 
 }
 
+//TODO (dpg):
 FourierForecaster::~FourierForecaster() {
 
 }
 
+//TODO (dpg): 
 FFT1DTransform::FFT1DTransform() {
 
 }
 
+//TODO (dpg): 
+FFT1DTransform::~FFT1DTransform() {
 
+}
+
+const fftw_complex * FFT1DTransform::get_result() {
+   return out_;
+}
+
+void FFT1DTransform::set_input(fftw_complex* in) {
+   in_ = in; 
+}
+
+//TODO (dpg): 
+IFFT1DTransform::IFFT1DTransform() {
+
+}
+
+//TODO (dpg):
+IFFT1DTransform::~IFFT1DTransform() {
+
+}
 
 } // ns: forecaster
 
--- ortools/forecaster/fourier_forecaster.h
+++ ortools/forecaster/fourier_forecaster.h
@@ -2,24 +2,55 @@
 #define OR_TOOLS_FORECASTER_FOURTIER_FORECASTER_H_
 #include <vector>
 #include <string>
+#include <memory>
 #include <map>
+#include <complex>
 #include "forecaster.h"
 #include "fftw3.h"
 
 namespace operations_research {
 namespace forecaster {
 
+class FFT1DTransform;
+class IFFT1DTransform;
+
 class FourierForecaster : public Forecaster {
   public:
      FourierForecaster();
      ForecasterType GetType();
      ~FourierForecaster();
+  protected:
+     std::unique_ptr<IFFT1DTransform> m_fft1d_transform;
 };
 
-class FFT1DTransform {
+class AbstractTransform {
+  public:
+    enum TransformError {
+      SUCCESS=0,
+      MISSING_DATA=1,
+      INTERNAL_ERROR=2,
+      UNSPECIFIED=4
+    };
+
+    virtual void set_input(fftw_complex* in) = 0;
+    virtual void execute(fftw_complex* in, int N) = 0;
+    virtual const fftw_complex* get_result() = 0;
+    virtual void clear() = 0; 
+
+
+};
+
+class FFT1DTransform : public AbstractTransform {
   public:
       FFT1DTransform();
       ~FFT1DTransform();
+      void set_input(fftw_complex* in) override;
+      void execute(fftw_complex* in, int N) override;
+      const fftw_complex* get_result() override;
+      void clear() override;
+  protected:
+      fftw_complex * in_;
+      fftw_complex * out_;
 
 };
 
