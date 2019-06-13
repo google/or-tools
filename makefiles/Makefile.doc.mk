8993faa4f8e1fd56e007806c4023c273ceb1d4ae
Laurent Perron <lperron@corp.google.com>
2019-06-13 13:49:59 +0200

    fix doc

--- makefiles/Makefile.doc.mk
+++ makefiles/Makefile.doc.mk
@@ -7,5 +7,5 @@ java-doc:
 	doxygen tools/java.doxy
 
 python-doc:
-	pdoc3 --html -o docs/python/ortools/sat/python/cp_model.html ortools/sat/python/cp_model.py
-	pdoc3 --html -o docs/python/ortools/util/sorted_interval_list.html ortools/gen/ortools/util/sorted_interval_list.py
+	pdoc3 --html -o docs/python/ortools/sat/python/ ortools/sat/python/cp_model.py
+	pdoc3 --html -o docs/python/ortools/util/ ortools/gen/ortools/util/sorted_interval_list.py
