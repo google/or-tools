4ec1e5dc9ba6b52d5c9c049ab198bbfd40e7d767
Laurent Perron <lperron@google.com>
2019-06-20 14:35:04 +0200

    bump dependencies' versions

--- makefiles/Makefile.doc.mk
+++ makefiles/Makefile.doc.mk
@@ -10,5 +10,5 @@ java-doc:
 	doxygen tools/java.doxy
 
 python-doc:
-	pdoc3 --html -o docs/python/ortools/sat/python/ ortools/sat/python/cp_model.py
-	pdoc3 --html -o docs/python/ortools/util/ ortools/gen/ortools/util/sorted_interval_list.py
+	$(SET_PYTHONPATH) pdoc3 --html -o docs/python/ortools/sat/python/ ortools/sat/python/cp_model.py
+	$(SET_PYTHONPATH) pdoc3 --html -o docs/python/ortools/util/ ortools/gen/ortools/util/sorted_interval_list.py
