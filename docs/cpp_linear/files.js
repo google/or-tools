bfb86b20a086210d522397486a126ad024656d07
Laurent Perron <lperron@google.com>
2019-07-15 14:27:40 -0700

    first pass at linear solver doc

--- docs/cpp_linear/files.js
+++ docs/cpp_linear/files.js
@@ -1,6 +1,6 @@
 var files =
 [
-    [ "linear_solver.h", "linear__solver_8h.html", null ],
+    [ "linear_solver.h", "linear__solver_8h.html", "linear__solver_8h" ],
     [ "linear_solver.pb.h", "linear__solver_8pb_8h.html", "linear__solver_8pb_8h" ],
     [ "model_exporter.h", "model__exporter_8h.html", "model__exporter_8h" ],
     [ "model_exporter_swig_helper.h", "model__exporter__swig__helper_8h.html", "model__exporter__swig__helper_8h" ],
---
+++ docs/cpp_linear/linear__solver_8h.js
@@ -0,0 +1,15 @@
+var linear__solver_8h =
+[
+    [ "MPSolver", "classoperations__research_1_1MPSolver.html", "classoperations__research_1_1MPSolver" ],
+    [ "MPObjective", "classoperations__research_1_1MPObjective.html", "classoperations__research_1_1MPObjective" ],
+    [ "MPVariable", "classoperations__research_1_1MPVariable.html", "classoperations__research_1_1MPVariable" ],
+    [ "MPConstraint", "classoperations__research_1_1MPConstraint.html", "classoperations__research_1_1MPConstraint" ],
+    [ "MPSolverParameters", "classoperations__research_1_1MPSolverParameters.html", "classoperations__research_1_1MPSolverParameters" ],
+    [ "MPSolverInterface", "classoperations__research_1_1MPSolverInterface.html", "classoperations__research_1_1MPSolverInterface" ],
+    [ "AbslParseFlag", "linear__solver_8h.html#a61dc18a85425d0a7cf6aa3e7ce3199f6", null ],
+    [ "AbslUnparseFlag", "linear__solver_8h.html#af04d1dfc591c35038a974202e50e541f", null ],
+    [ "operator<<", "linear__solver_8h.html#a2610f938f233d0adcd3142693f4a2683", null ],
+    [ "operator<<", "linear__solver_8h.html#a6e3ed7b755e2b756ef48c9b3bad4a780", null ],
+    [ "ToString", "linear__solver_8h.html#afc3e3b80841b587c6fbfd9e9f3ec9c59", null ],
+    [ "kDefaultPrimalTolerance", "linear__solver_8h.html#a07189276cc680928dad51ed197142077", null ]
+];
