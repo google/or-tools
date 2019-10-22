e21ac3b9ba8387beed45f04057f9e9b73e8df26b
Laurent Perron <lperron@google.com>
2019-10-22 15:04:20 +0200

    upgrade adjustable pq

--- ortools/base/adjustable_priority_queue-inl.h
+++ ortools/base/adjustable_priority_queue-inl.h
@@ -16,4 +16,182 @@
 
 #include "ortools/base/adjustable_priority_queue.h"
 
+namespace util {
+namespace pq {
+
+// Adjusts a heap so as to move a hole at position i closer to the root,
+// until a suitable position is found for element t.  Then, copies t into that
+// position.  This function is a slight generalization of std::push_heap() in
+// that it takes a pointer to an IndexMaintainer functor.  This functor is
+// called each time immediately after modifying a value in the underlying
+// container, with the offset of the modified element as its argument.
+template <typename RandomAccessIterator, typename Distance, typename T,
+          typename Compare, typename IndexMaintainer>
+inline void AdjustUpwards(const RandomAccessIterator& first, Distance i, T&& t,
+                          const Compare& comp, IndexMaintainer* maintainer) {
+  while (i > Distance(0)) {
+    const Distance parent = (i - 1) / 2;
+    if (!comp(*(first + parent), t)) break;
+    *(first + i) = std::move(*(first + parent));
+    (*maintainer)(i);
+    i = parent;
+  }
+  *(first + i) = std::forward<T>(t);
+  (*maintainer)(i);
+}
+
+// Adjusts a heap so as to move a hole at position i farther away from the root,
+// until a suitable position is found for element t.  Then, copies t into that
+// position.  This function is a generalization of std::pop_heap() in two
+// regards:  One, std::pop_heap() can only treat the first (i.e., maximum)
+// element of a heap as a hole, which is an artificial restriction.  And two,
+// this function takes a pointer to an IndexMaintainer functor, which is
+// called each time immediately after modifying a value in the underlying
+// conatiner, with the offset of the modified element as its argument.
+template <typename RandomAccessIterator, typename Distance, typename T,
+          typename Compare, typename IndexMaintainer>
+inline void AdjustDownwards(const RandomAccessIterator& first, Distance i,
+                            const Distance& length, T&& t, const Compare& comp,
+                            IndexMaintainer* maintainer) {
+  while (true) {
+    const Distance left_child = 1 + 2 * i;
+    if (left_child >= length) break;
+    const Distance right_child = left_child + 1;
+    const Distance& next_i = right_child < length &&
+        comp(*(first + left_child), *(first + right_child)) ?
+        right_child : left_child;
+    if (!comp(t, *(first + next_i)))
+      break;
+    *(first + i) = std::move(*(first + next_i));
+    (*maintainer)(i);
+    i = next_i;
+  }
+  *(first + i) = std::forward<T>(t);
+  (*maintainer)(i);
+}
+
+}  // namespace pq
+}  // namespace util
+
+template <typename T, typename Comparator>
+class LowerPriorityThan {
+ public:
+  explicit LowerPriorityThan(Comparator* compare) : compare_(compare) {}
+  bool operator()(T* a, T* b) const { return (*compare_)(*a, *b); }
+ private:
+  Comparator* compare_;
+};
+
+template <class T, class C, class I>
+inline T* AdjustablePriorityQueue<T, C, I>::Top() {
+  return elems_[0];
+}
+
+template <class T, class C, class I>
+inline const T* AdjustablePriorityQueue<T, C, I>::Top() const {
+  return elems_[0];
+}
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::AllTop(std::vector<T*>* topvec) {
+  topvec->clear();
+  if (Size() == 0) return;
+  std::list<int> need_to_check_children;
+  need_to_check_children.push_back(0);
+  // Implements breadth-first search down tree, stopping whenever
+  // there's an element < top
+  while (!need_to_check_children.empty()) {
+    int ind = need_to_check_children.front();
+    need_to_check_children.pop_front();
+    topvec->push_back(elems_[ind]);
+    int leftchild = 1 + 2 * ind;
+    if (leftchild < Size()) {
+      if (!LowerPriorityThan<T, C>(&c_)(elems_[leftchild], elems_[ind])) {
+        need_to_check_children.push_back(leftchild);
+      }
+      int rightchild = leftchild + 1;
+      if (rightchild < Size() &&
+          !LowerPriorityThan<T, C>(&c_)(elems_[rightchild], elems_[ind])) {
+        need_to_check_children.push_back(rightchild);
+      }
+    }
+  }
+}
+
+template <class T, class C, class I>
+class AQPSetHeapIndex {
+ public:
+  explicit AQPSetHeapIndex(AdjustablePriorityQueue<T, C, I>* queue)
+      : queue_(queue) {}
+  void operator()(int offset) const {
+    return queue_->imanip_.SetHeapIndex(queue_->elems_[offset], offset);
+  }
+
+ private:
+  AdjustablePriorityQueue<T, C, I>* queue_;
+};
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::Add(T* val) {
+  AQPSetHeapIndex<T, C, I> imanip(this);
+  // Extend the size of the vector by one.  We could just use
+  // vector<T>::resize(), but maybe T is not default-constructible.
+  elems_.push_back(val);
+  util::pq::AdjustUpwards(elems_.begin(), Size() - 1, val,
+                          LowerPriorityThan<T, C>(&c_), &imanip);
+}
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::NoteChangedPriority(T* val) {
+  AQPSetHeapIndex<T, C, I> imanip(this);
+  LowerPriorityThan<T, C> lower_priority(&c_);
+  int i = imanip_.GetHeapIndex(*val);
+  int parent = (i - 1) / 2;
+  if (lower_priority(elems_[parent], val)) {
+    util::pq::AdjustUpwards(elems_.begin(), i, val, lower_priority, &imanip);
+  } else {
+    util::pq::AdjustDownwards(elems_.begin(), i, Size(), val, lower_priority,
+                              &imanip);
+  }
+}
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::Remove(T* val) {
+  int end = elems_.size() - 1;
+  int i = imanip_.GetHeapIndex(*val);
+  if (i == end) {
+    elems_.pop_back();
+    return;
+  }
+  elems_[i] = elems_[end];
+  imanip_.SetHeapIndex(elems_[i], i);
+  elems_.pop_back();
+  NoteChangedPriority(elems_[i]);
+}
+
+template <class T, class C, class I>
+bool AdjustablePriorityQueue<T, C, I>::Contains(const T* val) const {
+  int i = imanip_.GetHeapIndex(*val);
+  return (i >= 0 && i < elems_.size() && elems_[i] == val);
+}
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::Clear() {
+  elems_.clear();
+}
+
+template <class T, class C, class I>
+void AdjustablePriorityQueue<T, C, I>::CheckValid() {
+  for (int i = 0; i < elems_.size(); ++i) {
+    int left_child = 1 + 2*i;
+    if (left_child < elems_.size()) {
+      CHECK(!(LowerPriorityThan<T, C>(&c_))(elems_[i], elems_[left_child]));
+    }
+    int right_child = left_child + 1;
+    if (right_child < elems_.size()) {
+      CHECK(!(LowerPriorityThan<T, C>(&c_))(elems_[i], elems_[right_child]));
+    }
+  }
+}
+
 #endif  // OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_INL_H_
--- ortools/base/adjustable_priority_queue.h
+++ ortools/base/adjustable_priority_queue.h
@@ -11,6 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_H_
+#define OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_H_
+
 #include <functional>
 #include <list>
 #include <vector>
@@ -19,75 +22,64 @@
 #include "ortools/base/logging.h"
 #include "ortools/base/macros.h"
 
-#ifndef OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_H_
-#define OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_H_
-
-namespace operations_research {
-
-template <typename T>
-class AdjustablePriorityQueue {
+template <class T> class APQDefaultHeapIndexManip {
  public:
+  void SetHeapIndex(T* t, int h) const { t->SetHeapIndex(h); }
+  int GetHeapIndex(const T& t) const { return t.GetHeapIndex(); }
+};
+
+template <class T, class Comp, class HeapIndexManip>
+class AQPSetHeapIndex;
+
+template<typename T, typename Comp = std::less<T>,
+         typename HeapIndexManip = APQDefaultHeapIndexManip<T> >
+class AdjustablePriorityQueue {
+  friend class AQPSetHeapIndex<T, Comp, HeapIndexManip>;
+ public:
+  // The objects references 'c' and 'm' are not required to be alive for the
+  // lifetime of this object.
   AdjustablePriorityQueue() {}
+  AdjustablePriorityQueue(const Comp& c) : c_(c) {}
+  AdjustablePriorityQueue(const Comp& c, const HeapIndexManip& m)
+      : c_(c), imanip_(m) {}
+  AdjustablePriorityQueue(const AdjustablePriorityQueue&) = delete;
+  AdjustablePriorityQueue& operator=(const AdjustablePriorityQueue&) = delete;
+  AdjustablePriorityQueue(AdjustablePriorityQueue&&) = default;
+  AdjustablePriorityQueue& operator=(AdjustablePriorityQueue&&) = default;
 
-  void Add(T* const val) {
-    elems_.push_back(val);
-    AdjustUpwards(elems_.size() - 1);
-  }
+  void Add(T* val);
 
-  void Remove(T* const val) {
-    const int i = val->GetHeapIndex();
-    if (i == elems_.size() - 1) {
-      elems_.pop_back();
-      return;
-    }
-    elems_[i] = elems_.back();
-    elems_[i]->SetHeapIndex(i);
-    elems_.pop_back();
-    NoteChangedPriority(elems_[i]);
-  }
+  void Remove(T* val);
 
-  bool Contains(const T* const val) const {
-    const int i = val->GetHeapIndex();
-    if (i < 0 || i >= elems_.size() || elems_[i] != val) {
-      return false;
-    }
-    return true;
-  }
+  bool Contains(const T* val) const;
 
-  void NoteChangedPriority(T* val) {
-    const int i = val->GetHeapIndex();
-    const int parent = (i - 1) / 2;
-    if (*elems_[parent] < *val) {
-      AdjustUpwards(i);
-    } else {
-      AdjustDownwards(i);
-    }
-  }
+  void NoteChangedPriority(T* val);
+  // If val ever changes its priority, you need to call this function
+  // to notify the pq so it can move it in the heap accordingly.
 
-  T* Top() { return elems_[0]; }
+  T* Top();
+  const T* Top() const;
 
-  const T* Top() const { return elems_[0]; }
+  void AllTop(std::vector<T*>* topvec);
+  // If there are ties for the top, this returns all of them.
 
   void Pop() { Remove(Top()); }
 
   int Size() const { return elems_.size(); }
 
+  // Returns the number of elements for which storage has been allocated.
+  int Capacity() const { return elems_.capacity(); }
+
+  // Allocates storage for a given number of elements.
+  void SetCapacity(size_t c) { elems_.reserve(c); }
+
   bool IsEmpty() const { return elems_.empty(); }
 
-  void Clear() { elems_.clear(); }
+  void Clear();
 
-  void CheckValid() const {
-    for (int i = 0; i < elems_.size(); ++i) {
-      const int left_child = 1 + 2 * i;
-      if (left_child < elems_.size()) {
-        CHECK_GE(elems_[i], elems_[left_child]);
-      }
-      const int right_child = left_child + 1;
-      if (right_child < elems_.size()) {
-        CHECK_GE(elems_[i], elems_[right_child]);
-      }
-    }
-  }
+  // CHECKs that the heap is actually a heap (each "parent" of >=
+  // priority than its child).
+  void CheckValid();
 
   // This is for debugging, e.g. the caller can use it to
   // examine the heap for rationality w.r.t. other parts of the
@@ -95,47 +87,9 @@ class AdjustablePriorityQueue {
   const std::vector<T*>* Raw() const { return &elems_; }
 
  private:
-  void AdjustUpwards(int i) {
-    T* const t = elems_[i];
-    while (i > 0) {
-      const int parent = (i - 1) / 2;
-      if (!(*elems_[parent] < *t)) {
-        break;
-      }
-      elems_[i] = elems_[parent];
-      elems_[i]->SetHeapIndex(i);
-      i = parent;
-    }
-    elems_[i] = t;
-    t->SetHeapIndex(i);
-  }
-
-  void AdjustDownwards(int i) {
-    T* const t = elems_[i];
-    while (true) {
-      const int left_child = 1 + 2 * i;
-      if (left_child >= elems_.size()) {
-        break;
-      }
-      const int right_child = left_child + 1;
-      const int next_i = (right_child < elems_.size() &&
-                          *elems_[left_child] < *elems_[right_child])
-                             ? right_child
-                             : left_child;
-      if (!(*t < *elems_[next_i])) {
-        break;
-      }
-      elems_[i] = elems_[next_i];
-      elems_[i]->SetHeapIndex(i);
-      i = next_i;
-    }
-    elems_[i] = t;
-    t->SetHeapIndex(i);
-  }
-
+  Comp c_;
+  HeapIndexManip imanip_;
   std::vector<T*> elems_;
-  DISALLOW_COPY_AND_ASSIGN(AdjustablePriorityQueue);
 };
-}  // namespace operations_research
 
 #endif  // OR_TOOLS_BASE_ADJUSTABLE_PRIORITY_QUEUE_H_
