604acf554988bce2a02caf941245550313292060
Laurent Perron <lperron@google.com>
2019-11-28 14:31:05 +0100

    fix example

--- ortools/linear_solver/samples/MipVarArray.cs
+++ ortools/linear_solver/samples/MipVarArray.cs
@@ -19,15 +19,16 @@ using Google.OrTools.LinearSolver;
 
 public class MipVarArray
 {
-  class DataModel {
+  class DataModel
+  {
     public double[,] ConstraintCoeffs = {
       {5, 7, 9, 2, 1},
       {18, 4, -9, 10, 12},
       {4, 7, 3, 8, 5},
       {5, 13, 16, 3, -7},
     };
-    public double[] Bounds{250, 285, 211, 315};
-    public double[] ObjCoeffs{ 7, 8, 2, 9, 6};
+    public double[] Bounds = { 250, 285, 211, 315 };
+    public double[] ObjCoeffs = { 7, 8, 2, 9, 6 };
     public int NumVars = 5;
     public int NumConstraints = 4;
   }
@@ -45,16 +46,19 @@ public class MipVarArray
 
     // [START variables]
     MPVariable[] x = new MPVariable[data.NumVars];
-    for (int j = 0; j < data.NumVars; j++) {
+    for (int j = 0; j < data.NumVars; j++)
+    {
       x[j] = MakeIntVar(0.0, double.PositiveInfinity, "x");
     }
     Console.WriteLine("Number of variables = " + solver.NumVariables());
     // [END variables]
 
     // [START constraints]
-    for (int i = 0; i < data.NumConstraints; ++i) {
+    for (int i = 0; i < data.NumConstraints; ++i)
+    {
       MPConstraint constraint = solver.MakeConstraint(0, data.Bounds[i], "");
-      for (int j = 0; j < data.NumVars; ++j) {
+      for (int j = 0; j < data.NumVars; ++j)
+      {
         constraint.SetCoefficient(x[j], data.ConstraintCoeffs[i][j]);
       }
     }
@@ -62,45 +66,49 @@ public class MipVarArray
     // [END constraints]
 
     // [START objective]
-    objective = solver.Objective()
-    for (int j = 0; j < data.NumVars; ++j) {
-      objective.SetCoefficient(x[j], data.ObjCoeffs[j])
-    objective.SetMaximization()
-    // [END objective]
-
-    // [START solve]
-    Solver.ResultStatus resultStatus = solver.Solve();
-    // Check that the problem has an optimal solution.
-    if (resultStatus != Solver.ResultStatus.OPTIMAL)
+    objective = solver.Objective();
+    for (int j = 0; j < data.NumVars; ++j)
     {
-      Console.WriteLine("The problem does not have an optimal solution!");
-      return;
+      objective.SetCoefficient(x[j], data.ObjCoeffs[j]);
+      objective.SetMaximization();
+      // [END objective]
+
+      // [START solve]
+      Solver.ResultStatus resultStatus = solver.Solve();
+      // Check that the problem has an optimal solution.
+      if (resultStatus != Solver.ResultStatus.OPTIMAL)
+      {
+        Console.WriteLine("The problem does not have an optimal solution!");
+        return;
+      }
+      // [END solve]
+
+      // [START print_solution]
+      Console.WriteLine("Solution:");
+      Console.WriteLine("Objective value = " + solver.Objective().Value());
+      const MPSolver::ResultStatus result_status = solver.Solve();
+      // Check that the problem has an optimal solution.
+      if (result_status != MPSolver::OPTIMAL)
+      {
+        Console.WriteLine("The problem does not have an optimal solution.");
+      }
+
+      Console.WriteLine("Solution:");
+      Console.WriteLine("Optimal objective value = " + solver.Objective().Value());
+
+      for (int j = 0; j < data.num_vars; ++j)
+      {
+        Console.WriteLine("x[" + j + "] = " + x[j].SolutionValue());
+      }
+      // [END print_solution]
+
+      // [START advanced]
+      Console.WriteLine("\nAdvanced usage:");
+      Console.WriteLine("Problem solved in " + solver.WallTime() + " milliseconds");
+      Console.WriteLine("Problem solved in " + solver.Iterations() + " iterations");
+      Console.WriteLine("Problem solved in " + solver.Nodes() + " branch-and-bound nodes");
+      // [END advanced]
     }
-    // [END solve]
-
-    // [START print_solution]
-    Console.WriteLine("Solution:");
-    Console.WriteLine("Objective value = " + solver.Objective().Value());
-    const MPSolver::ResultStatus result_status = solver.Solve();
-    // Check that the problem has an optimal solution.
-    if (result_status != MPSolver::OPTIMAL) {
-      Console.WriteLine("The problem does not have an optimal solution.");
-    }
-
-    Console.WriteLine("Solution:");
-    Console.WriteLine("Optimal objective value = " + solver.Objective().Value());
-
-    for (int j = 0; j < data.num_vars; ++j) {
-      Console.WriteLine("x[" + j + "] = " + x[j].SolutionValue());
-    }
-    // [END print_solution]
-
-    // [START advanced]
-    Console.WriteLine("\nAdvanced usage:");
-    Console.WriteLine("Problem solved in " + solver.WallTime() + " milliseconds");
-    Console.WriteLine("Problem solved in " + solver.Iterations() + " iterations");
-    Console.WriteLine("Problem solved in " + solver.Nodes() + " branch-and-bound nodes");
-    // [END advanced]
   }
 }
 // [END program]
