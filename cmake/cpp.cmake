96b582b64f552fc29cde43b5a8b6d2ff9f9a8f37
Corentin Le Molgat <corentinl@google.com>
2020-01-30 11:45:29 +0100

    CMake: prefer to search for config.cmake before looking for modules

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -7,6 +7,11 @@ set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 set(THREAD_PREFER_PTHREAD_FLAG TRUE)
 find_package(Threads REQUIRED)
 
+# Tell find_package() to try “Config” mode before “Module” mode if no mode was specified.
+# This should avoid find_package() to first find our FindXXX.cmake modules if
+# distro package already provide a CMake config file...
+set(CMAKE_FIND_PACKAGE_PREFER_CONFIG TRUE)
+
 # libprotobuf force us to depends on ZLIB::ZLIB target
 if(BUILD_ZLIB)
  find_package(ZLIB REQUIRED CONFIG)
