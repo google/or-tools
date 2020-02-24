4c7456b175520da22870ebd240e50d2dd7f4fcee
Corentin Le Molgat <corentinl@google.com>
2020-02-24 17:26:17 +0100

    CMake: Update Python support
    * Add define SWIGWORDSIZE64 on Linux
    * Add -doxygen option
    * add ortools/util/python
    * Move python generated stuff to BINARY_DIR/python
    * Bump CMake >= 3.14 as requirement
      * SWIG module rework from 3.9 to 3.14
      * Python module rework in 3.12
    * Drop Python 2.7 support (like in Makefile)
    * Fix clean target
      * OUTPUT/BYPRODUCTS path are relative to CURRENT_BINARY_DIR not WORKING_DIR
    * Fix ortools package install in virtualenv
    * Clean target now remove the <build>/venv directory

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -2,6 +2,9 @@ if(NOT BUILD_PYTHON)
   return()
 endif()
 
+# Use latest UseSWIG module
+cmake_minimum_required(VERSION 3.14)
+
 if(NOT TARGET ortools::ortools)
   message(FATAL_ERROR "Python: missing ortools TARGET")
 endif()
@@ -9,6 +12,32 @@ endif()
 # Will need swig
 find_package(SWIG REQUIRED)
 include(UseSWIG)
+
+if(${SWIG_VERSION} VERSION_GREATER_EQUAL 4)
+  list(APPEND CMAKE_SWIG_FLAGS "-doxygen")
+endif()
+
+if(UNIX AND NOT APPLE)
+  list(APPEND CMAKE_SWIG_FLAGS "-DSWIGWORDSIZE64")
+endif()
+
+# Setup Python
+# prefer Python 3.8 over 3.7 over ...
+# user can overwrite it e.g.:
+# cmake -S. -Bbuild -DBUILD_PYTHON=ON -DPython_ADDITIONAL_VERSIONS="3.9"
+set(Python_ADDITIONAL_VERSIONS "3.8;3.7;3.6;3.5" CACHE STRING "Python to use for binding")
+find_package(PythonInterp REQUIRED)
+message(STATUS "Found Python: ${PYTHON_EXECUTABLE} (found version \"${PYTHON_VERSION_STRING}\")")
+
+if(${PYTHON_VERSION_STRING} VERSION_GREATER_EQUAL 3)
+  list(APPEND CMAKE_SWIG_FLAGS "-py3;-DPY3")
+endif()
+
+# Find Python Library
+# Force PythonLibs to find the same version than the python interpreter.
+set(Python_ADDITIONAL_VERSIONS "${PYTHON_VERSION_STRING}")
+find_package(PythonLibs REQUIRED)
+message(STATUS "Found Python Include: ${PYTHON_INCLUDE_DIRS} (found version \"${PYTHONLIBS_VERSION_STRING}\")")
 
 # Generate Protobuf py sources
 set(PROTO_PYS)
@@ -23,13 +52,13 @@ foreach(PROTO_FILE ${proto_py_files})
   #message(STATUS "protoc proto(py): ${PROTO_FILE}")
   get_filename_component(PROTO_DIR ${PROTO_FILE} DIRECTORY)
   get_filename_component(PROTO_NAME ${PROTO_FILE} NAME_WE)
-  set(PROTO_PY ${PROJECT_BINARY_DIR}/${PROTO_DIR}/${PROTO_NAME}_pb2.py)
+  set(PROTO_PY ${PROJECT_BINARY_DIR}/python/${PROTO_DIR}/${PROTO_NAME}_pb2.py)
   #message(STATUS "protoc py: ${PROTO_PY}")
   add_custom_command(
     OUTPUT ${PROTO_PY}
     COMMAND protobuf::protoc
     "--proto_path=${PROJECT_SOURCE_DIR}"
-    "--python_out=${PROJECT_BINARY_DIR}"
+    "--python_out=${PROJECT_BINARY_DIR}/python"
     ${PROTO_FILE}
     DEPENDS ${PROTO_FILE} protobuf::protoc
     COMMENT "Running C++ protocol buffer compiler on ${PROTO_FILE}"
@@ -38,55 +67,46 @@ foreach(PROTO_FILE ${proto_py_files})
 endforeach()
 add_custom_target(Py${PROJECT_NAME}_proto DEPENDS ${PROTO_PYS} ortools::ortools)
 
-# Setup Python
-set(Python_ADDITIONAL_VERSIONS "3.8;3.7;3.6;3.5;2.7" CACHE STRING "Python to use for binding")
-find_package(PythonInterp REQUIRED)
-# Force PythonLibs to find the same version than the python interpreter.
-set(Python_ADDITIONAL_VERSIONS "${PYTHON_VERSION_STRING}")
-# PythonLibs require enable_language(CXX)
-enable_language(CXX)
-find_package(PythonLibs REQUIRED)
-
-if(${PYTHON_VERSION_STRING} VERSION_GREATER 3)
-  set(CMAKE_SWIG_FLAGS "-py3;-DPY3")
-endif()
-
 # CMake will remove all '-D' prefix (i.e. -DUSE_FOO become USE_FOO)
 #get_target_property(FLAGS ortools::ortools COMPILE_DEFINITIONS)
 set(FLAGS -DUSE_BOP -DUSE_GLOP -DABSL_MUST_USE_RESULT)
 if(USE_COINOR)
   list(APPEND FLAGS
-     "-DUSE_CBC"
-     "-DUSE_CLP"
-  )
+    "-DUSE_CBC"
+    "-DUSE_CLP"
+    )
 endif()
 list(APPEND CMAKE_SWIG_FLAGS ${FLAGS} "-I${PROJECT_SOURCE_DIR}")
 
-foreach(SUBPROJECT constraint_solver linear_solver sat graph algorithms data)
+foreach(SUBPROJECT constraint_solver linear_solver sat graph algorithms data util)
   add_subdirectory(ortools/${SUBPROJECT}/python)
 endforeach()
 
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/util COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/constraint_solver/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/linear_solver/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/sat/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/sat/python COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/graph/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/algorithms/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/__init__.py ${PROJECT_BINARY_DIR}/ortools/data/ COPYONLY)
+#######################
+## Python Packaging  ##
+#######################
+#file(MAKE_DIRECTORY python/${PROJECT_NAME})
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME})
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/util)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/constraint_solver)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/linear_solver)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/sat)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/sat/python)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/graph)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/algorithms)
+file(COPY ortools/__init__.py DESTINATION python/${PROJECT_NAME}/data)
 
-configure_file(${PROJECT_SOURCE_DIR}/ortools/linear_solver/linear_solver_natural_api.py
-  ${PROJECT_BINARY_DIR}/ortools/linear_solver/ COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/sat/python/cp_model.py
-  ${PROJECT_BINARY_DIR}/ortools/sat/python COPYONLY)
-configure_file(${PROJECT_SOURCE_DIR}/ortools/sat/python/visualization.py
-  ${PROJECT_BINARY_DIR}/ortools/sat/python COPYONLY)
+file(COPY ortools/linear_solver/linear_solver_natural_api.py
+  DESTINATION python/ortools/linear_solver)
+file(COPY ortools/sat/python/cp_model.py
+  DESTINATION python/ortools/sat/python)
+file(COPY ortools/sat/python/visualization.py
+  DESTINATION python/ortools/sat/python)
 
 # To use a cmake generator expression (aka $<>), it must be processed at build time
 # i.e. inside a add_custom_command()
 # This command will depend on TARGET(s) in cmake generator expression
-add_custom_command(OUTPUT setup.py dist ${PROJECT_NAME}.egg-info
+add_custom_command(OUTPUT python/setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "from setuptools import dist, find_packages, setup" > setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "" >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "class BinaryDistribution(dist.Distribution):" >> setup.py
@@ -121,12 +141,13 @@ add_custom_command(OUTPUT setup.py dist ${PROJECT_NAME}.egg-info
   COMMAND ${CMAKE_COMMAND} -E echo "  packages=find_packages()," >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  package_data={" >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  'ortools':[$<$<NOT:$<PLATFORM_ID:Windows>>:'.libs/*', '../$<TARGET_SONAME_FILE_NAME:ortools>'>]," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.constraint_solver':['$<TARGET_FILE_NAME:_pywrapcp>', '*.pyi']," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.linear_solver':['$<TARGET_FILE_NAME:_pywraplp>', '*.pyi']," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.sat':['$<TARGET_FILE_NAME:_pywrapsat>', '*.pyi']," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.graph':['$<TARGET_FILE_NAME:_pywrapgraph>']," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.algorithms':['$<TARGET_FILE_NAME:_pywrapknapsack_solver>']," >> setup.py
-  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.data':['$<TARGET_FILE_NAME:_pywraprcpsp>', '*.pyi']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.constraint_solver':['$<TARGET_FILE_NAME:pywrapcp>', '*.pyi']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.linear_solver':['$<TARGET_FILE_NAME:pywraplp>', '*.pyi']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.sat':['$<TARGET_FILE_NAME:pywrapsat>', '*.pyi']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.graph':['$<TARGET_FILE_NAME:pywrapgraph>']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.algorithms':['$<TARGET_FILE_NAME:pywrapknapsack_solver>']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.data':['$<TARGET_FILE_NAME:pywraprcpsp>', '*.pyi']," >> setup.py
+  COMMAND ${CMAKE_COMMAND} -E echo "  'ortools.util':['$<TARGET_FILE_NAME:sorted_interval_list>', '*.pyi']," >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  }," >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  include_package_data=True," >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  install_requires=[" >> setup.py
@@ -151,8 +172,12 @@ add_custom_command(OUTPUT setup.py dist ${PROJECT_NAME}.egg-info
   COMMAND ${CMAKE_COMMAND} -E echo "  'Topic :: Software Development :: Libraries :: Python Modules'" >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo "  ]," >> setup.py
   COMMAND ${CMAKE_COMMAND} -E echo ")" >> setup.py
+  COMMENT "Generate setup.py at build time (to use generator expression)"
+  WORKING_DIRECTORY python
   VERBATIM)
 
+# Find if python module MODULE_NAME is available,
+# if not install it to the Python user install directory.
 function(search_python_module MODULE_NAME)
   execute_process(
     COMMAND ${PYTHON_EXECUTABLE} -c "import ${MODULE_NAME}; print(${MODULE_NAME}.__version__)"
@@ -178,9 +203,28 @@ search_python_module(wheel)
 
 # Main Target
 add_custom_target(python_package ALL
-  DEPENDS setup.py Py${PROJECT_NAME}_proto
+  DEPENDS
+    ortools::ortools
+    Py${PROJECT_NAME}_proto
+    python/setup.py
   COMMAND ${CMAKE_COMMAND} -E remove_directory dist
-  COMMAND ${PYTHON_EXECUTABLE} setup.py bdist bdist_wheel
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${PROJECT_NAME}/.libs
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywrapknapsack_solver> ${PROJECT_NAME}/algorithms
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywrapgraph> ${PROJECT_NAME}/graph
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywrapcp> ${PROJECT_NAME}/constraint_solver
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywraplp> ${PROJECT_NAME}/linear_solver
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywrapsat> ${PROJECT_NAME}/sat
+  COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pywraprcpsp> ${PROJECT_NAME}/data
+  #COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:ortools> ${PROJECT_NAME}/.libs
+  COMMAND ${CMAKE_COMMAND} -E copy $<$<NOT:$<PLATFORM_ID:Windows>>:$<TARGET_SONAME_FILE:ortools>> ${PROJECT_NAME}/.libs
+  #COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_egg bdist_wheel
+  COMMAND ${PYTHON_EXECUTABLE} setup.py bdist_wheel
+  BYPRODUCTS
+    python/${PROJECT_NAME}
+    python/build
+    python/dist
+    python/${PROJECT_NAME}.egg-info
+  WORKING_DIRECTORY python
   )
 
 # Test
@@ -188,23 +232,23 @@ if(BUILD_TESTING)
   # Look for python module virtualenv
   search_python_module(virtualenv)
   # Testing using a vitual environment
-  set(VENV_EXECUTABLE ${PYTHON_EXECUTABLE} -m virtualenv)
   set(VENV_DIR ${CMAKE_BINARY_DIR}/venv)
   if(WIN32)
-    set(VENV_BIN_DIR "${VENV_DIR}\\Scripts")
+    set(VENV_PYTHON_EXECUTABLE "${VENV_DIR}\\Scripts\\python.exe")
   else()
-    set(VENV_BIN_DIR ${VENV_DIR}/bin)
+    set(VENV_PYTHON_EXECUTABLE ${VENV_DIR}/bin/python)
   endif()
   # make a virtualenv to install our python package in it
   add_custom_command(TARGET python_package POST_BUILD
-    COMMAND ${VENV_EXECUTABLE} -p ${PYTHON_EXECUTABLE} ${VENV_DIR}
-    COMMAND ${VENV_BIN_DIR}/python setup.py install
-    COMMAND ${CMAKE_COMMAND} -E copy
-    ${CMAKE_CURRENT_SOURCE_DIR}/test.py.in
-    ${VENV_DIR}/test.py
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    COMMAND ${PYTHON_EXECUTABLE} -m virtualenv -p ${PYTHON_EXECUTABLE} ${VENV_DIR}
+    # Must not call it in a folder containing the setup.py otherwise pip call it
+    # (i.e. "python setup.py bdist") while we want to consume the wheel package
+    COMMAND ${VENV_PYTHON_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PROJECT_NAME}
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/test.py.in ${VENV_DIR}/test.py
+    BYPRODUCTS ${VENV_DIR}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} VERBATIM)
   # run the tests within the virtualenv
-  add_test(pytest_venv ${VENV_BIN_DIR}/python ${VENV_DIR}/test.py)
+  add_test(pytest_venv ${VENV_PYTHON_EXECUTABLE} ${VENV_DIR}/test.py)
 
   #add_subdirectory(examples/python)
   #add_subdirectory(examples/notebook)
