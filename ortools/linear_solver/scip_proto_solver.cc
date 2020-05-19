0b03e40329ebd2a5b292e34a7c1056aba605e928
Laurent Perron <lperron@google.com>
2020-05-19 17:07:48 +0200

    fix scip compilation

--- ortools/linear_solver/scip_proto_solver.cc
+++ ortools/linear_solver/scip_proto_solver.cc
@@ -785,7 +785,7 @@ absl::StatusOr<MPSolutionResponse> ScipSolveProto(
       request.solver_specific_parameters(), scip);
   if (!parameters_status.ok()) {
     response.set_status(MPSOLVER_MODEL_INVALID_SOLVER_PARAMETERS);
-    response.set_status_str(parameters_status.message());
+    response.set_status_str(std::string(parameters_status.message()));
     return response;
   }
   // Default clock type. We use wall clock time because getting CPU user seconds
