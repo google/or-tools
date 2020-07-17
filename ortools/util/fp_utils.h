db874af7fe21231884f79dcae79935c1f164b018
Mizux Seiha <mizux.dev@gmail.com>
2020-07-17 11:07:49 +0200

    Fix FreeBSD support (Fix #2105)

--- ortools/util/fp_utils.h
+++ ortools/util/fp_utils.h
@@ -81,7 +81,7 @@ class ScopedFloatingPointEnv {
 #elif defined(ARCH_K8)
     CHECK_EQ(0, fegetenv(&fenv_));
     excepts &= FE_ALL_EXCEPT;
-#ifdef __APPLE__
+#if defined(__APPLE__) || defined(__FreeBSD__)
     fenv_.__control &= ~excepts;
 #else  // Linux
     fenv_.__control_word &= ~excepts;
