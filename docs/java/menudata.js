bcf05285e3f2967b302ac7dbe1f677f87dd91286
Mizux Seiha <mizux.dev@gmail.com>
2020-09-01 16:00:33 +0200

    Update generated doc

--- docs/java/menudata.js
+++ docs/java/menudata.js
@@ -23,6 +23,6 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Namespaces",url:"namespaces.html"},
+{text:"Packages",url:"namespaces.html"},
 {text:"Classes",url:"annotated.html"},
 {text:"Files",url:"files.html"}]}
