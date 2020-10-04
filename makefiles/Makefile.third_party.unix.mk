65cb31df94e891f748d3a52f01f0b81d057decbb
Laurent Perron <lperron@google.com>
2020-10-04 18:14:49 +0200

    fix

--- makefiles/Makefile.third_party.unix.mk
+++ makefiles/Makefile.third_party.unix.mk
@@ -442,6 +442,7 @@ $(_ABSL_STATIC_LIB_DIR)libabsl_malloc_internal.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_periodic_sampler.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_random_distributions.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_random_internal_distribution_test_util.a \
+$(_ABSL_STATIC_LIB_DIR)libabsl_random_internal_platform.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_random_internal_pool_urbg.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_random_internal_randen.a \
 $(_ABSL_STATIC_LIB_DIR)libabsl_random_internal_randen_hwaes.a \
