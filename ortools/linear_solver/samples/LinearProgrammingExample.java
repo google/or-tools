0e615d5ad98921d299b10f1c12ec83009dbf4d32
Mizux Seiha <mizux.dev@gmail.com>
2020-12-07 16:00:32 +0100

    linear_solver: Add missing import region tag

--- ortools/linear_solver/samples/LinearProgrammingExample.java
+++ ortools/linear_solver/samples/LinearProgrammingExample.java
@@ -13,12 +13,13 @@
 
 // [START program]
 package com.google.ortools.linearsolver.samples;
-
+// [START import]
 import com.google.ortools.Loader;
 import com.google.ortools.linearsolver.MPConstraint;
 import com.google.ortools.linearsolver.MPObjective;
 import com.google.ortools.linearsolver.MPSolver;
 import com.google.ortools.linearsolver.MPVariable;
+// [END import]
 
 /** Simple linear programming example.*/
 public class LinearProgrammingExample {
