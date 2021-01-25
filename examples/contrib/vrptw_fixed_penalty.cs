e5d09b54aeb603acadab7d3da10ac9eafbf8926f
Owen Lacey <owen.lacey@audacia.systems>
2021-01-25 12:25:57 +0000

    Update vrptw_fixed_penalty.cs

--- examples/contrib/vrptw_fixed_penalty.cs
+++ examples/contrib/vrptw_fixed_penalty.cs
@@ -1,4 +1,4 @@
-// Copyright 2012 Hakan Kjellerstrand
+// Copyright 2021 Owen Lacey
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
