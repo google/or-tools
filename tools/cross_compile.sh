7fb3ef66d066404937977b521d5706a8350c0f40
Mizux Seiha <mizux.dev@gmail.com>
2021-01-26 11:07:55 +0100

    Cleanup mips64 scripts
    Still not working yet...

--- tools/cross_compile.sh
+++ tools/cross_compile.sh
@@ -28,6 +28,10 @@ function unpack() {
 }
 
 function install_qemu() {
+  if [[ "${QEMU_ARCH}" == "DISABLED" ]]; then
+    >&2 echo 'QEMU is disabled !'
+    return 0
+  fi
   local -r QEMU_VERSION=${QEMU_VERSION:=5.2.0}
   local -r QEMU_TARGET=${QEMU_ARCH}-linux-user
 
@@ -93,17 +97,14 @@ function clean_build() {
 
 function expand_linaro_config() {
   #ref: https://releases.linaro.org/components/toolchain/binaries/
-  #local -r LINARO_VERSION=7.2-2017.11
   local -r LINARO_VERSION=7.5-2019.12
   local -r LINARO_ROOT_URL=https://releases.linaro.org/components/toolchain/binaries/${LINARO_VERSION}
 
-  #local -r GCC_VERSION=7.2.1-2017.11
   local -r GCC_VERSION=7.5.0-2019.12
   local -r GCC_URL=${LINARO_ROOT_URL}/${TARGET}/gcc-linaro-${GCC_VERSION}-x86_64_${TARGET}.tar.xz
   local -r GCC_RELATIVE_DIR="gcc-linaro-${GCC_VERSION}-x86_64_${TARGET}"
   unpack "${GCC_URL}" "${GCC_RELATIVE_DIR}"
 
-  #local -r SYSROOT_VERSION=2.25-2017.11
   local -r SYSROOT_VERSION=2.25-2019.12
   local -r SYSROOT_URL=${LINARO_ROOT_URL}/${TARGET}/sysroot-glibc-linaro-${SYSROOT_VERSION}-${TARGET}.tar.xz
   local -r SYSROOT_RELATIVE_DIR=sysroot-glibc-linaro-${SYSROOT_VERSION}-${TARGET}
@@ -139,58 +140,73 @@ QEMU_ARGS+=( -E LD_LIBRARY_PATH=/lib )
 }
 
 function expand_codescape_config() {
-  local -r DATE=2020.06-01
-  local -r CODESCAPE_URL=https://codescape.mips.com/components/toolchain/${DATE}/Codescape.GNU.Tools.Package.${DATE}.for.MIPS.MTI.Linux.CentOS-6.x86_64.tar.gz
+  # ref: https://codescape.mips.com/components/toolchain/2019.02-04/downloads.html
+  #local -r DATE=2020.06-01
+  local -r DATE=2019.02-04
+  #local -r CODESCAPE_URL=https://codescape.mips.com/components/toolchain/${DATE}/Codescape.GNU.Tools.Package.${DATE}.for.MIPS.MTI.Linux.CentOS-6.x86_64.tar.gz
+  local -r CODESCAPE_URL=https://codescape.mips.com/components/toolchain/${DATE}/Codescape.GNU.Tools.Package.${DATE}.for.MIPS.IMG.Linux.CentOS-6.x86_64.tar.gz
   local -r GCC_URL=${CODESCAPE_URL}
-  local -r GCC_RELATIVE_DIR="mips-mti-linux-gnu/${DATE}"
+  #local -r GCC_RELATIVE_DIR="mips-mti-linux-gnu/${DATE}"
+  local -r GCC_RELATIVE_DIR="mips-img-linux-gnu/${DATE}"
   unpack "${GCC_URL}" "${GCC_RELATIVE_DIR}"
 
   local -r GCC_DIR=${ARCHIVE_DIR}/${GCC_RELATIVE_DIR}
   local MIPS_FLAGS=""
   local FLAVOUR=""
-  local LIBC_DIR_SUFFIX=""
   case "${TARGET}" in
-    "mips64")    MIPS_FLAGS="-EB -mabi=64"; FLAVOUR="mips-r2-hard"; LIBC_DIR_SUFFIX="lib64" ;;
-    "mips64el")  MIPS_FLAGS="-EL -mabi=64"; FLAVOUR="mipsel-r2-hard"; LIBC_DIR_SUFFIX="lib64" ;;
+    "mips64")
+      MIPS_FLAGS="-EB -mabi=64";
+      #FLAVOUR="mips-r2-hard";
+      FLAVOUR="mips-r6-hard";
+      ;;
+    "mips64el")
+      MIPS_FLAGS="-EL -mabi=64";
+      #FLAVOUR="mipsel-r2-hard"
+      FLAVOUR="mipsel-r6-hard"
+      ;;
     *)
       >&2 echo 'unknown mips platform'
       exit 1 ;;
   esac
-
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_FIND_ROOT_PATH="${GCC_DIR}" )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_SYSTEM_NAME=Linux )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_SYSTEM_PROCESSOR="${TARGET}" )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_C_COMPILER=mips-mti-linux-gnu-gcc )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_CXX_COMPILER=mips-mti-linux-gnu-g++ )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_C_COMPILER_ARG1="${MIPS_FLAGS}" )
-  CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_CXX_COMPILER_ARG1="${MIPS_FLAGS}" )
-
   local -r SYSROOT_DIR=${GCC_DIR}/sysroot/${FLAVOUR}
+  local -r STAGING_DIR=${SYSROOT_DIR}-stage
 
-  # Keeping only the sysroot of interest to save on travis cache.
-  if [[ "${CONTINUOUS_INTEGRATION}" = "true" ]]; then
-    for folder in "${GCC_DIR}"/sysroot/*; do
-      if [[ "${folder}" != "${SYSROOT_DIR}" ]]; then
-        rm -rf "${folder}"
-      fi
-    done
-  fi
+  # Write a Toolchain file
+  # note: This is manadatory to use a file in order to have the CMake variable
+  # 'CMAKE_CROSSCOMPILING' set to TRUE.
+  # ref: https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-linux
+  cat >"$TOOLCHAIN_FILE" <<EOL
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ${TARGET})
 
-  local -r LIBC_DIR=${GCC_DIR}/mips-mti-linux-gnu/lib/${FLAVOUR}/${LIBC_DIR_SUFFIX}
-  QEMU_ARGS+=( -L "${SYSROOT_DIR}" )
-  QEMU_ARGS+=( -E LD_PRELOAD="${LIBC_DIR}/libstdc++.so.6:${LIBC_DIR}/libgcc_s.so.1" )
+set(CMAKE_SYSROOT ${SYSROOT_DIR})
+set(CMAKE_STAGING_PREFIX ${STAGING_DIR})
+
+set(tools ${GCC_DIR})
+set(CMAKE_C_COMPILER \${tools}/bin/mips-img-linux-gnu-gcc)
+set(CMAKE_C_COMPILER_ARG "${MIPS_FLAGS}")
+set(CMAKE_CXX_COMPILER \${tools}/bin/mips-img-linux-gnu-g++)
+set(CMAKE_CXX_FLAGS "${MIPS_FLAGS}")
+set(CMAKE_CXX_FLAGS "${MIPS_FLAGS} -L${SYSROOT_DIR}/usr/lib64")
+
+set(CMAKE_FIND_ROOT_PATH ${GCC_DIR})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+EOL
+
+CMAKE_ADDITIONAL_ARGS+=( -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}" )
+local -r LIBC_DIR=${GCC_DIR}/mips-img-linux-gnu/lib/${FLAVOUR}/lib64
+QEMU_ARGS+=( -L "${SYSROOT_DIR}" )
+QEMU_ARGS+=( -E LD_PRELOAD="${LIBC_DIR}/libstdc++.so.6:${LIBC_DIR}/libgcc_s.so.1" )
 }
 
 function build() {
   cd "${PROJECT_DIR}" || exit 2
-  clean_build
-
-  # CMake Configuration
   set -x
+  clean_build
   cmake -S. -B"${BUILD_DIR}" "${CMAKE_DEFAULT_ARGS[@]}" "${CMAKE_ADDITIONAL_ARGS[@]}"
-
-  # CMake Build
-  #cmake --build "${BUILD_DIR}" --target host_tools -v
   cmake --build "${BUILD_DIR}" --target all -j8 -v
   set +x
 }
@@ -228,7 +244,7 @@ function usage() {
   echo -e "$NAME - Build using a cross toolchain.
 
 SYNOPSIS
-\t$NAME [-h|--help]
+\t$NAME [-h|--help] [toolchain|build|qemu|test|all]
 
 DESCRIPTION
 \tCross compile Google OR-Tools (or Glop) using a cross toolchain.
@@ -239,6 +255,11 @@ DESCRIPTION
 
 OPTIONS
 \t-h --help: show this help text
+\ttoolchain: download, unpack toolchain and generate CMake toolchain file
+\tbuild: toolchain + build the project using the toolchain file (note: remove previous build dir)
+\tqemu: download, unpack and build qemu
+\ttest: qemu + run all executable using qemu (note: don't build !)
+\tall: build + test (default)
 
 EXAMPLES
 * Using export:
@@ -252,12 +273,10 @@ PROJECT=or-tools TARGET=aarch64-linux-gnu $0"
 
 # Main
 function main() {
-  if [[ -n ${1-} ]]; then
-    case $1 in
-      -h | --help)
-        usage; exit ;;
-    esac
-  fi
+  case ${1} in
+    -h | --help)
+      usage; exit ;;
+  esac
 
   assert_defined PROJECT
   assert_defined TARGET
@@ -307,23 +326,21 @@ function main() {
       >&2 echo "Unknown TARGET '${TARGET}'..."
       exit 1 ;;
   esac
-  declare -r QEMU_INSTALL=${ARCHIVE_DIR}/qemu
+  declare -r QEMU_INSTALL=${ARCHIVE_DIR}/qemu-${QEMU_ARCH}
 
-  if [[ -n ${1-} ]]; then
-    case $1 in
-      toolchain)
-        exit ;;
-      build)
-        build ;;
-      qemu)
-        install_qemu ;;
-      test)
-        run_test ;;
-      *)
-        build
-        run_test ;;
-    esac
-  fi
+  case ${1} in
+    toolchain)
+      exit ;;
+    build)
+      build ;;
+    qemu)
+      install_qemu ;;
+    test)
+      run_test ;;
+    *)
+      build
+      run_test ;;
+  esac
 }
 
-main "$1"
+main "${1:-all}"
