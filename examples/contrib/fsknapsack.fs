c0f6978f3b73af4c1a8ec3967dbf27d4ee78efa8
Mizux Seiha <mizux.dev@gmail.com>
2021-04-02 10:08:51 +0200

    Update copyright

--- examples/contrib/fsknapsack.fs
+++ examples/contrib/fsknapsack.fs
@@ -1,5 +1,5 @@
 (*
-Copyright 2010-2017 Google
+Copyright 2010-2021 Google
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
