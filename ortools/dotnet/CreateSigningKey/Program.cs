1f0a909b83060db9dfdac1ea821bf6cffd6faba1
Mizux Seiha <mizux.dev@gmail.com>
2021-04-23 19:51:52 +0200

    dotnet: Remove weird <feff> character in CreateSigningKey

--- ortools/dotnet/CreateSigningKey/Program.cs
+++ ortools/dotnet/CreateSigningKey/Program.cs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-﻿using System;
+using System;
 using System.IO;
 using System.Security.Cryptography;
 
