7382845e5f8df6631bedce4a2f20f3940e21ac09
Laurent Perron <lperron@google.com>
2021-06-11 15:46:46 +0200

    regroup overloaded methods

--- ortools/java/com/google/ortools/sat/LinearExpr.java
+++ ortools/java/com/google/ortools/sat/LinearExpr.java
@@ -84,14 +84,14 @@ public interface LinearExpr {
     return new ScalProd(variable, coefficient, 0);
   }
 
-  /** Creates an affine expression (var * coefficient + offset). */
-  static LinearExpr affine(IntVar variable, long coefficient, long offset) {
-    return new ScalProd(variable, coefficient, offset);
-  }
-
   /** Creates a linear term (lit * coefficient). */
   static LinearExpr term(Literal lit, long coefficient) {
     return new ScalProd(lit, coefficient, 0);
+  }
+
+  /** Creates an affine expression (var * coefficient + offset). */
+  static LinearExpr affine(IntVar variable, long coefficient, long offset) {
+    return new ScalProd(variable, coefficient, offset);
   }
 
   /** Creates an affine expression (lit * coefficient + offset). */
