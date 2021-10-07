4ba2ef5a4e7f369f6c0016412255a6200eace188
Mizux Seiha <mizux.dev@gmail.com>
2021-10-07 13:52:56 +0200

    graph: Update Python simple_max_flow

--- ortools/graph/samples/simple_max_flow_program.py
+++ ortools/graph/samples/simple_max_flow_program.py
@@ -20,20 +20,21 @@ from ortools.graph import pywrapgraph
 
 def main():
     """MaxFlow simple interface example."""
+    # [START solver]
+    # Instantiate a SimpleMaxFlow solver.
+    max_flow = pywrapgraph.SimpleMaxFlow()
+    # [END solver]
 
     # [START data]
     # Define three parallel arrays: start_nodes, end_nodes, and the capacities
     # between each pair. For instance, the arc from node 0 to node 1 has a
     # capacity of 20.
-
     start_nodes = [0, 0, 0, 1, 1, 2, 2, 3, 3]
     end_nodes = [1, 2, 3, 2, 4, 3, 4, 2, 4]
     capacities = [20, 30, 10, 40, 30, 10, 20, 5, 20]
     # [END data]
 
-    # Instantiate a SimpleMaxFlow solver.
     # [START constraints]
-    max_flow = pywrapgraph.SimpleMaxFlow()
     # Add each arc.
     for arc in zip(start_nodes, end_nodes, capacities):
         max_flow.AddArcWithCapacity(arc[0], arc[1], arc[2])
@@ -41,19 +42,24 @@ def main():
 
     # [START solve]
     # Find the maximum flow between node 0 and node 4.
-    if max_flow.Solve(0, 4) == max_flow.OPTIMAL:
-        print('Max flow:', max_flow.OptimalFlow())
-        print('')
-        print('  Arc    Flow / Capacity')
-        for i in range(max_flow.NumArcs()):
-            print('%1s -> %1s   %3s  / %3s' %
-                  (max_flow.Tail(i), max_flow.Head(i), max_flow.Flow(i),
-                   max_flow.Capacity(i)))
-        print('Source side min-cut:', max_flow.GetSourceSideMinCut())
-        print('Sink side min-cut:', max_flow.GetSinkSideMinCut())
-    else:
-        print('There was an issue with the max flow input.')
+    status =  max_flow.Solve(0, 4)
     # [END solve]
+
+    # [START print_solution]
+    if status != max_flow.OPTIMAL:
+        print('There was an issue with the max flow input.')
+        print(f'Status: {status}')
+        exit(1)
+    print('Max flow:', max_flow.OptimalFlow())
+    print('')
+    print('  Arc    Flow / Capacity')
+    for i in range(max_flow.NumArcs()):
+        print('%1s -> %1s   %3s  / %3s' %
+              (max_flow.Tail(i), max_flow.Head(i),
+               max_flow.Flow(i), max_flow.Capacity(i)))
+    print('Source side min-cut:', max_flow.GetSourceSideMinCut())
+    print('Sink side min-cut:', max_flow.GetSinkSideMinCut())
+    # [END print_solution]
 
 
 if __name__ == '__main__':
