32d3362e1d09e2f09324345b59de7bb030b8633f
Mitchell Skaggs <skaggsm333@gmail.com>
2021-10-10 15:43:22 -0500

    Replace directory traversal logic with hardcoded files to extract
    This significantly increases compatibility with alternate ClassLoader implementations like ones implementing the OSGi model

--- ortools/java/com/google/ortools/Loader.java
+++ ortools/java/com/google/ortools/Loader.java
@@ -14,117 +14,73 @@
 package com.google.ortools;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystemAlreadyExistsException;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Collections;
 import java.util.Objects;
 
-/** Load native libraries needed for using ortools-java.*/
+/**
+ * Helper class to load native libraries needed for using ortools-java.
+ */
 public class Loader {
   private static final String RESOURCE_PREFIX;
+  private static final String[] FILES_TO_EXTRACT;
   static {
-    String librarySuffix = System.mapLibraryName("test").split("\\.")[1];
+    String librarySuffix = System.mapLibraryName("test").split("test\\.")[1];
     switch (librarySuffix) {
       case "dll":
         RESOURCE_PREFIX = "win32-x86-64";
+        FILES_TO_EXTRACT = new String[]{"jniortools.dll"};
         break;
       case "so":
         RESOURCE_PREFIX = "linux-x86-64";
+        FILES_TO_EXTRACT = new String[]{"libjniortools.so", "libortools.so"};
         break;
       case "dylib":
         RESOURCE_PREFIX = "darwin-x86-64";
+        FILES_TO_EXTRACT = new String[]{"libjniortools.dylib", "libortools.dylib"};
         break;
       default:
         throw new UnsupportedOperationException(String.format("Unknown library suffix %s!", librarySuffix));
     }
   }
-  private static final String RESOURCE_PATH = "ortools-" + RESOURCE_PREFIX + "/";
-
-  /** Try to locate the native libraries directory.*/
-  private static URI getNativeResourceURI() throws IOException {
-    ClassLoader loader = Loader.class.getClassLoader();
-    URL resourceURL = loader.getResource(RESOURCE_PATH);
-    Objects.requireNonNull(resourceURL,
-        String.format("Resource %s was not found in ClassLoader %s", RESOURCE_PATH, loader));
-
-    URI resourceURI;
-    try {
-      resourceURI = resourceURL.toURI();
-    } catch (URISyntaxException e) {
-      throw new IOException(e);
-    }
-    return resourceURI;
-  }
-
-  @FunctionalInterface
-  private interface PathConsumer<T extends IOException> {
-    void accept(Path path) throws T;
-  }
 
   /**
-   * Extract native resources in a temp directory.
-   * @param resourceURI Native resource location.
-   * @return The directory path containing all extracted libraries.
+   * Extract native resources to a temp directory.
+   * @return The path containing all extracted libraries.
    */
-  private static Path unpackNativeResources(URI resourceURI) throws IOException {
-    Path tempPath;
-    tempPath = Files.createTempDirectory("ortools-java");
+  private static Path unpackNativeResources() throws IOException {
+    Path tempPath = Files.createTempDirectory("ortools-java");
     tempPath.toFile().deleteOnExit();
 
-    PathConsumer<?> visitor;
-    visitor = (Path sourcePath) -> Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
-      @Override
-      public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        Path newPath = tempPath.resolve(sourcePath.getParent().relativize(file).toString());
-        Files.copy(file, newPath);
-        newPath.toFile().deleteOnExit();
-        return FileVisitResult.CONTINUE;
-      }
-
-      @Override
-      public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-          throws IOException {
-        Path newPath = tempPath.resolve(sourcePath.getParent().relativize(dir).toString());
-        Files.copy(dir, newPath);
-        newPath.toFile().deleteOnExit();
-        return FileVisitResult.CONTINUE;
-      }
-    });
-
-    FileSystem fs;
-    try {
-      fs = FileSystems.newFileSystem(resourceURI, Collections.emptyMap());
-    } catch (FileSystemAlreadyExistsException e) {
-      fs = FileSystems.getFileSystem(resourceURI);
-      if (fs == null) {
-        throw new IllegalArgumentException();
+    ClassLoader loader = Loader.class.getClassLoader();
+    for (String file : FILES_TO_EXTRACT) {
+      String fullPath = "ortools-" + RESOURCE_PREFIX + "/" + file;
+      URL url = Objects.requireNonNull(
+              loader.getResource(fullPath),
+              String.format("Resource %s was not found in ClassLoader %s", fullPath, loader)
+      );
+      try (InputStream lib = url.openStream()) {
+        Files.copy(lib, tempPath.resolve(file));
       }
     }
-    Path p = fs.provider().getPath(resourceURI);
-    visitor.accept(p);
+
     return tempPath;
   }
 
-  /** Unpack and Load the native libraries needed for using ortools-java.*/
   private static boolean loaded = false;
-  public static void loadNativeLibraries() {
+
+  /**
+   * Extract and load the native libraries needed for using ortools-java.
+   */
+  public synchronized static void loadNativeLibraries() {
     if (!loaded) {
       try {
-        URI resourceURI = getNativeResourceURI();
-        Path tempPath = unpackNativeResources(resourceURI);
-        // Load the native library
-        System.load(tempPath.resolve(RESOURCE_PATH)
-                        .resolve(System.mapLibraryName("jniortools"))
-                        .toString());
+        // Extract natives
+        Path tempPath = unpackNativeResources();
+        // Load natives
+        System.load(tempPath.resolve(System.mapLibraryName("jniortools")).toString());
         loaded = true;
       } catch (IOException e) {
         throw new RuntimeException(e);
