ddeb35881e7628c1bb1f1aa4e072f6bbd7298c1e
Mizux Seiha <mizux.dev@gmail.com>
2021-11-12 01:32:32 +0100

    cmake: Use virtualenv instead of venv
    windows runner fail when using venv...

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -272,12 +272,15 @@ install(SCRIPT ${PROJECT_BINARY_DIR}/python/python-install.cmake)
 ##  Python Test  ##
 ###################
 if(BUILD_TESTING)
-  search_python_internal_module(NAME venv)
+  search_python_module(NAME virtualenv PACKAGE virtualenv)
+  # venv not working on github runners
+  # search_python_internal_module(NAME venv)
   # Testing using a vitual environment
-  set(VENV_EXECUTABLE ${Python3_EXECUTABLE} -m venv)
+  set(VENV_EXECUTABLE ${Python3_EXECUTABLE} -m virtualenv)
+  #set(VENV_EXECUTABLE ${Python3_EXECUTABLE} -m venv)
   set(VENV_DIR ${CMAKE_CURRENT_BINARY_DIR}/python/venv)
   if(WIN32)
-    set(VENV_Python3_EXECUTABLE "${VENV_DIR}\\Scripts\\python.exe")
+    set(VENV_Python3_EXECUTABLE ${VENV_DIR}/Scripts/python.exe)
   else()
     set(VENV_Python3_EXECUTABLE ${VENV_DIR}/bin/python)
   endif()
@@ -285,7 +288,8 @@ if(BUILD_TESTING)
   add_custom_command(TARGET python_package POST_BUILD
     # Clean previous install otherwise pip install may do nothing
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${VENV_DIR}
-    COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
+    COMMAND ${VENV_EXECUTABLE} -p ${Python3_EXECUTABLE} ${VENV_DIR}
+    #COMMAND ${VENV_EXECUTABLE} ${VENV_DIR}
     # Must NOT call it in a folder containing the setup.py otherwise pip call it
     # (i.e. "python setup.py bdist") while we want to consume the wheel package
     COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
