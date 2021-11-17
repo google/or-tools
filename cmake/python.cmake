f0fdd90c64a4304f0f8817803f35328ff375a728
Mizux Seiha <mizux.dev@gmail.com>
2021-11-17 20:57:19 +0100

    python: install pandas and matplotlib in venv

--- cmake/python.cmake
+++ cmake/python.cmake
@@ -296,6 +296,8 @@ if(BUILD_TESTING)
     # Must NOT call it in a folder containing the setup.py otherwise pip call it
     # (i.e. "python setup.py bdist") while we want to consume the wheel package
     COMMAND ${VENV_Python3_EXECUTABLE} -m pip install --find-links=${CMAKE_CURRENT_BINARY_DIR}/python/dist ${PYTHON_PROJECT}
+    # install modules only required to run examples
+    COMMAND ${VENV_Python3_EXECUTABLE} -m pip install pandas matplotlib
     BYPRODUCTS ${VENV_DIR}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "Create venv and install ${PYTHON_PROJECT}"
