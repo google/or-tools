499b070eaceb94be1ee07a05607e2a263499e361
Laurent Perron <lperron@google.com>
2021-11-29 11:28:07 +0100

    polish BUILD files

--- ortools/base/version.cc
+++ ortools/base/version.cc
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <string>
+#include "ortools/base/version.h"
 
 #include "absl/strings/str_cat.h"
 
