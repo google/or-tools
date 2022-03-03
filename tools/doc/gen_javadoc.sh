319f26f7993c48fb5427f77d1de79c664a9e9074
Mizux Seiha <mizux.dev@gmail.com>
2022-03-04 00:19:37 +0100

    doc: Fix gen_javadoc.sh

--- tools/doc/gen_javadoc.sh
+++ tools/doc/gen_javadoc.sh
@@ -10,5 +10,5 @@ OUTPUT_DIR="docs/javadoc"
 rm -rf ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}
 
-ARCHIVE=$(find "temp_java/ortools-java/target" -iname "ortools-java-${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}.*-javadoc.jar")
+ARCHIVE=$(find "dependencies/java/ortools-java/target" -iname "ortools-java-${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}.*-javadoc.jar")
 (cd ${OUTPUT_DIR} && jar -xvf "../../${ARCHIVE}")
