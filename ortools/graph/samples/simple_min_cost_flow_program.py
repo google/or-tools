1af55b2be37490c97e021edb2bea414cb3bb18dd
Corentin Le Molgat <corentinl@google.com>
2022-06-20 18:27:09 +0200

    Add license boilerplate

--- ortools/graph/samples/simple_min_cost_flow_program.py
+++ ortools/graph/samples/simple_min_cost_flow_program.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # [START program]
 """From Bradley, Hax and Maganti, 'Applied Mathematical Programming', figure 8.1."""
 # [START import]
--- ortools/linear_solver/samples/assignment_task_sizes_mip.py
+++ ortools/linear_solver/samples/assignment_task_sizes_mip.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # [START program]
 """MIP example that solves an assignment problem."""
 # [START import]
