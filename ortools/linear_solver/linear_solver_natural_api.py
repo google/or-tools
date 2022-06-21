5899cbe811a833467e785a6dd0db842cd152bd39
Corentin Le Molgat <corentinl@google.com>
2022-06-21 16:54:48 +0200

    linear_solver: format fix

--- ortools/linear_solver/linear_solver_natural_api.py
+++ ortools/linear_solver/linear_solver_natural_api.py
@@ -10,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Patch to the python wrapper of ../linear_solver.h providing an algebraic API.
 
 This is directly imported, and use exclusively in ./linear_solver.swig. See that
