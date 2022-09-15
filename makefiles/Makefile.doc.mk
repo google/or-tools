db1b578f9300d28e19b11249d99160cb62548d9c
Corentin Le Molgat <corentinl@google.com>
2022-09-15 10:22:15 +0200

    make: fix doc target

--- makefiles/Makefile.doc.mk
+++ makefiles/Makefile.doc.mk
@@ -30,45 +30,15 @@ doxy-doc: cpp python java dotnet
 
 .PHONY: java-doc # Create Javadoc ref documentation.
 java-doc: java
+	bash -c "command -v mvn"
 	tools/doc/gen_javadoc.sh
 
 .PHONY: python-doc # Create python documentation.
-python-doc:
+python-doc: python
 	bash -c "command -v pdoc"
 	$(SET_PYTHONPATH) pdoc \
  --logo https://developers.google.com/optimization/images/orLogo.png \
  -o docs/python/ \
  --no-search -d google \
  --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/sat/python/cp_model.py
-	$(SET_PYTHONPATH) pdoc \
- --logo https://developers.google.com/optimization/images/orLogo.png \
- -o docs/python/ortools/util/ \
- --no-search -d google \
- --footer-text "OR-Tools ${ORTOOLS_MAJOR}.${ORTOOLS_MINOR}" \
- --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/util/python/sorted_interval_list.py
-	$(SET_PYTHONPATH) pdoc \
- --logo https://developers.google.com/optimization/images/orLogo.png \
- -o docs/python/ortools/linear_solver/ \
- --no-search -d google \
- --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/linear_solver/pywraplp.py
-	$(SET_PYTHONPATH) pdoc \
- --logo https://developers.google.com/optimization/images/orLogo.png \
- -o docs/python/ortools/constraint_solver/ \
- --no-search -d google \
- --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/constraint_solver/pywrapcp.py
-	$(SET_PYTHONPATH) pdoc \
- --logo https://developers.google.com/optimization/images/orLogo.png \
- -o docs/python/ortools/algorithms/ \
- --no-search -d google \
- --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/algorithms/pywrapknapsack_solver.py
-	$(SET_PYTHONPATH) pdoc \
- --logo https://developers.google.com/optimization/images/orLogo.png \
- -o docs/python/ortools/graph/ \
- --no-search -d google \
- --footer-text "OR-Tools ${OR_TOOLS_MAJOR}.${OR_TOOLS_MINOR}" \
- dependencies/python/ortools/graph/pywrapgraph.py
+ build_make/python/ortools
