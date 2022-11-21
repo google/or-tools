fcd35537943980f0334341f0ccc08a890c3d6953
Corentin Le Molgat <corentinl@google.com>
2022-11-21 17:44:51 +0100

    util: Update rouding_modes_benchmark.cc

--- ortools/util/rounding_modes_benchmark.cc
+++ ortools/util/rounding_modes_benchmark.cc
@@ -40,10 +40,6 @@
 // a good hardware implementation of interval computation.
 // Note: compile with -mavx512f to get access to avx512.
 //
-// IMPORTANT NOTICE: I did not manage to get this work with g++-12.
-// g++-12 -mavx512f -O3 -frounding-math rounding_modes_benchmark.cc
-// Some computations seem to be stuck with a rounding towards -infinity.
-//
 // Interesting references:
 // What every computer scientist should know about floating-point arithmetic
 // ACM Computing Surveys Volume 23 Issue 1 March 1991 pp 5–48
@@ -72,141 +68,145 @@
 // Name,ns/it,result
 // SumOfIntegers<RoundToNearestEven>,1.1801, \
 //     [,500000000067108992.00000000000000,500000000067108992.00000000000000,]
-// SumOfIntegers<StdLibIntervals>,19.5461, \
+// SumOfIntegers<StdRounding>,19.5461, \
 //     [,499999987755261568.00000000000000,500000013244738176.00000000000000,]
-// SumOfIntegers<IntrinsicIntervals>,5.71184, \
+// SumOfIntegers<LowLevelRounding>,5.71184, \
 //     [,499999987755261568.00000000000000,500000013244738176.00000000000000,]
-// SumOfIntegers<Avx512Intervals>,1.17857, \
+// SumOfIntegers<Avx512Rounding>,1.17857, \
 //     [,499999987755261568.00000000000000,500000013244738176.00000000000000,]
 // SumOfSquareRoots<RoundToNearestEven>,2.7984, \
 //     [,21081851083600.55859375000000,21081851083600.55859375000000,]
-// SumOfSquareRoots<StdLibIntervals>,23.91, \
+// SumOfSquareRoots<StdRounding>,23.91, \
 //     [,21081850394046.03906250000000,21081851773099.68359375000000,]
-// SumOfSquareRoots<IntrinsicIntervals>,6.64908, \
+// SumOfSquareRoots<LowLevelRounding>,6.64908, \
 //     [,21081850394046.03906250000000,21081851773099.68359375000000,]
-// SumOfSquareRoots<Avx512Intervals>,2.91871, \
+// SumOfSquareRoots<Avx512Rounding>,2.91871, \
 //     [,21081850394046.03906250000000,21081851773099.68359375000000,]
 
 // TODO(user): make it work on ARM, RISC-V, and POWER.
 
-#include <cstdio>
-#if defined(__x86_64__)
-#include <emmintrin.h>
-#include <immintrin.h>
-#include <x86intrin.h>
-#include <xmmintrin.h>
-#endif
-
+#include <cassert>
 #include <cfenv>   // NOLINT
 #include <chrono>  // NOLINT(build/c++11)
 #include <cmath>
 #include <cstdint>
+#include <cstdio>
 
-// One is supposed to use the following pragma incantation to be able to change
-// rounding modes. It is not clear whether it is needed or not. As per
-// https://en.cppreference.com/w/cpp/numeric/fenv, few current compilers,
-// such as HP aCC, Oracle Studio, or IBM XL support this. But it seems to be
-// necessary. Better safe than sorry...
+#if defined(__x86_64__)
+#include <x86intrin.h>
+#endif
+#if defined(_M_AMD64)  // For MSVC.
+#include <intrin.h>
+#endif
+#if defined(__x86_64__) || defined(_M_AMD64)
+#define AMD64
+#include <emmintrin.h>
+#include <immintrin.h>
+#include <xmmintrin.h>
+#endif
+
+// Is this really necessary? g++ does not like it.
+#if defined(__clang__)
 #pragma STDC FENV_ACCESS ON
+#endif
 
 struct RoundToNearestEven {
   // Abuse of language to describe an "interval" computed using the standard
   // round-to-even mode.
   static inline double AddUp(double a, double b) { return a + b; }
-
   static inline double AddDown(double a, double b) { return a + b; }
 };
 
-class StdLibScopedRoudingMode {
+class StdRoundingScope {
  public:
-  explicit StdLibScopedRoudingMode(int new_mode)
-      : saved_mode_(std::fegetround()) {
+  explicit StdRoundingScope(int new_mode) : saved_mode_(std::fegetround()) {
     std::fesetround(new_mode);
   }
-  ~StdLibScopedRoudingMode() { std::fesetround(saved_mode_); }
+  ~StdRoundingScope() { std::fesetround(saved_mode_); }
 
  private:
   int saved_mode_;
 };
 
-struct StdLibIntervals {
+struct StdRounding {
   // Simplest possible implementation of addition with rounding modes on a
   // single double, using the standard fegetround() / fesetround()
   // functions.
   static inline double AddUp(double a, double b) {
-    StdLibScopedRoudingMode up(FE_UPWARD);
+    StdRoundingScope up(FE_UPWARD);
     return a + b;
   }
 
   static inline double AddDown(double a, double b) {
-    StdLibScopedRoudingMode down(FE_DOWNWARD);
+    StdRoundingScope down(FE_DOWNWARD);
     return a + b;
   }
 };
 
-// Making it a little bit faster. We use inline assembly to only touch
-// the MX CSR register, which controls the rounding mode on the SSE2
-// and AVX{1,2,512} instructions only, ie the xmm's, ymm's and zmm's.
+// Making it a little bit faster. Supported only on x86-64 for now.
+// We use intrinsics to only touch the non-x87 FP rounding modes.
 // Contrary to fesetround, it does nothing about x87 rounding mode.
 // This is safe because we reset the status to what it was right after
 // performing the operation. Also, we store the previous status, and
 // therefore minimize the number of reads from the status register.
 
-#if defined(__x86_64__)
-// Returns the contents of the MX control status register on x86.
-inline unsigned int GetMXStatus() { return _mm_getcsr(); }
-
-// Sets the contents of the MX control status register on x86.
-inline void SetMXStatus(unsigned int status) { _mm_setcsr(status); }
-
-// Sets the rounding mode, using the constants as defined in <cfenv>.
-// As said above, we're not touching the x87 part of the CPU.
-inline void SetRoundingMode(int status, int mode) {
-  constexpr int kRoundingModeMask = 0x6000;
-  constexpr int kRoundingModeShift = 3;
-  SetMXStatus((status & ~kRoundingModeMask) | (mode << kRoundingModeShift));
+// Returns the contents of the floating-point control register.
+inline unsigned int GetFloatingPointControlRegister() {
+#if defined(AMD64)
+  // Returns the contents of the MX control status register on x86.
+  return _mm_getcsr();
+#else
+  // TODO(user): implement for other architectures.
+  return 0;
+#endif  // AMD64
 }
 
-class IntrinsicScopedRoundingMode {
+// Sets the contents of the floating-point control register.
+inline void SetFloatingPointControlRegister(unsigned int status) {
+#if defined(AMD64)
+  // Sets the contents of the MX control status register on x86.
+  _mm_setcsr(status);
+#endif
+}
+
+// Sets the rounding mode, using the constants as defined in <cfenv>.
+inline void SetRoundingMode(int status, int mode) {
+#if defined(AMD64)
+  // As said above, we're not touching the x87 part of the CPU.
+  constexpr int kRoundingModeMask = 0x6000;
+  constexpr int kRoundingModeShift = 3;
+  SetFloatingPointControlRegister((status & ~kRoundingModeMask) |
+                                  (mode << kRoundingModeShift));
+#endif  // AMD64
+}
+
+class LowLevelRoundingScope {
  public:
-  explicit IntrinsicScopedRoundingMode(int new_mode)
-      : saved_status_(GetMXStatus()) {
+  explicit LowLevelRoundingScope(int new_mode)
+      : saved_status_(GetFloatingPointControlRegister()) {
     SetRoundingMode(saved_status_, new_mode);
   }
-  ~IntrinsicScopedRoundingMode() { SetMXStatus(saved_status_); }
+  ~LowLevelRoundingScope() { SetFloatingPointControlRegister(saved_status_); }
 
  private:
   int saved_status_;
 };
 
-struct IntrinsicIntervals {
+struct LowLevelRounding {
   // Faster rounding addition using the above rounding mode functions.
   static inline double AddUp(double a, double b) {
-    IntrinsicScopedRoundingMode up(FE_UPWARD);
+    LowLevelRoundingScope up(FE_UPWARD);
     return a + b;
   }
 
   static inline double AddDown(double a, double b) {
-    IntrinsicScopedRoundingMode down(FE_DOWNWARD);
+    LowLevelRoundingScope down(FE_DOWNWARD);
     return a + b;
   }
 };
-#else
-struct IntrinsicIntervals {
-  static inline double AddUp(double a, double b) {
-    IntrinsicScopedRoundingMode up(FE_UPWARD);
-    return 0.0;
-  }
-
-  static inline double AddDown(double a, double b) {
-    IntrinsicScopedRoundingMode down(FE_DOWNWARD);
-    return 0.0;
-  }
-};
-#endif
 
 #ifdef __AVX512F__
-struct Avx512Intervals {
+struct Avx512Rounding {
   // Even better implementation of rounding addition using instructions
   // for which one can specify the rounding mode explicitly.
   // Using AVX512.
@@ -232,7 +232,7 @@ struct Avx512Intervals {
   }
 };
 #else
-struct Avx512Intervals {
+struct Avx512Rounding {
   static inline double AddUp(double a, double b) { return 0.0; }
   static inline double AddDown(double a, double b) { return 0.0; }
 };
@@ -248,7 +248,7 @@ struct Interval {
 // the correct answer is guaranteed to be.
 typedef Interval (*IntervalFunction)(int n);
 
-// This is a small runner that call an IntervalFunction, counts the
+// This is a small runner that calls an IntervalFunction, counts the
 // nanoseconds in a portable way, counts the cycles, and reports various
 // stats. We're not concerned with timing the instructions independently.
 // Just reporting the number of cycles per iteration is good enough.
@@ -270,9 +270,9 @@ void Runner(IntervalFunction f, int n) {
 // 500'000'000'500'000'000 or 5.000000005e+17.
 template <typename Impl>
 Interval SumOfIntegers(int n) {
+  const double max = n;
   double l = 0.0;
   double u = 0.0;
-  const double max = n;
   for (double d = 0.0; d <= max; d += 1.0) {
     l = Impl::AddDown(l, d);
     u = Impl::AddUp(u, d);
@@ -284,9 +284,9 @@ Interval SumOfIntegers(int n) {
 // 500'000'000'067'108'992 with clang on an x86_64 PC running Linux, far
 // from the actual result.
 template Interval SumOfIntegers<RoundToNearestEven>(int);
-template Interval SumOfIntegers<StdLibIntervals>(int);
-template Interval SumOfIntegers<IntrinsicIntervals>(int);
-template Interval SumOfIntegers<Avx512Intervals>(int);
+template Interval SumOfIntegers<StdRounding>(int);
+template Interval SumOfIntegers<LowLevelRounding>(int);
+template Interval SumOfIntegers<Avx512Rounding>(int);
 
 // Compute the sum of the square roots of the first natural numbers up to
 // and including n.
@@ -294,26 +294,28 @@ template Interval SumOfIntegers<Avx512Intervals>(int);
 // of Errors?" by Gregory Tarzy and Neil Toda from Sun Microsytems on
 // 2004-01-20 on the now-defunct site developers.sun.com.
 // The standard rounding mode returns 21081851083600.55859375000000
-// while the correct answer computed using CAS software) is
+// while the correct answer computed using Maxima is
 // 21081851083600.37596259382529338.
 // Interestingly, a Pentium II at 400MHz took more than 6 hours to complete.
 // Different pairs of (OS, CPU) produced quite different results. Always
 // wrong.
 template <typename Impl>
 Interval SumOfSquareRoots(int n) {
+  Interval result(0.0, 0.0);
   double l = 0.0;
   double u = 0.0;
   for (int i = 0; i <= n; ++i) {
-    l = Impl::AddDown(l, std::sqrt(i));
-    u = Impl::AddUp(u, std::sqrt(i));
+    const double d = std::sqrt(i);
+    l = Impl::AddDown(l, d);
+    u = Impl::AddUp(u, d);
   }
   return Interval(l, u);
 }
 
 template Interval SumOfSquareRoots<RoundToNearestEven>(int);
-template Interval SumOfSquareRoots<StdLibIntervals>(int);
-template Interval SumOfSquareRoots<IntrinsicIntervals>(int);
-template Interval SumOfSquareRoots<Avx512Intervals>(int);
+template Interval SumOfSquareRoots<StdRounding>(int);
+template Interval SumOfSquareRoots<LowLevelRounding>(int);
+template Interval SumOfSquareRoots<Avx512Rounding>(int);
 
 #define BENCHMARK(fn, n) \
   {                      \
@@ -322,7 +324,7 @@ template Interval SumOfSquareRoots<Avx512Intervals>(int);
   }
 
 int main() {
-#if !defined(__x86_64__)
+#if !defined(AMD64)
   printf("Warning: x86-64 instrinsics not supported.\n");
 #endif
 #if !defined(__AVX512F__)
@@ -331,12 +333,12 @@ int main() {
   const int n = 1'000'000'000;
   printf("Name,ns/it,result\n");
   BENCHMARK(SumOfIntegers<RoundToNearestEven>, n);
-  BENCHMARK(SumOfIntegers<StdLibIntervals>, n);
-  BENCHMARK(SumOfIntegers<IntrinsicIntervals>, n);
-  BENCHMARK(SumOfIntegers<Avx512Intervals>, n);
+  BENCHMARK(SumOfIntegers<StdRounding>, n);
+  BENCHMARK(SumOfIntegers<LowLevelRounding>, n);
+  BENCHMARK(SumOfIntegers<Avx512Rounding>, n);
   BENCHMARK(SumOfSquareRoots<RoundToNearestEven>, n);
-  BENCHMARK(SumOfSquareRoots<StdLibIntervals>, n);
-  BENCHMARK(SumOfSquareRoots<IntrinsicIntervals>, n);
-  BENCHMARK(SumOfSquareRoots<Avx512Intervals>, n);
+  BENCHMARK(SumOfSquareRoots<StdRounding>, n);
+  BENCHMARK(SumOfSquareRoots<LowLevelRounding>, n);
+  BENCHMARK(SumOfSquareRoots<Avx512Rounding>, n);
   return 0;
 }
