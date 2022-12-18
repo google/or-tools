609fd843a9f07bcd4c91f653354069b2748300b8
Laurent Perron <lperron@google.com>
2022-12-18 21:53:06 +0100

    remove unneded include

--- ortools/sat/cp_model_loader.cc
+++ ortools/sat/cp_model_loader.cc
@@ -24,7 +24,6 @@
 #include "absl/container/btree_set.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/container/flat_hash_set.h"
-#include "absl/meta/type_traits.h"
 #include "absl/strings/str_cat.h"
 #include "absl/types/span.h"
 #include "ortools/algorithms/sparse_permutation.h"
