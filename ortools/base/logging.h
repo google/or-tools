d59f1f5c5d3f3f039f8b2df6752c92fe01233a8d
Corentin Le Molgat <corentinl@google.com>
2023-04-11 13:45:22 +0200

    remove deps to protobuf in logging.h

--- ortools/base/logging.h
+++ ortools/base/logging.h
@@ -27,7 +27,6 @@
 #include "absl/status/status.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
-#include "google/protobuf/message.h"
 #include "ortools/base/macros.h"
 #include "ortools/base/vlog.h"
 
