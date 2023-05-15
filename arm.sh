4a24267a5204a554c172d440f9358a947964e015
Corentin Le Molgat <corentinl@google.com>
2023-05-15 18:14:17 +0200

    cross compile fixup

--- arm.sh
+++ arm.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 #./tools/cross_compile.sh --help
 
-#export PROJECT=or-tools
-export PROJECT=glop
+export PROJECT=or-tools
+#export PROJECT=glop
+
 #export TARGET=x86_64
-export TARGET=aarch64-linux-gnu
+export TARGET=aarch64
+#export TARGET=mips64
+#export TARGET=ppc64
 
 ./tools/cross_compile.sh toolchain
 ./tools/cross_compile.sh build
