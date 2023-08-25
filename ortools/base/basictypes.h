f0b1a33069c88f140471e7849ab372016d8f8652
Laurent Perron <lperron@google.com>
2023-08-25 07:42:45 +0200

    missing

---
+++ ortools/base/basictypes.h
@@ -0,0 +1,24 @@
+// Copyright 2010-2022 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Basic integer type definitions for various platforms
+//
+#ifndef OR_TOOLS_BASE_BASICTYPES_H_
+#define OR_TOOLS_BASE_BASICTYPES_H_
+
+// Argument type used in interfaces that can optionally take ownership
+// of a passed in argument.  If TAKE_OWNERSHIP is passed, the called
+// object takes ownership of the argument.  Otherwise it does not.
+enum Ownership { DO_NOT_TAKE_OWNERSHIP, TAKE_OWNERSHIP };
+
+#endif  // OR_TOOLS_BASE_BASICTYPES_H_
