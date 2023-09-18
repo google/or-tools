09fe839f713ee6d592e1e57640429bb47c3b9733
Corentin Le Molgat <corentinl@google.com>
2023-09-18 13:29:25 +0200

    base: export from Google3
    * fix indent
    * fix includes

--- ortools/base/sysinfo.cc
+++ ortools/base/sysinfo.cc
@@ -47,7 +47,8 @@ int64_t GetProcessMemoryUsage() {
   int64_t resident_memory = t_info.resident_size;
   return resident_memory;
 }
-#elif defined(__GNUC__) && !defined(__FreeBSD__) && !defined(__EMSCRIPTEN__) // LINUX
+#elif defined(__GNUC__) && !defined(__FreeBSD__) && \
+    !defined(__EMSCRIPTEN__)  // LINUX
 int64_t GetProcessMemoryUsage() {
   unsigned size = 0;
   char buf[30];
@@ -59,14 +60,14 @@ int64_t GetProcessMemoryUsage() {
   fclose(pf);
   return int64_t{1024} * size;
 }
-#elif defined(__FreeBSD__)                        // FreeBSD
+#elif defined(__FreeBSD__)    // FreeBSD
 int64_t GetProcessMemoryUsage() {
   int who = RUSAGE_SELF;
   struct rusage rusage;
   getrusage(who, &rusage);
   return (int64_t)(int64_t{1024} * rusage.ru_maxrss);
 }
-#elif defined(_MSC_VER)                           // WINDOWS
+#elif defined(_MSC_VER)       // WINDOWS
 int64_t GetProcessMemoryUsage() {
   HANDLE hProcess;
   PROCESS_MEMORY_COUNTERS pmc;
@@ -81,7 +82,7 @@ int64_t GetProcessMemoryUsage() {
   }
   return memory;
 }
-#else                                             // Unknown, returning 0.
+#else                         // Unknown, returning 0.
 int64_t GetProcessMemoryUsage() { return 0; }
 #endif
 
