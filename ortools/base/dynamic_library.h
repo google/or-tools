528ec991f57a7ab06b1407c87da32dc04b911875
Andrea Sgattoni <andrea.sgattoni@rte-france.com>
2023-10-30 17:27:04 +0100

    revert to LoadLibrary instead of LoadLibraryA

--- ortools/base/dynamic_library.h
+++ ortools/base/dynamic_library.h
@@ -48,7 +48,7 @@ static constexpr size_t kMaxFunctionsNotFound = 10;
   bool TryToLoad(const std::string& library_name) {
     library_name_ = std::string(library_name);
 #if defined(_MSC_VER)
-    library_handle_ = static_cast<void*>(LoadLibraryA(library_name.c_str()));
+    library_handle_ = static_cast<void*>(LoadLibrary(library_name.c_str()));
 #elif defined(__GNUC__)
     library_handle_ = dlopen(library_name.c_str(), RTLD_NOW);
 #endif
