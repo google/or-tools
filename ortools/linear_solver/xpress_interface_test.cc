199830096abbf2d5ec3070092184060b31c201bf
Peter Mitri <peter.mitri@rte-france.com>
2024-04-29 11:54:49 +0200

    [XPRESS] Extract variable & constraint names only before writing MPS + fix a test (#4185)
    * Update XPRESS interface
    
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>
    
    * review comments
    
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>
    
    * revert bad commit
    
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>
    
    * disable MPS formatting in test
    
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>
    
    ---------
    
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>

--- ortools/linear_solver/xpress_interface_test.cc
+++ ortools/linear_solver/xpress_interface_test.cc
@@ -706,11 +706,11 @@ TEST_F(XpressFixtureMIP, SolverVersion) {
 
 TEST_F(XpressFixtureMIP, Write) {
   MPVariable* x1 = solver.MakeIntVar(-1.2, 9.3, "C1");
-  MPVariable* x2 = solver.MakeNumVar(-1, 5.147593849384714, "C2");
+  MPVariable* x2 = solver.MakeNumVar(-1, 5.147593849384714, "SomeColumnName");
   MPConstraint* c1 = solver.MakeRowConstraint(-solver.infinity(), 1, "R1");
   c1->SetCoefficient(x1, 3);
   c1->SetCoefficient(x2, 1.5);
-  MPConstraint* c2 = solver.MakeRowConstraint(3, 5, "R2");
+  MPConstraint* c2 = solver.MakeRowConstraint(3, 5, "SomeRowName");
   c2->SetCoefficient(x2, -1.1122334455667788);
   MPObjective* obj = solver.MutableObjective();
   obj->SetMaximization();
@@ -730,30 +730,34 @@ TEST_F(XpressFixtureMIP, Write) {
   tmpFile.close();
   std::filesystem::remove_all(temporary_working_dir);
 
-  EXPECT_EQ(tmpBuffer.str(), R"(NAME          newProb
-OBJSENSE  MAXIMIZE
-ROWS
- N  __OBJ___
- L  R1
- L  R2
-COLUMNS
-    C1        __OBJ___  1
-    C1        R1        3
-    C2        __OBJ___  2
-    C2        R1        1.5
-    C2        R2        -1.1122334455667788
-RHS
-    RHS00001  R1        1
-    RHS00001  R2        5
-RANGES
-    RNG00001  R2        2
-BOUNDS
- UI BND00001  C1        9
- LO BND00001  C1        -1
- UP BND00001  C2        5.147593849384714
- LO BND00001  C2        -1
-ENDATA
-)");
+  // disable formatting to keep the expected MPS readable
+  // clang-format off
+  std::string expectedMps = std::string("") +
+                            "NAME          newProb" + "\n" +
+                            "OBJSENSE  MAXIMIZE" + "\n" +
+                            "ROWS" + "\n" +
+                            " N  __OBJ___        " + "\n" +
+                            " L  R1              " + "\n" +
+                            " L  SomeRowName     " + "\n" +
+                            "COLUMNS" + "\n" +
+                            "    C1                __OBJ___          1" + "\n" +
+                            "    C1                R1                3" + "\n" +
+                            "    SomeColumnName    __OBJ___          2" + "\n" +
+                            "    SomeColumnName    R1                1.5" + "\n" +
+                            "    SomeColumnName    SomeRowName       -1.1122334455667788" + "\n" +
+                            "RHS" + "\n" +
+                            "    RHS00001          R1                1" + "\n" +
+                            "    RHS00001          SomeRowName       5" + "\n" +
+                            "RANGES" + "\n" +
+                            "    RNG00001          SomeRowName       2" + "\n" +
+                            "BOUNDS" + "\n" +
+                            " UI BND00001          C1                9" + "\n" +
+                            " LO BND00001          C1                -1" + "\n" +
+                            " UP BND00001          SomeColumnName    5.147593849384714" + "\n" +
+                            " LO BND00001          SomeColumnName    -1" + "\n" +
+                            "ENDATA" + "\n";
+  // clang-format on
+  EXPECT_EQ(tmpBuffer.str(), expectedMps);
 }
 
 TEST_F(XpressFixtureLP, SetPrimalTolerance) {
