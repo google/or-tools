38bf143dae01dc64a7ceb3bbb1380e1bf472035d
Laurent Perron <lperron@google.com>
2024-10-09 16:40:51 +0200

    fix test for windows

--- ortools/sat/work_assignment_test.cc
+++ ortools/sat/work_assignment_test.cc
@@ -146,9 +146,9 @@ class SharedTreeSolveTest : public testing::TestWithParam<absl::string_view> {
     params.set_num_workers(4);
     params.set_shared_tree_num_workers(4);
     params.set_cp_model_presolve(false);
-    params.MergeFrom(SatParameters{
-        google::protobuf::contrib::parse_proto::ParseTextProtoOrDie(
-            GetParam())});
+    const SatParameters to_merge =
+        google::protobuf::contrib::parse_proto::ParseTextProtoOrDie(GetParam());
+    params.MergeFrom(to_merge);
     return params;
   }
 };
