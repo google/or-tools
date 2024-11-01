16fc39841fc863636a3becdeb73a155a7c7ed636
ulfw <ulf.worsoe@mosek.com>
2024-11-01 11:27:44 +0100

    fix mosek/cmake config for windows

--- cmake/FindMOSEK.cmake
+++ cmake/FindMOSEK.cmake
@@ -100,6 +100,26 @@ function(FindMosekPlatformInPath RESULT PATH)
 endfunction()
 
 
+function(MosekVersionFromPath RESVMAJOR RESVMINOR PATH)
+    execute_process(COMMAND "${PATH}/bin/mosek" "-v" RESULT_VARIABLE CMDRES OUTPUT_VARIABLE VERTEXT)
+    if (${CMDRES} EQUAL 0)
+        if (${VERTEXT} MATCHES "^MOSEK version [0-9]+[.][0-9]+[.][0-9]+")
+            string(REGEX REPLACE "^MOSEK version ([0-9]+)[.]([0-9]+).*" "\\1;\\2" MSKVER ${VERTEXT})
+
+            list(GET MSKVER 0 VMAJOR)
+            list(GET MSKVER 1 VMINOR)
+            set(${RESVMAJOR} "${VMAJOR}" PARENT_SCOPE)
+            set(${RESVMINOR} "${VMINOR}" PARENT_SCOPE)
+
+            #message(STATUS "mskver = ${VMAJOR}.${VMINOR} -> ${${RESVMAJOR}}.${${RESVMINOR}}")
+            #message(STATUS "    ${RESVMAJOR} = ${${RESVMAJOR}}")
+            #message(STATUS "    ${RESVMINOR} = ${${RESVMINOR}}")
+            return()
+        endif()
+    endif()
+endfunction()
+
+
 # Where to look for MOSEK:
 # Standard procedure in Linux/OSX is to install MOSEK in the home directory, i.e.
 #   $HOME/mosek/X.Y/...
@@ -129,24 +149,32 @@ if(NOT MOSEK_PLATFORM_DIR)
 endif()
 if(NOT MOSEK_PLATFORM_DIR)
     # Look in users home dir
-    if(ENV{HOMEDRIVE} AND ENV{HOMEPATH})
-        FindMosekPlatformInPath(MOSEK_PLATFORM_DIR"$ENV{HOMEDRIVE}$ENV{HOMEPATH}")
+    if(EXISTS "$ENV{HOMEDRIVE}$ENV{HOMEPATH}")
+        FindMosekPlatformInPath(MOSEK_PLATFORM_DIR "$ENV{HOMEDRIVE}$ENV{HOMEPATH}")
     endif()
 endif()
 
 if(NOT MOSEK_PLATFORM_DIR)
-    message(FATAL_ERROR "  MOSEK_PLATFORM_DIR could not be detected")
+  message(FATAL_ERROR "  MOSEK_PLATFORM_DIR could not be detected")
 else()
-  message(STATUS "  MOSEK_PLATFORM_DIR detected: ${MOSEK_PLATFORM_DIR}")
-  set(MOSEK_PFDIR_FOUND TRUE)
+  MosekVersionFromPath(MSKVMAJOR MSKVMINOR "${MOSEK_PLATFORM_DIR}")
+  if(MSKVMAJOR)
+    message(STATUS "  MOSEK ${MSKVMAJOR}.${MSKVMINOR}: ${MOSEK_PLATFORM_DIR}")
+    set(MOSEK_PFDIR_FOUND TRUE)
+  else()
+    message(STATUS "  Found MOSEK_PLATFORM_DIR, but failed to determine version")
+  endif()
 endif()
 
-
-if(MOSEK_PFDIR_FOUND AND NOT TARGET Mosek::Mosek)
+if(MOSEK_PFDIR_FOUND AND MSKVMAJOR AND MSKVMINOR AND NOT TARGET mosek::mosek)
   add_library(mosek::mosek UNKNOWN IMPORTED)
   
   find_path(MOSEKINC mosek.h HINTS ${MOSEK_PLATFORM_DIR}/h)
-  find_library(LIBMOSEK mosek64 HINTS ${MOSEK_PLATFORM_DIR}/bin)
+  if(WIN32)
+      find_library(LIBMOSEK mosek64_ HINTS ${MOSEK_PLATFORM_DIR}/bin)
+  else()
+      find_library(LIBMOSEK mosek64 HINTS ${MOSEK_PLATFORM_DIR}/bin)
+  endif()
 
   if(LIBMOSEK) 
       set_target_properties(mosek::mosek PROPERTIES IMPORTED_LOCATION ${LIBMOSEK})
@@ -155,7 +183,7 @@ if(MOSEK_PFDIR_FOUND AND NOT TARGET Mosek::Mosek)
   if (MOSEKINC)
       target_include_directories(mosek::mosek INTERFACE "${MOSEKINC}")
   endif()
-elseif(NOT TARGET Mosek::Mosek)
+elseif(NOT TARGET mosek::mosek)
   add_library(mosek::mosek UNKNOWN IMPORTED)
   
   find_path(MOSEKINC mosek.h)
@@ -166,6 +194,6 @@ elseif(NOT TARGET Mosek::Mosek)
   endif()
 
   if (MOSEKINC)
-      target_include_directories(mosek::mosek INTERFACE "${MOSEKINC}")
+    target_include_directories(mosek::mosek INTERFACE "${MOSEKINC}")
   endif()
 endif()
