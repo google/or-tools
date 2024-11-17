8386d7ab6d55f740c143c14fa4dd8b86a18a8351
Laurent Perron <lperron@google.com>
2024-11-17 14:17:24 +0100

    more work on set cover

--- ortools/algorithms/set_cover_test.cc
+++ ortools/algorithms/set_cover_test.cc
@@ -13,6 +13,7 @@
 
 #include <limits>
 #include <string>
+#include <vector>
 
 #include "absl/log/check.h"
 #include "absl/strings/str_cat.h"
@@ -49,48 +50,79 @@ TEST(SetCoverTest, GuidedLocalSearchVerySmall) {
   CHECK(inv.CheckConsistency(CL::kRedundancy));
 }
 
-SetCoverModel CreateKnightsCoverModel(int num_rows, int num_cols) {
-  SetCoverModel model;
-  constexpr int knight_row_move[] = {2, 1, -1, -2, -2, -1, 1, 2};
-  constexpr int knight_col_move[] = {1, 2, 2, 1, -1, -2, -2, -1};
-  for (int row = 0; row < num_rows; ++row) {
-    for (int col = 0; col < num_cols; ++col) {
-      model.AddEmptySubset(1);
-      model.AddElementToLastSubset(row * num_cols + col);
-      for (int i = 0; i < 8; ++i) {
-        const int new_row = row + knight_row_move[i];
-        const int new_col = col + knight_col_move[i];
-        if (new_row >= 0 && new_row < num_rows && new_col >= 0 &&
-            new_col < num_cols) {
-          model.AddElementToLastSubset(new_row * num_cols + new_col);
+class KnightsCover {
+ public:
+  KnightsCover(int num_rows, int num_cols)
+      : num_rows_(num_rows), num_cols_(num_cols), model_() {
+    constexpr int knight_row_move[] = {2, 1, -1, -2, -2, -1, 1, 2};
+    constexpr int knight_col_move[] = {1, 2, 2, 1, -1, -2, -2, -1};
+    for (int row = 0; row < num_rows_; ++row) {
+      for (int col = 0; col < num_cols_; ++col) {
+        model_.AddEmptySubset(1);
+        model_.AddElementToLastSubset(ElementNumber(row, col));
+        for (int i = 0; i < 8; ++i) {
+          const int new_row = row + knight_row_move[i];
+          const int new_col = col + knight_col_move[i];
+          if (IsOnBoard(new_row, new_col)) {
+            model_.AddElementToLastSubset(ElementNumber(new_row, new_col));
+          }
         }
       }
     }
   }
-  return model;
-}
 
-void DisplayKnightsCoverSolution(const SubsetBoolVector& choices, int num_rows,
-                                 int num_cols) {
-  std::string line;
-  std::string separator = "+";
-  for (int col = 0; col < num_cols; ++col) {
-    absl::StrAppend(&separator, "-+");
-  }
-  LOG(INFO) << separator;
-  for (int row = 0; row < num_rows; ++row) {
-    line = "|";
-    for (int col = 0; col < num_cols; ++col) {
-      const SubsetIndex subset(row * num_cols + col);
-      absl::StrAppend(&line, choices[subset] ? "X|" : " |");
+  SetCoverModel model() const { return model_; }
+
+  void DisplaySolution(const SubsetBoolVector& choices) const {
+    std::string line;
+    std::string separator = "+";
+    for (int col = 0; col < num_cols_; ++col) {
+      absl::StrAppend(&separator, "-+");
     }
-    LOG(INFO) << line;
     LOG(INFO) << separator;
+    for (int row = 0; row < num_rows_; ++row) {
+      line = "|";
+      for (int col = 0; col < num_cols_; ++col) {
+        const SubsetIndex subset(SubsetNumber(row, col));
+        absl::StrAppend(&line, choices[subset] ? "X|" : " |");
+      }
+      LOG(INFO) << line;
+      LOG(INFO) << separator;
+    }
   }
-}
+
+  void ClearSubsetWithinRadius(int row, int col, int radius,
+                               SetCoverInvariant* inv) const {
+    for (int r = row - radius; r <= row + radius; ++r) {
+      for (int c = col - radius; c <= col + radius; ++c) {
+        if (!IsOnBoard(r, c)) continue;
+        const SubsetIndex subset(SubsetNumber(r, c));
+        if (inv->is_selected()[subset]) {
+          inv->Deselect(subset, CL::kFreeAndUncovered);
+        }
+      }
+    }
+  }
+
+ private:
+  bool IsOnBoard(int row, int col) const {
+    return row >= 0 && row < num_rows_ && col >= 0 && col < num_cols_;
+  }
+  // There is a 1:1 mapping between elements and subsets because the
+  // subset i corresponds to placing the element i at the position (r, c).
+  ElementIndex ElementNumber(int row, int col) const {
+    return ElementIndex(row * num_cols_ + col);
+  }
+  SubsetIndex SubsetNumber(int row, int col) const {
+    return SubsetIndex(row * num_cols_ + col);
+  }
+  int num_rows_;
+  int num_cols_;
+  SetCoverModel model_;
+};
 
 TEST(SetCoverProtoTest, SaveReload) {
-  SetCoverModel model = CreateKnightsCoverModel(10, 10);
+  SetCoverModel model = KnightsCover(10, 10).model();
   SetCoverProto proto = model.ExportModelAsProto();
   SetCoverModel reloaded;
   reloaded.ImportModelFromProto(proto);
@@ -101,7 +133,7 @@ TEST(SetCoverProtoTest, SaveReload) {
 }
 
 TEST(SolutionProtoTest, SaveReloadTwice) {
-  SetCoverModel model = CreateKnightsCoverModel(3, 3);
+  SetCoverModel model = KnightsCover(3, 3).model();
   SetCoverInvariant inv(&model);
   GreedySolutionGenerator greedy(&inv);
   CHECK(greedy.NextSolution());
@@ -147,29 +179,6 @@ TEST(SetCoverTest, InitialValues) {
   EXPECT_TRUE(inv.CheckConsistency(CL::kFreeAndUncovered));
 }
 
-TEST(SetCoverTest, Preprocessor) {
-  SetCoverModel model;
-  model.AddEmptySubset(1);
-  model.AddElementToLastSubset(0);
-  model.AddEmptySubset(1);
-  model.AddElementToLastSubset(1);
-  model.AddElementToLastSubset(2);
-  model.AddEmptySubset(1);
-  model.AddElementToLastSubset(1);
-  model.AddEmptySubset(1);
-  model.AddElementToLastSubset(2);
-  EXPECT_TRUE(model.ComputeFeasibility());
-
-  SetCoverInvariant inv(&model);
-  Preprocessor preprocessor(&inv);
-  preprocessor.NextSolution();
-  EXPECT_TRUE(inv.CheckConsistency(CL::kCostAndCoverage));
-  GreedySolutionGenerator greedy(&inv);
-  EXPECT_TRUE(greedy.NextSolution());
-  LOG(INFO) << "GreedySolutionGenerator cost: " << inv.cost();
-  EXPECT_TRUE(inv.CheckConsistency(CL::kFreeAndUncovered));
-}
-
 TEST(SetCoverTest, Infeasible) {
   SetCoverModel model;
   model.AddEmptySubset(1);
@@ -186,12 +195,12 @@ static constexpr int SIZE = 16;
 #endif
 
 TEST(SetCoverTest, KnightsCoverCreation) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   EXPECT_TRUE(model.ComputeFeasibility());
 }
 
 TEST(SetCoverTest, KnightsCoverTrivalAndGreedy) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   EXPECT_TRUE(model.ComputeFeasibility());
   SetCoverInvariant inv(&model);
 
@@ -214,7 +223,7 @@ TEST(SetCoverTest, KnightsCoverTrivalAndGreedy) {
 }
 
 TEST(SetCoverTest, KnightsCoverGreedy) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   SetCoverInvariant inv(&model);
 
   GreedySolutionGenerator greedy(&inv);
@@ -227,7 +236,7 @@ TEST(SetCoverTest, KnightsCoverGreedy) {
 }
 
 TEST(SetCoverTest, KnightsCoverDegree) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   SetCoverInvariant inv(&model);
 
   ElementDegreeSolutionGenerator degree(&inv);
@@ -240,7 +249,7 @@ TEST(SetCoverTest, KnightsCoverDegree) {
 }
 
 TEST(SetCoverTest, KnightsCoverGLS) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   SetCoverInvariant inv(&model);
   GreedySolutionGenerator greedy(&inv);
   CHECK(greedy.NextSolution());
@@ -251,7 +260,7 @@ TEST(SetCoverTest, KnightsCoverGLS) {
 }
 
 TEST(SetCoverTest, KnightsCoverRandom) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   EXPECT_TRUE(model.ComputeFeasibility());
   SetCoverInvariant inv(&model);
 
@@ -267,7 +276,7 @@ TEST(SetCoverTest, KnightsCoverRandom) {
 }
 
 TEST(SetCoverTest, KnightsCoverTrivial) {
-  SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+  SetCoverModel model = KnightsCover(SIZE, SIZE).model();
   EXPECT_TRUE(model.ComputeFeasibility());
   SetCoverInvariant inv(&model);
 
@@ -288,7 +297,8 @@ TEST(SetCoverTest, KnightsCoverGreedyAndTabu) {
 #else
   constexpr int BoardSize = 15;
 #endif
-  SetCoverModel model = CreateKnightsCoverModel(BoardSize, BoardSize);
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
   SetCoverInvariant inv(&model);
 
   GreedySolutionGenerator greedy(&inv);
@@ -304,7 +314,7 @@ TEST(SetCoverTest, KnightsCoverGreedyAndTabu) {
   CHECK(gts.NextSolution(1'000));
   LOG(INFO) << "GuidedTabuSearch cost: " << inv.cost();
   EXPECT_TRUE(inv.CheckConsistency(CL::kFreeAndUncovered));
-  DisplayKnightsCoverSolution(inv.is_selected(), BoardSize, BoardSize);
+  knights.DisplaySolution(inv.is_selected());
 }
 
 TEST(SetCoverTest, KnightsCoverGreedyRandomClear) {
@@ -313,7 +323,8 @@ TEST(SetCoverTest, KnightsCoverGreedyRandomClear) {
 #else
   constexpr int BoardSize = 15;
 #endif
-  SetCoverModel model = CreateKnightsCoverModel(BoardSize, BoardSize);
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
   SetCoverInvariant inv(&model);
   Cost best_cost = std::numeric_limits<Cost>::max();
   SubsetBoolVector best_choices = inv.is_selected();
@@ -334,7 +345,7 @@ TEST(SetCoverTest, KnightsCoverGreedyRandomClear) {
     }
   }
   inv.LoadSolution(best_choices);
-  DisplayKnightsCoverSolution(best_choices, BoardSize, BoardSize);
+  knights.DisplaySolution(best_choices);
   LOG(INFO) << "RandomClear cost: " << best_cost;
   // The best solution found until 2023-08 has a cost of 350.
   // http://www.contestcen.com/kn50.htm
@@ -349,7 +360,8 @@ TEST(SetCoverTest, KnightsCoverElementDegreeRandomClear) {
 #else
   constexpr int BoardSize = 15;
 #endif
-  SetCoverModel model = CreateKnightsCoverModel(BoardSize, BoardSize);
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
   SetCoverInvariant inv(&model);
   Cost best_cost = std::numeric_limits<Cost>::max();
   SubsetBoolVector best_choices;
@@ -370,8 +382,52 @@ TEST(SetCoverTest, KnightsCoverElementDegreeRandomClear) {
     ClearRandomSubsets(0.1 * inv.trace().size(), &inv);
   }
   inv.LoadSolution(best_choices);
-  DisplayKnightsCoverSolution(best_choices, BoardSize, BoardSize);
+  knights.DisplaySolution(best_choices);
   LOG(INFO) << "RandomClear cost: " << best_cost;
+  // The best solution found until 2023-08 has a cost of 350.
+  // http://www.contestcen.com/kn50.htm
+  if (BoardSize == 50) {
+    CHECK_GE(inv.cost(), 350);
+  }
+}
+
+TEST(SetCoverTest, KnightsCoverElementDegreeRadiusClear) {
+#ifdef NDEBUG
+  constexpr int BoardSize = 50;
+#else
+  constexpr int BoardSize = 15;
+#endif
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
+  SetCoverInvariant inv(&model);
+  Cost best_cost = std::numeric_limits<Cost>::max();
+  SubsetBoolVector best_choices;
+  int iteration = 0;
+  for (int radius = 7; radius >= 1; --radius) {
+    for (int row = 0; row < BoardSize; ++row) {
+      for (int col = 0; col < BoardSize; ++col) {
+        LazyElementDegreeSolutionGenerator degree(&inv);
+        CHECK(degree.NextSolution());
+        CHECK(inv.CheckConsistency(CL::kCostAndCoverage));
+
+        SteepestSearch steepest(&inv);
+        CHECK(steepest.NextSolution(100));
+
+        if (inv.cost() < best_cost) {
+          best_cost = inv.cost();
+          best_choices = inv.is_selected();
+          LOG(INFO) << "Best cost: " << best_cost
+                    << " at iteration = " << iteration;
+        }
+        inv.LoadSolution(best_choices);
+        knights.ClearSubsetWithinRadius(row, col, radius, &inv);
+        ++iteration;
+      }
+    }
+  }
+  inv.LoadSolution(best_choices);
+  knights.DisplaySolution(best_choices);
+  LOG(INFO) << "RadiusClear cost: " << best_cost;
   // The best solution found until 2023-08 has a cost of 350.
   // http://www.contestcen.com/kn50.htm
   if (BoardSize == 50) {
@@ -385,7 +441,8 @@ TEST(SetCoverTest, KnightsCoverRandomClearMip) {
 #else
   constexpr int BoardSize = 15;
 #endif
-  SetCoverModel model = CreateKnightsCoverModel(BoardSize, BoardSize);
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
   SetCoverInvariant inv(&model);
   GreedySolutionGenerator greedy(&inv);
   CHECK(greedy.NextSolution());
@@ -409,7 +466,7 @@ TEST(SetCoverTest, KnightsCoverRandomClearMip) {
     }
     inv.LoadSolution(best_choices);
   }
-  DisplayKnightsCoverSolution(best_choices, BoardSize, BoardSize);
+  knights.DisplaySolution(best_choices);
   LOG(INFO) << "RandomClearMip cost: " << best_cost;
   // The best solution found until 2023-08 has a cost of 350.
   // http://www.contestcen.com/kn50.htm
@@ -424,12 +481,13 @@ TEST(SetCoverTest, KnightsCoverMip) {
 #else
   constexpr int BoardSize = 15;
 #endif
-  SetCoverModel model = CreateKnightsCoverModel(BoardSize, BoardSize);
+  KnightsCover knights(BoardSize, BoardSize);
+  SetCoverModel model = knights.model();
   SetCoverInvariant inv(&model);
   SetCoverMip mip(&inv);
   mip.NextSolution(true, .5);
   LOG(INFO) << "Mip cost: " << inv.cost();
-  DisplayKnightsCoverSolution(inv.is_selected(), BoardSize, BoardSize);
+  knights.DisplaySolution(inv.is_selected());
   if (BoardSize == 50) {
     CHECK_GE(inv.cost(), 350);
   }
@@ -437,7 +495,7 @@ TEST(SetCoverTest, KnightsCoverMip) {
 
 void BM_Steepest(benchmark::State& state) {
   for (auto s : state) {
-    SetCoverModel model = CreateKnightsCoverModel(SIZE, SIZE);
+    SetCoverModel model = KnightsCover(SIZE, SIZE).model();
     SetCoverInvariant inv(&model);
     GreedySolutionGenerator greedy(&inv);
     SteepestSearch steepest(&inv);
