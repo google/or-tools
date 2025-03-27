09cec082a01576157a299a1d8650f7b5021df08e
Laurent Perron <lperron@google.com>
2025-03-27 08:28:58 -0700

    remove useless comments

--- ortools/scheduling/python/rcpsp_test.py
+++ ortools/scheduling/python/rcpsp_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for ortools.scheduling.python.rcpsp."""
-
 from absl import app
 from absl import flags
 from absl.testing import absltest
