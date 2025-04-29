d74962d55c0ed5c98103fca29761d889169c0521
Francesco Cavaliere <francescocava95@gmail.com>
2025-04-29 17:47:05 +0200

    Updated `bin_packing_cft.cc` example

--- ortools/algorithms/samples/bin_packing_cft.cc
+++ ortools/algorithms/samples/bin_packing_cft.cc
@@ -1,6 +1,21 @@
+
+// Copyright 2025 Francesco Cavaliere
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <absl/log/initialize.h>
 
 #include <cstdlib>
+#include <random>
 
 #include "ortools/algorithms/bin_packing.h"
 #include "ortools/base/init_google.h"
@@ -15,11 +30,20 @@ int main(int argc, char** argv) {
 
   BinPackingModel model = ReadBpp(absl::GetFlag(FLAGS_instance));
 
-  BinPackingSetCoverModel scp_model =
-      GenerateBins(model, absl::GetFlag(FLAGS_bins));
+  // Quick run with a minimal set of bins
+  BinPackingSetCoverModel scp_model = GenerateInitialBins(model);
+  scp::PrimalDualState best_result = scp::RunCftHeuristic(scp_model);
 
-  scp::PrimalDualState result = scp::RunCftHeuristic(scp_model);
-  auto [solution, dual] = result;
+  // Run the CFT again with more bins to get a better solution
+  std::mt19937 rnd(0);
+  AddRandomizedBins(model, absl::GetFlag(FLAGS_bins), scp_model, rnd);
+  scp::PrimalDualState result =
+      scp::RunCftHeuristic(scp_model, best_result.solution);
+  if (result.solution.cost() < best_result.solution.cost()) {
+    best_result = result;
+  }
+
+  auto [solution, dual] = best_result;
   if (solution.subsets().empty()) {
     std::cerr << "Error: failed to find any solution\n";
   } else {
