9b7ba5b8e3df261fb04b3209758cd20e24e492fa
Corentin Le Molgat <corentinl@google.com>
2025-05-26 15:07:31 +0200

    scheduling: export from google3

--- ortools/scheduling/rcpsp_parser.cc
+++ ortools/scheduling/rcpsp_parser.cc
@@ -17,6 +17,8 @@
 #include <string>
 #include <vector>
 
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/strings/match.h"
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_split.h"
