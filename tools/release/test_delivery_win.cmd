2b2a5f69e10e052439b7beef03541da3b23e2437
Corentin Le Molgat <corentinl@google.com>
2025-05-26 16:00:20 +0200

    tools/release: remove Python 3.8 support
    note: BTW Protobuf v31.0 do not support Python 3.8

--- tools/release/test_delivery_win.cmd
+++ tools/release/test_delivery_win.cmd
@@ -11,49 +11,18 @@ make.exe print-OR_TOOLS_VERSION | tee.exe test.log
 which.exe cmake || exit 1
 which.exe cmake | tee.exe -a test.log
 REM Python
-which.exe C:\python38-64\python.exe || exit 1
-echo C:\python38-64\python.exe: FOUND | tee.exe -a test.log
 which.exe C:\python39-64\python.exe || exit 1
 echo C:\python39-64\python.exe: FOUND | tee.exe -a test.log
 which.exe C:\python310-64\python.exe || exit 1
 echo C:\python310-64\python.exe: FOUND | tee.exe -a test.log
 which.exe C:\python311-64\python.exe || exit 1
 echo C:\python311-64\python.exe: FOUND | tee.exe -a test.log
+which.exe C:\python312-64\python.exe || exit 1
+echo C:\python312-64\python.exe: FOUND | tee.exe -a test.log
+which.exe C:\python313-64\python.exe || exit 1
+echo C:\python313-64\python.exe: FOUND | tee.exe -a test.log
 
 set LOCAL_PATH=%PATH%
-
-REM ##################
-REM ##  PYTHON 3.8  ##
-REM ##################
-echo Cleaning Python... | tee.exe -a test.log
-make.exe clean_python WINDOWS_PATH_TO_PYTHON=c:\python38-64
-echo Cleaning Python...DONE | tee.exe -a test.log
-
-REM make.exe python WINDOWS_PATH_TO_PYTHON=c:\python38-64 || exit 1
-REM echo make python3.8: DONE | tee.exe -a build.log
-REM make.exe test_python WINDOWS_PATH_TO_PYTHON=c:\python38-64 || exit 1
-REM echo make test_python3.8: DONE | tee.exe -a build.log
-echo Rebuild Python3.8 pypi archive... | tee.exe -a test.log
-make.exe package_python WINDOWS_PATH_TO_PYTHON=c:\python38-64 || exit 1
-echo Rebuild Python3.8 pypi archive...DONE | tee.exe -a test.log
-
-echo Creating Python3.8 venv... | tee.exe -a test.log
-set PATH=c:\python38-64;c:\python38-64\Scripts;%PATH%
-python -m pip install virtualenv
-set TEMP_DIR=temp_python38
-python -m virtualenv %TEMP_DIR%\venv
-set PATH=%LOCAL_PATH%
-echo Creating Python3.8 venv...DONE | tee.exe -a test.log
-
-echo Installing ortools Python3.8 venv... | tee.exe -a test.log
-FOR %%i IN (%TEMP_DIR%\ortools\dist\*.whl) DO %TEMP_DIR%\venv\Scripts\python -m pip install %%i
-echo Installing ortools Python3.8 venv...DONE | tee.exe -a test.log
-
-echo Testing ortools Python3.8... | tee.exe -a test.log
-%TEMP_DIR%\venv\Scripts\python cmake\samples\python\sample.py 2>&1 | tee.exe -a test.log
-echo Testing ortools Python3.8...DONE | tee.exe -a test.log
-
-FOR %%i IN (%TEMP_DIR%\ortools\dist\*.whl) DO copy %%i .
 
 REM ##################
 REM ##  PYTHON 3.9  ##
