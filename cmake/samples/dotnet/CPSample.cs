8a2a537cc5f9009b4902ac14a0207147434f20ce
Corentin Le Molgat <corentinl@google.com>
2025-06-16 11:48:43 +0200

    cmake: format samples

--- cmake/samples/dotnet/CPSample.cs
+++ cmake/samples/dotnet/CPSample.cs
@@ -16,22 +16,26 @@ using Xunit;
 
 using Google.OrTools.ConstraintSolver;
 
-namespace Google.OrTools.Tests {
-  public class ConstraintSolverTest {
+namespace Google.OrTools.Tests
+{
+public class ConstraintSolverTest
+{
     [Theory]
     [InlineData(false)]
     [InlineData(true)]
-    public void SolverTest(bool callGC) {
-      Solver solver = new Solver("Solver");
-      IntVar x = solver.MakeIntVar(3, 7, "x");
+    public void SolverTest(bool callGC)
+    {
+        Solver solver = new Solver("Solver");
+        IntVar x = solver.MakeIntVar(3, 7, "x");
 
-      if (callGC) {
-        GC.Collect();
-      }
+        if (callGC)
+        {
+            GC.Collect();
+        }
 
-      Assert.Equal(3, x.Min());
-      Assert.Equal(7, x.Max());
-      Assert.Equal("x(3..7)", x.ToString());
+        Assert.Equal(3, x.Min());
+        Assert.Equal(7, x.Max());
+        Assert.Equal("x(3..7)", x.ToString());
     }
-  }
+}
 } // namespace Google.Sample.Tests
