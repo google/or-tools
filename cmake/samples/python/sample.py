91eae80e1c0c3d8f3db97df4b4fce22cd9ac89f3
Corentin Le Molgat <corentinl@google.com>
2025-06-16 11:47:36 +0200

    cmake: Format samples

--- cmake/samples/python/sample.py
+++ cmake/samples/python/sample.py
@@ -14,12 +14,15 @@
 
 """Sample to test or-tools installation."""
 import ortools
+
 # from ortools.algorithms import knapsack_solver
 from ortools.constraint_solver import pywrapcp
+
 # from ortools.graph.python import linear_sum_assignment
 # from ortools.graph.python import max_flow
 # from ortools.graph.python import min_cost_flow
 from ortools.linear_solver import pywraplp
+
 # from ortools.linear_solver import linear_solver_pb2
 # from ortools.sat.python import cp_model_helper
 # from ortools.sat.python import cp_model
@@ -28,34 +31,34 @@ from ortools.linear_solver import pywraplp
 
 
 def lpsolver_test():
-  """Test pywraplp."""
-  print('Test lpsolver...')
-  lpsolver = pywraplp.Solver('LinearTest',
-                             pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-  lpsolver.Solve()
-  print('Test lpsolver...DONE')
+    """Test pywraplp."""
+    print("Test lpsolver...")
+    lpsolver = pywraplp.Solver("LinearTest", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    lpsolver.Solve()
+    print("Test lpsolver...DONE")
 
 
 def cpsolver_test():
-  """Test pywrapcp."""
-  print('Test cpsolver...')
-  cpsolver = pywrapcp.Solver('ConstraintTest')
-  num_vals = 3
-  x = cpsolver.IntVar(0, num_vals - 1, 'x')
-  y = cpsolver.IntVar(0, num_vals - 1, 'y')
-  z = cpsolver.IntVar(0, num_vals - 1, 'z')
-  cpsolver.Add(x != y)
-  db = cpsolver.Phase([x, y, z], cpsolver.CHOOSE_FIRST_UNBOUND,
-                      cpsolver.ASSIGN_MIN_VALUE)
-  cpsolver.Solve(db)
-  print('Test cpsolver...DONE')
+    """Test pywrapcp."""
+    print("Test cpsolver...")
+    cpsolver = pywrapcp.Solver("ConstraintTest")
+    num_vals = 3
+    x = cpsolver.IntVar(0, num_vals - 1, "x")
+    y = cpsolver.IntVar(0, num_vals - 1, "y")
+    z = cpsolver.IntVar(0, num_vals - 1, "z")
+    cpsolver.Add(x != y)
+    db = cpsolver.Phase(
+        [x, y, z], cpsolver.CHOOSE_FIRST_UNBOUND, cpsolver.ASSIGN_MIN_VALUE
+    )
+    cpsolver.Solve(db)
+    print("Test cpsolver...DONE")
 
 
 def main():
-  print(ortools.__version__)
-  lpsolver_test()
-  cpsolver_test()
+    print(ortools.__version__)
+    lpsolver_test()
+    cpsolver_test()
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
