6af8bd54b4728cd61afcdfb167e217cfae8036c0
Guillaume Chatelet <gchatelet@google.com>
2025-07-02 12:20:11 +0000

    Turn some `.i` files into `.swig`
    This helps with the import / export process.

--- ortools/linear_solver/python/pywraplp_test.py
+++ ortools/linear_solver/python/pywraplp_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Simple unit tests for python/linear_solver.i. Not exhaustive."""
+"""Simple unit tests for python/linear_solver.swig. Not exhaustive."""
 
 import unittest
 from ortools.linear_solver import linear_solver_pb2
