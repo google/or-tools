24197c83ca96b87f0a21ebbdf2ff8f260d18e6c6
Corentin Le Molgat <corentinl@google.com>
2025-09-08 16:08:17 +0200

    math_opt: export fixup from google3

--- ortools/math_opt/cpp/message_callback.cc
+++ ortools/math_opt/cpp/message_callback.cc
@@ -92,7 +92,7 @@ MessageCallback PrinterMessageCallback(std::ostream& output_stream,
 
 MessageCallback InfoLoggerMessageCallback(const absl::string_view prefix,
                                           const absl::SourceLocation loc) {
-  return [=](const std::vector<std::string>& messages) {
+  return [=](absl::Span<const std::string> messages) {
     for (const std::string& message : messages) {
       LOG(INFO).AtLocation(loc.file_name(), loc.line()) << prefix << message;
     }
@@ -127,8 +127,7 @@ MessageCallback RepeatedPtrFieldMessageCallback(
   // it uses an absl::Mutex that is not.
   const auto impl = std::make_shared<VectorLikeMessageCallbackImpl<
       google::protobuf::RepeatedPtrField<std::string>>>(sink);
-  return
-      [=](const std::vector<std::string>& messages) { impl->Call(messages); };
+  return [=](absl::Span<const std::string> messages) { impl->Call(messages); };
 }
 
 }  // namespace operations_research::math_opt
