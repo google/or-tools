6711344cf51d510d0bfda81e974f5938cca25997
Corentin Le Molgat <corentinl@google.com>
2025-11-05 10:29:19 +0100

    set_cover: update include guards

--- ortools/set_cover/capacity_model.h
+++ ortools/set_cover/capacity_model.h
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef OR_TOOLS_SET_COVER_CAPACITY_MODEL_H_
-#define OR_TOOLS_SET_COVER_CAPACITY_MODEL_H_
+#ifndef ORTOOLS_SET_COVER_CAPACITY_MODEL_H_
+#define ORTOOLS_SET_COVER_CAPACITY_MODEL_H_
 
 #include <cmath>
 #include <cstdint>
@@ -152,4 +152,4 @@ class CapacityModel {
 };
 }  // namespace operations_research
 
-#endif  // OR_TOOLS_SET_COVER_CAPACITY_MODEL_H_
+#endif  // ORTOOLS_SET_COVER_CAPACITY_MODEL_H_
--- ortools/set_cover/set_cover_mip.h
+++ ortools/set_cover/set_cover_mip.h
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef OR_TOOLS_SET_COVER_SET_COVER_MIP_H_
-#define OR_TOOLS_SET_COVER_SET_COVER_MIP_H_
+#ifndef ORTOOLS_SET_COVER_SET_COVER_MIP_H_
+#define ORTOOLS_SET_COVER_SET_COVER_MIP_H_
 
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
@@ -76,4 +76,4 @@ class SetCoverMip : public SubsetListBasedSolutionGenerator {
 };
 }  // namespace operations_research
 
-#endif  // OR_TOOLS_SET_COVER_SET_COVER_MIP_H_
+#endif  // ORTOOLS_SET_COVER_SET_COVER_MIP_H_
