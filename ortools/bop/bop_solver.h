38b6fc38c785fff4ecd02bc4d454b7eca4c23256
Corentin Le Molgat <corentinl@google.com>
2025-11-05 10:05:59 +0100

    bop: update include guards

--- ortools/bop/bop_solver.h
+++ ortools/bop/bop_solver.h
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef OR_TOOLS_BOP_BOP_SOLVER_H_
-#define OR_TOOLS_BOP_BOP_SOLVER_H_
+#ifndef ORTOOLS_BOP_BOP_SOLVER_H_
+#define ORTOOLS_BOP_BOP_SOLVER_H_
 
 // Solver for Boolean Optimization Problems built on top of the SAT solver.
 // To optimize a problem the solver uses several optimization strategies like
@@ -90,4 +90,4 @@ class BopSolver {
 };
 }  // namespace bop
 }  // namespace operations_research
-#endif  // OR_TOOLS_BOP_BOP_SOLVER_H_
+#endif  // ORTOOLS_BOP_BOP_SOLVER_H_
