00051edcd0b205e57e20f4dd9d4133747950b18e
Guillaume Chatelet <gchatelet@google.com>
2026-01-08 14:50:40 +0000

    Dispatch root java samples into their respective folders.

--- ortools/graph/samples/LinearAssignmentAPI.java
+++ ortools/graph/samples/LinearAssignmentAPI.java
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.ortools.java;
+package com.google.ortools.graph.samples;
 
 import com.google.ortools.Loader;
 import com.google.ortools.graph.LinearSumAssignment;
