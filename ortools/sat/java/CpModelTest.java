5d262a2c142ee6aa76dac9ddeb4030797499676d
Corentin Le Molgat <corentinl@google.com>
2026-01-28 17:42:53 +0100

    java: Fix junit export

--- ortools/sat/java/CpModelTest.java
+++ ortools/sat/java/CpModelTest.java
@@ -14,7 +14,7 @@
 package com.google.ortools.sat;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.google.ortools.Loader;
 import com.google.ortools.sat.CpSolverStatus;
@@ -27,16 +27,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Consumer;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /** Tests the CpSolver java interface. */
-@RunWith(JUnit4.class)
 public final class CpModelTest {
-  @SuppressWarnings("EmptyMethods")
-  @Before
+  @BeforeEach
   public void setUp() {
     Loader.loadNativeLibraries();
   }
