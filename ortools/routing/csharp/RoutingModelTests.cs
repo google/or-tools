23edaa466ff9c986f2c902a9a8b258011fcd5a26
Corentin Le Molgat <corentinl@google.com>
2026-02-04 14:56:46 +0100

    routing: rework csharp test
    * split tests
    * add few tests to improve coverage

--- ortools/routing/csharp/RoutingModelTests.cs
+++ ortools/routing/csharp/RoutingModelTests.cs
@@ -19,7 +19,7 @@ using Google.OrTools.Routing;
 
 namespace Google.OrTools.Tests
 {
-public class RoutingSolverTest
+public class RoutingModelTest
 {
     [Theory]
     [InlineData(false)]
@@ -206,119 +206,6 @@ public class RoutingSolverTest
         Assignment solution = routing.SolveWithParameters(searchParameters);
         // 0 --(+1)-> 1 --(+1)-> 2 --(+1)-> 3 --(+1)-> 4 --(+1)-> 0 := +5
         Assert.Equal(5, solution.ObjectiveValue());
-    }
-}
-
-public class BoundCostTest
-{
-    [Fact]
-    public void TestCtor()
-    {
-        // Create Routing Index Manager
-        BoundCost boundCost = new BoundCost();
-        Assert.NotNull(boundCost);
-        Assert.Equal(0, boundCost.bound);
-        Assert.Equal(0, boundCost.cost);
-
-        boundCost = new BoundCost(97 /*bound*/, 101 /*cost*/);
-        Assert.NotNull(boundCost);
-        Assert.Equal(97, boundCost.bound);
-        Assert.Equal(101, boundCost.cost);
-    }
-}
-
-public class RoutingDimensionTest
-{
-    [Fact]
-    public void TestCtor()
-    {
-        // Create Routing Index Manager
-        IndexManager manager = new IndexManager(31 /*locations*/, 7 /*vehicle*/, 3 /*depot*/);
-        Assert.NotNull(manager);
-        // Create Routing Model.
-        Model routing = new Model(manager);
-        Assert.NotNull(routing);
-        // Create a distance callback.
-        int transitIndex = routing.RegisterTransitCallback((long fromIndex, long toIndex) =>
-                                                           {
-                                                               // Convert from routing variable Index to
-                                                               // distance matrix NodeIndex.
-                                                               var fromNode = manager.IndexToNode(fromIndex);
-                                                               var toNode = manager.IndexToNode(toIndex);
-                                                               return Math.Abs(toNode - fromNode);
-                                                           });
-        Assert.True(routing.AddDimension(transitIndex, 100, 100, true, "Dimension"));
-        Dimension dimension = routing.GetDimensionOrDie("Dimension");
-    }
-
-    [Fact]
-    public void TestSoftSpanUpperBound()
-    {
-        // Create Routing Index Manager
-        IndexManager manager = new IndexManager(31 /*locations*/, 7 /*vehicle*/, 3 /*depot*/);
-        Assert.NotNull(manager);
-        // Create Routing Model.
-        Model routing = new Model(manager);
-        Assert.NotNull(routing);
-        // Create a distance callback.
-        int transitIndex = routing.RegisterTransitCallback((long fromIndex, long toIndex) =>
-                                                           {
-                                                               // Convert from routing variable Index to
-                                                               // distance matrix NodeIndex.
-                                                               var fromNode = manager.IndexToNode(fromIndex);
-                                                               var toNode = manager.IndexToNode(toIndex);
-                                                               return Math.Abs(toNode - fromNode);
-                                                           });
-        Assert.True(routing.AddDimension(transitIndex, 100, 100, true, "Dimension"));
-        Dimension dimension = routing.GetDimensionOrDie("Dimension");
-
-        BoundCost boundCost = new BoundCost(/*bound=*/97, /*cost=*/43);
-        Assert.NotNull(boundCost);
-        Assert.False(dimension.HasSoftSpanUpperBounds());
-        foreach (int v in Enumerable.Range(0, manager.GetNumberOfVehicles()).ToArray())
-        {
-            dimension.SetSoftSpanUpperBoundForVehicle(boundCost, v);
-            BoundCost bc = dimension.GetSoftSpanUpperBoundForVehicle(v);
-            Assert.NotNull(bc);
-            Assert.Equal(97, bc.bound);
-            Assert.Equal(43, bc.cost);
-        }
-        Assert.True(dimension.HasSoftSpanUpperBounds());
-    }
-
-    [Fact]
-    public void TestQuadraticCostSoftSpanUpperBound()
-    {
-        // Create Routing Index Manager
-        IndexManager manager = new IndexManager(31 /*locations*/, 7 /*vehicle*/, 3 /*depot*/);
-        Assert.NotNull(manager);
-        // Create Routing Model.
-        Model routing = new Model(manager);
-        Assert.NotNull(routing);
-        // Create a distance callback.
-        int transitIndex = routing.RegisterTransitCallback((long fromIndex, long toIndex) =>
-                                                           {
-                                                               // Convert from routing variable Index to
-                                                               // distance matrix NodeIndex.
-                                                               var fromNode = manager.IndexToNode(fromIndex);
-                                                               var toNode = manager.IndexToNode(toIndex);
-                                                               return Math.Abs(toNode - fromNode);
-                                                           });
-        Assert.True(routing.AddDimension(transitIndex, 100, 100, true, "Dimension"));
-        Dimension dimension = routing.GetDimensionOrDie("Dimension");
-
-        BoundCost boundCost = new BoundCost(/*bound=*/97, /*cost=*/43);
-        Assert.NotNull(boundCost);
-        Assert.False(dimension.HasQuadraticCostSoftSpanUpperBounds());
-        foreach (int v in Enumerable.Range(0, manager.GetNumberOfVehicles()).ToArray())
-        {
-            dimension.SetQuadraticCostSoftSpanUpperBoundForVehicle(boundCost, v);
-            BoundCost bc = dimension.GetQuadraticCostSoftSpanUpperBoundForVehicle(v);
-            Assert.NotNull(bc);
-            Assert.Equal(97, bc.bound);
-            Assert.Equal(43, bc.cost);
-        }
-        Assert.True(dimension.HasQuadraticCostSoftSpanUpperBounds());
     }
 }
 
