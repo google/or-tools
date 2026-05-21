8fc99cb5340ca61d949c3274557b326b1a493283
Laurent Perron <lperron@google.com>
2026-05-21 15:58:01 +0200

    graph: export from google3

--- ortools/graph/bounded_dijkstra_benchmark.cc
+++ ortools/graph/bounded_dijkstra_benchmark.cc
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 #include <cstdint>
+#include <functional>
 #include <memory>
 #include <random>
 #include <vector>
@@ -27,35 +28,27 @@
 namespace operations_research {
 namespace {
 
-template <bool arc_lengths_are_discrete>
-void BM_GridGraph(benchmark::State& state) {
-  typedef util::StaticGraph<int> Graph;
-  const int64_t kWidth = 100;
-  const int64_t kHeight = 10000;
-  const int kSourceNode = static_cast<int>(kWidth * kHeight / 2);
-  std::unique_ptr<Graph> graph =
-      util::Create2DGridGraph<Graph>(/*width=*/kWidth, /*height=*/kHeight);
-  BoundedDijkstraWrapper<Graph, int64_t>::ByArc<int64_t> arc_lengths(
-      graph->num_arcs(), 0);
-  const int64_t min_length = arc_lengths_are_discrete ? 0 : 1;
-  const int64_t max_length = arc_lengths_are_discrete ? 2 : 1000000000000000L;
-  std::mt19937 random(12345);
-  for (int64_t& length : arc_lengths) {
-    length = absl::Uniform(random, min_length, max_length + 1);
-  }
-  BoundedDijkstraWrapper<Graph, int64_t> dijkstra(graph.get(), &arc_lengths);
-  const int64_t kSearchRadius = kWidth * (min_length + max_length) / 2;
+constexpr int64_t kBenchmarkWidth = 100;
+constexpr int64_t kBenchmarkHeight = 10000;
+using Graph = util::StaticGraph<int>;
+
+template <typename T>
+void RunBenchmark(benchmark::State& state, T& dijkstra,
+                  const int64_t search_radius) {
+  const int kSourceNode =
+      static_cast<int>(kBenchmarkWidth * kBenchmarkHeight / 2);
   // NOTE(user): The expected number of nodes visited is in ϴ(kWidth²),
   // since the search radius is ϴ(kWidth). The exact constant is hard to
   // derive mathematically as a function of the radius formula, so I measured
   // it empirically and it was close to 0.5, which seemed about right.
-  const int64_t kExpectedApproximateSearchSize = kWidth * kWidth / 2;
+  const int64_t kExpectedApproximateSearchSize =
+      kBenchmarkWidth * kBenchmarkWidth / 2;
   int64_t total_nodes_visited = 0;
   for (auto _ : state) {
     const int num_nodes_visited =
         dijkstra
             .RunBoundedDijkstra(/*source_node=*/kSourceNode,
-                                /*distance_limit=*/kSearchRadius)
+                                /*distance_limit=*/search_radius)
             .size();
     // We verify that the Dijkstra search size is in the expected range, to
     // make sure that we're measuring what we want in this benchmark.
@@ -66,8 +59,45 @@ void BM_GridGraph(benchmark::State& state) {
   state.SetItemsProcessed(total_nodes_visited);
 }
 
+template <bool arc_lengths_are_discrete>
+void BM_GridGraph(benchmark::State& state) {
+  std::unique_ptr<Graph> graph =
+      util::Create2DGridGraph<Graph>(kBenchmarkWidth, kBenchmarkHeight);
+  BoundedDijkstraWrapper<Graph, int64_t>::ByArc<int64_t> arc_lengths(
+      graph->num_arcs(), 0);
+  const int64_t min_length = arc_lengths_are_discrete ? 0 : 1;
+  const int64_t max_length = arc_lengths_are_discrete ? 2 : 1000000000000000L;
+  std::mt19937 random(12345);
+  for (int64_t& length : arc_lengths) {
+    length = absl::Uniform(random, min_length, max_length + 1);
+  }
+  const int64_t kSearchRadius = kBenchmarkWidth * (min_length + max_length) / 2;
+  BoundedDijkstraWrapper<Graph, int64_t> dijkstra(graph.get(), &arc_lengths);
+  RunBenchmark(state, dijkstra, kSearchRadius);
+}
+
 BENCHMARK(BM_GridGraph<true>);
 BENCHMARK(BM_GridGraph<false>);
+
+void BM_GridGraph_ArcLengthFunctor(benchmark::State& state) {
+  std::unique_ptr<Graph> graph =
+      util::Create2DGridGraph<Graph>(kBenchmarkWidth, kBenchmarkHeight);
+  BoundedDijkstraWrapper dijkstra(
+      graph.get(), [](int node_id) { return 1 + (node_id % 3); });
+  RunBenchmark(state, dijkstra, 2 * kBenchmarkWidth);
+}
+BENCHMARK(BM_GridGraph_ArcLengthFunctor);
+
+void BM_GridGraph_ArcLengthStdFunction(benchmark::State& state) {
+  std::unique_ptr<Graph> graph =
+      util::Create2DGridGraph<Graph>(kBenchmarkWidth, kBenchmarkHeight);
+  std::function<int64_t(int)> get_arc_length = [](int node_id) {
+    return 1 + (node_id % 3);
+  };
+  BoundedDijkstraWrapper dijkstra(graph.get(), get_arc_length);
+  RunBenchmark(state, dijkstra, 2 * kBenchmarkWidth);
+}
+BENCHMARK(BM_GridGraph_ArcLengthStdFunction);
 
 }  // namespace
 }  // namespace operations_research
